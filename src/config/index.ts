87e36841b0d318f5c32f09adb1f981ad166ef89b
josemak25 <amakirij@gmail.com>
2020-07-04 23:01:35 +0100

    add app env config file

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+//@ts-nocheck
+const { TRIBL_SERVER_BASE_URI } = 'react-native-dotenv';
+
+type EnvPropsTypes = { TRIBL_SERVER_BASE_URI: string };
+
+const ENVIRONMENT_VARIABLES = { TRIBL_SERVER_BASE_URI } as EnvPropsTypes;
+
+export default ENVIRONMENT_VARIABLES;
