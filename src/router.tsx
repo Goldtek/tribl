32045204ab13e84de43a286367eeb9b7cd784399
josemak25 <amakirij@gmail.com>
2020-07-05 01:31:21 +0100

    add router for holding all stack navigators

---
+++ src/router.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import BottomNavigator from './navigator/bottomNavigator';
+import AuthNavigator from './navigator/authNavigator';
+import { useThemeContext } from './theme';
+import Screens from './screens';
+
+const RootStack = createStackNavigator();
+
+export default function AppNavigator() {
+  const { fonts, colors } = useThemeContext();
+
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator
+        initialRouteName="CommunityScreen"
+        screenOptions={{
+          headerBackTitleStyle: {
+            fontFamily: fonts.WORK_SANS_MEDIUM,
+            color: colors.PRIMARY_TEXT,
+            fontSize: fonts.MEDIUM_SIZE,
+            textTransform: 'capitalize'
+          }
+        }}
+      >
+        <RootStack.Screen
+          name="WalkThroughScreen"
+          component={Screens.WalkThroughScreen}
+        />
+
+        <RootStack.Screen name="SignupScreen" component={AuthNavigator} />
+
+        <RootStack.Screen
+          name="CommunityScreen"
+          component={BottomNavigator}
+          options={{ headerShown: false }}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+}
