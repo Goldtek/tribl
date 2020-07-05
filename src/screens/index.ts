6c983d036b440b858acb4b6ec0c01398aae8af7a
josemak25 <amakirij@gmail.com>
2020-07-05 01:18:10 +0100

    add default export for all native screen

---
+++ src/screens/index.ts
@@ -0,0 +1,13 @@
+import WalkThroughScreen from './walkThrough';
+import CommunityScreen from './community';
+import ProfileScreen from './profile';
+import InboxScreen from './inbox';
+import TriblScreen from './tribl';
+
+export default {
+  WalkThroughScreen,
+  CommunityScreen,
+  ProfileScreen,
+  InboxScreen,
+  TriblScreen
+};
