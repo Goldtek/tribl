37fdb87ea5566b705356138eb7060ce50102f2ec
josemak25 <amakirij@gmail.com>
2020-07-05 22:58:50 +0100

    add walk through screen component styles

--- src/screens/walkThrough/styles.ts
+++ src/screens/walkThrough/styles.ts
@@ -1,15 +1,14 @@
 import styled from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export const Container = styled.View`
+export const GradientContainer = styled(LinearGradient)`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.WHITE};
 `;
 
-export const Welcome = styled.Text`
-  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
-  font-family: ${({ theme }) => theme.fonts.WORK_SANS_REGULAR};
-  color: ${({ theme }) => theme.colors.PRIMARY_TEXT};
-  text-transform: capitalize;
+export const Container = styled.View`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
 `;
