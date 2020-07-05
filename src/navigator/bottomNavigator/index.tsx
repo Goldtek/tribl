68b3fea42198cc624cb712e54c8a605837994d29
josemak25 <amakirij@gmail.com>
2020-07-05 01:27:51 +0100

    add app bottom navigator for holding and routing through all screens

---
+++ src/navigator/bottomNavigator/index.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import CommunityIcon from '../../../assets/icons/communityIcon';
+import ProfileIcon from '../../../assets/icons/profileIcon';
+import InboxIcon from '../../../assets/icons/inboxIcon';
+import TriblIcon from '../../../assets/icons/tribIcon';
+import CommunityNavigator from '../communityNavigator';
+import { useThemeContext } from '../../theme';
+import Screens from '../../screens';
+
+// IMPORT FOR ALL CUSTOM STYLES
+import { IconContainer, Label } from './styles';
+
+export type TabBarIconTypes = { color: string };
+
+const BottomTab = createMaterialBottomTabNavigator();
+
+export default function BottomNavigator() {
+  const { colors } = useThemeContext();
+
+  return (
+    <BottomTab.Navigator
+      activeColor={colors.PRIMARY}
+      inactiveColor={colors.SECONDARY_TEXT}
+      barStyle={{ backgroundColor: colors.WHITE }}
+      initialRouteName="ConnectionScreen"
+      labeled={false}
+    >
+      <BottomTab.Screen
+        name="ConnectionScreen"
+        component={CommunityNavigator}
+        options={{
+          tabBarIcon: ({ color }: TabBarIconTypes) => (
+            <IconContainer>
+              <CommunityIcon fillColor={color} />
+              <Label style={{ color }}>connection</Label>
+            </IconContainer>
+          )
+        }}
+      />
+
+      <BottomTab.Screen
+        name="InboxScreen"
+        component={Screens.InboxScreen}
+        options={{
+          tabBarIcon: ({ color }: TabBarIconTypes) => (
+            <IconContainer>
+              <InboxIcon fillColor={color} />
+              <Label style={{ color }}>inbox</Label>
+            </IconContainer>
+          )
+        }}
+      />
+
+      <BottomTab.Screen
+        name="TriblScreen"
+        component={Screens.TriblScreen}
+        options={{
+          tabBarIcon: ({ color }: TabBarIconTypes) => (
+            <IconContainer>
+              <TriblIcon fillColor={color} />
+              <Label style={{ color }}>tribl</Label>
+            </IconContainer>
+          )
+        }}
+      />
+
+      <BottomTab.Screen
+        name="ProfileScreen"
+        component={Screens.ProfileScreen}
+        options={{
+          tabBarIcon: ({ color }: TabBarIconTypes) => (
+            <IconContainer>
+              <ProfileIcon fillColor={color} />
+              <Label style={{ color }}>profile</Label>
+            </IconContainer>
+          )
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
---
+++ src/navigator/bottomNavigator/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const IconContainer = styled.View`
+  flex: 1;
+  width: ${RFValue(100)}px;
+  height: ${RFValue(100)}px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Label = styled.Text`
+  font-size: ${({ theme }) => RFValue(theme.fonts.MEDIUM_SIZE - 2)}px;
+  font-family: ${({ theme }) => theme.fonts.WORK_SANS_MEDIUM};
+  color: ${({ theme }) => theme.colors.SECONDARY_TEXT};
+  text-align: center;
+  text-transform: capitalize;
+`;
