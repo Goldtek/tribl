2e3092eb8697eda66a06fc2c5d7df9b52bc69fe9
josemak25 <amakirij@gmail.com>
2020-07-05 01:46:44 +0100

    add device dimension test and add resetting mocks values after every test

--- __tests__/config/jest.setup.ts
+++ __tests__/config/jest.setup.ts
@@ -1,3 +1,7 @@
 import { cleanup } from '@testing-library/react-native';
 
+// RESET ALL MOCKS AFTER EVERY TEST
+afterEach(jest.resetAllMocks);
+
+// CLEAN UP VIRTUAL TEST DOM AFTER ALL TEST
 afterAll(cleanup);
