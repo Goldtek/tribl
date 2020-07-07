b6961c20f5fd94f6bb1ff708e7cfc6755508511f
josemak25 <amakirij@gmail.com>
2020-07-07 23:43:22 +0100

    add all signup screens to default screen export file

--- src/screens/signup/index.tsx
+++ src/screens/signup/index.tsx
@@ -1,9 +1,17 @@
 import SelectCountryScreen from './selectCountry';
+import CreateAccountScreen from './createAccount';
+import AvatarUploadScreen from './avatarUpload';
+import IdentifyUserScreen from './identifyUser';
+import UserLocationScreen from './userLocation';
 import GetStartedScreen from './getStarted';
 import OTPScreen from './otp';
 
 export default {
+  CreateAccountScreen,
   SelectCountryScreen,
+  AvatarUploadScreen,
+  IdentifyUserScreen,
+  UserLocationScreen,
   GetStartedScreen,
   OTPScreen
 };
