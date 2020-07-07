675b14ad862006a9c9f2b3609c5bc8c69281e205
josemak25 <amakirij@gmail.com>
2020-07-07 13:59:44 +0100

    add mocks and fixed device test cases

--- __tests__/config/jest.setup.ts
+++ __tests__/config/jest.setup.ts
@@ -1,5 +1,10 @@
 import { cleanup } from '@testing-library/react-native';
 
+jest.mock(
+  'react-native-localize',
+  () => require('../../__mocks__/Localize').default
+);
+
 // RESET ALL MOCKS AFTER EVERY TEST
 afterEach(jest.resetAllMocks);
 
--- __tests__/utils/device.ts
+++ __tests__/utils/device.ts
@@ -1,8 +1,12 @@
 describe('TEST DEVICE UTIL METHOD', () => {
   test('test when util method is called it returns user phone width and height values', () => {
     const device = require('../../src/utils/device');
+
     expect(device).not.toBeNull();
+    expect(device.DEVICE_DEFAULT_COUNTRY).toBeTruthy();
+    expect(device.DEVICE_DEFAULT_LANGUAGE).toBeTruthy();
     expect(device.DEVICE_FULL_HEIGHT).toBeTruthy();
     expect(device.DEVICE_FULL_WIDTH).toBeTruthy();
+    expect(device.DEVICE_OS).toBeTruthy();
   });
 });
