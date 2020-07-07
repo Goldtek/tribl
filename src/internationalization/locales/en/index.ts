ce58ebcbda228f7590abb7f32d74416ed9fe5241
josemak25 <amakirij@gmail.com>
2020-07-07 14:06:37 +0100

    import signup locale internationalization translations on default internationalization export

--- src/internationalization/locales/en/index.ts
+++ src/internationalization/locales/en/index.ts
@@ -1,8 +1,10 @@
 import walkThrough from './walkThrough.json';
+import signup from './signup.json';
 
 export default {
   en: {
     translation: {
+      signup,
       walkThrough,
       change_language_english: 'In english',
       change_language_french: 'In french'
