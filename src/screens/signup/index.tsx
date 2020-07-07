f140b256f28e2f892222e3c528fa6a5fb108a033
josemak25 <amakirij@gmail.com>
2020-07-07 14:19:59 +0100

    add default export for all signup screens

--- src/screens/signup/index.tsx
+++ src/screens/signup/index.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import { NavigationInterface } from '../types';
-import { Button } from 'react-native-paper';
+import SelectCountryScreen from './selectCountry';
+import GetStartedScreen from './getStarted';
+import OTPScreen from './otp';
 
-// IMPORT FOR ALL CUSTOM STYLES
-import { Container, Welcome } from './styles';
-
-// DEFINE SCREEN PROP TYPES
-interface SignupScreenProp extends NavigationInterface {}
-
-export default function SignupScreen(props: SignupScreenProp) {
-  return (
-    <Container>
-      <Button
-        icon="camera"
-        mode="contained"
-        onPress={() => console.log('Pressed')}
-      >
-        Press me
-      </Button>
-      <Welcome>Signup Screen</Welcome>
-    </Container>
-  );
-}
+export default {
+  SelectCountryScreen,
+  GetStartedScreen,
+  OTPScreen
+};
