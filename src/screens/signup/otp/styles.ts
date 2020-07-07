6c81237a034bcccfdbee3bf862262202a5c7c6ca
josemak25 <amakirij@gmail.com>
2020-07-07 23:42:55 +0100

    add otp screen component and implemented its user flow functionalities and also added its styles

--- src/screens/signup/otp/styles.ts
+++ src/screens/signup/otp/styles.ts
@@ -1,15 +1,13 @@
 import styled from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${({ theme }) => theme.colors.WHITE};
 `;
 
-export const Welcome = styled.Text`
-  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
-  font-family: ${({ theme }) => theme.fonts.WORK_SANS_REGULAR};
-  color: ${({ theme }) => theme.colors.PRIMARY_TEXT};
-  text-transform: capitalize;
+export const GradientContainer = styled(LinearGradient)`
+  height: ${RFValue(50)}px;
+  justify-content: center;
+  align-items: center;
 `;
