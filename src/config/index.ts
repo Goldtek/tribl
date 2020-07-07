efa0649c5e85e4738202b8b58ac5b954baf0961d
josemak25 <amakirij@gmail.com>
2020-07-07 14:05:14 +0100

    change import on default device language

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,11 @@
 //@ts-nocheck
 const { TRIBL_SERVER_BASE_URI } = 'react-native-dotenv';
 
-type EnvPropsTypes = { TRIBL_SERVER_BASE_URI: string };
+// FIX THIS TO USE ENVIRONMENT VARIABLES (APP SECRETES)
+const TRIBL_SERVER_BASE_URI =
+  Config.TRIBL_SERVER_BASE_URI ||
+  'https://api.8base.com/ckbtmw54y000507lb1osb1qov';
 
-const ENVIRONMENT_VARIABLES = { TRIBL_SERVER_BASE_URI } as EnvPropsTypes;
+const ENVIRONMENT_VARIABLES = { TRIBL_SERVER_BASE_URI };
 
 export default ENVIRONMENT_VARIABLES;
