8a7bfb76ff0ae1ac6fa6474ba455596a66ee07d1
josemak25 <amakirij@gmail.com>
2020-07-07 14:05:14 +0100

    change import on default device language

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,13 @@
+/*
+ ****************************************************************
+ ******************    ALL APP CONSTANTS   **********************
+ ****************************************************************
+ */
+
+// ALL ASYNC STORAGE CONSTANTS
 export const USER_FIRST_LAUNCH = '@FIRST_TIME_LAUNCH';
+export const USER_TOKEN: string = '@USER_TOKEN';
 
+// ALL PAGE LOGICAL CONSTANTS
 export const PAGINATION_DEFAULT: number = 15;
-
 export const LANGUAGE_DEFAULT: string = 'en';
--- src/internationalization/index.ts
+++ src/internationalization/index.ts
@@ -1,16 +1,12 @@
-import * as Localization from 'react-native-localize';
 import { initReactI18next } from 'react-i18next';
-import { LANGUAGE_DEFAULT } from '../constants';
-import resources from './locales';
 import i18n from 'i18next';
-
-const defaultPhoneLanguage = Localization.findBestAvailableLanguage([
-  LANGUAGE_DEFAULT
-]);
+import { LANGUAGE_DEFAULT } from '../constants';
+import { DEVICE_DEFAULT_LANGUAGE } from '../utils/device';
+import resources from './locales';
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: defaultPhoneLanguage?.languageTag,
+  lng: DEVICE_DEFAULT_LANGUAGE,
   fallbackLng: LANGUAGE_DEFAULT,
   interpolation: { escapeValue: false },
   cleanCode: true
