e3c5e9812d1763ca53c27ba788de91c6bfe12b26
josemak25 <amakirij@gmail.com>
2020-07-07 14:15:44 +0100

    add get started screen component on signup

--- src/screens/inbox/index.tsx
+++ src/screens/inbox/index.tsx
@@ -6,9 +6,9 @@ import { Button } from 'react-native-paper';
 import { Container, Welcome } from './styles';
 
 // DEFINE SCREEN PROP TYPES
-interface InboxScreenProp extends NavigationInterface {}
+interface ScreenProp extends NavigationInterface {}
 
-export default function InboxScreen(props: InboxScreenProp) {
+export default function InboxScreen(props: ScreenProp) {
   return (
     <Container>
       <Button
