d42e7358eec97aab2f227eedde351c74624b21a3
josemak25 <amakirij@gmail.com>
2020-07-18 02:27:13 +0100

    remove all unused codes on cache mutations and merge all catch mutations to one query

--- src/graphql/cache/mutations.ts
+++ src/graphql/cache/mutations.ts
@@ -6,20 +6,6 @@ import gql from 'graphql-tag';
  ****************************************************************
  */
 
-// CHANGE USER COUNTRY
-export const CHANGE_USER_COUNTRY = gql`
-  mutation ChangeUserCountry($countryCode: String!) {
-    changeUserCountry(countryCode: $countryCode) @client
-  }
-`;
-
-// ADD USER PHONE NUMBER TO STORE
-export const ADD_USER_PHONE_NUMBER = gql`
-  mutation AddUserPhoneNumber($number: String!) {
-    addUserPhoneNumber(number: $number) @client
-  }
-`;
-
 // ADD USER DETAILS TO STORE
 export const ADD_USER_DETAILS = gql`
   mutation AddUserDetails($details: String!) {
