c532ffb26ce787bae5ea792754401e35457629fa
josemak25 <amakirij@gmail.com>
2020-07-18 02:36:40 +0100

    add server intergration for otp screen

--- src/screens/signup/otp/index.tsx
+++ src/screens/signup/otp/index.tsx
@@ -1,5 +1,11 @@
-import React, { useState } from 'react';
-import { Button, ProgressBar, Title, Paragraph } from 'react-native-paper';
+import React, { useState, Fragment } from 'react';
+import {
+  Button,
+  ProgressBar,
+  Title,
+  Paragraph,
+  Snackbar
+} from 'react-native-paper';
 import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useMutation, useQuery } from '@apollo/react-hooks';
@@ -21,6 +27,7 @@ import { NavigationInterface } from '../../types';
 import { DEVICE_ID } from '../../../utils/device';
 import { useThemeContext } from '../../../theme';
 import useCountDown from './useCountDown';
+import Storage from '../../../storage';
 
 // IMPORT FOR ALL CUSTOM STYLES
 import { Container } from './styles';
@@ -37,7 +44,11 @@ export default function OTPScreen(props: ScreenProp) {
   const { colors, fonts } = useThemeContext();
   const { t } = useTranslation();
 
-  const [state, setState] = useState({ otp: '', loading: false });
+  const [state, setState] = useState({
+    otp: '',
+    inputError: false,
+    loading: false
+  });
 
   const [sendOtp] = useMutation<OTPInterface>(SEND_USER_OTP, {
     variables: {
@@ -60,162 +71,173 @@ export default function OTPScreen(props: ScreenProp) {
     restartOtpTimer();
   };
 
-  const handleSubmit = () => {
-    if (!state.otp || state.loading) return;
-    setState({ ...state, loading: true });
+  const handleInputError = () => {
+    setState({ ...state, inputError: !state.inputError });
+  };
+
+  const handleSubmit = (sentOTP: string) => {
+    if (!sentOTP || state.loading) return handleInputError();
+    setState({ ...state, otp: sentOTP, loading: true });
 
     setTimeout(async () => {
-      // const response = await verifyOtp();
-
-      // AsyncStorage.setItem(
-      //   USER_AUTH_KEYS,
-      //   JSON.stringify(response.data?.validateOtp)
-      // );
+      const response = await verifyOtp();
+      Storage.addUserAuth(response.data?.validateOtp);
 
       setState({ ...state, loading: false });
       navigation.reset({ index: 0, routes: [{ name: 'CreateAccountScreen' }] });
-    }, 3000);
+    }, 0);
   };
+
+
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <Container
-        style={{
-          height: '100%',
-          paddingLeft: RFValue(20),
-          paddingRight: RFValue(20)
-        }}
-      >
-        <ProgressBar
-          progress={1 / 5}
-          color={colors.PRIMARY}
-          style={{
-            height: RFValue(5),
-            backgroundColor: '#F2F2F7',
-            borderRadius: 4,
-            marginBottom: RFValue(30)
-          }}
-        />
-
-        <Title
-          style={{
-            fontFamily: fonts.WORK_SANS_SEMI_BOLD,
-            fontSize: RFValue(Math.ceil(fonts.LARGE_SIZE)),
-            color: colors.PRIMARY,
-            textTransform: 'capitalize',
-            lineHeight: RFValue(30)
-          }}
-        >
-          {t(`signup.screenThree.subTitle`)}
-        </Title>
-
-        <Title
-          style={{
-            fontFamily: fonts.WORK_SANS_BOLD,
-            fontSize: RFValue(Math.ceil(fonts.LARGE_SIZE * 1.6)),
-            color: colors.PRIMARY_TEXT,
-            lineHeight: RFValue(30),
-            marginTop: 20
-          }}
-        >
-          {t(`signup.screenThree.title`)}
-        </Title>
-
-        <Paragraph
-          style={{
-            fontFamily: fonts.WORK_SANS_REGULAR,
-            fontSize: RFValue(fonts.LARGE_SIZE),
-            color: colors.SECONDARY_TEXT,
-            lineHeight: RFValue(22)
-          }}
-        >
-          {t(`signup.screenThree.paragraph`) + ' (305) 408-1935.'}
-        </Paragraph>
-
-        <Container style={{ height: RFValue(100), marginTop: 10 }}>
-          <OTPInputView
-            pinCount={4}
-            autoFocusOnLoad={true}
-            keyboardType="number-pad"
-            placeholderCharacter="0"
-            onCodeChanged={(otp) => setState({ ...state, otp })}
-            onCodeFilled={handleSubmit}
-            codeInputFieldStyle={{
-              width: RFValue(65),
-              height: RFValue(70),
-              fontSize: RFValue(fonts.LARGE_SIZE * 2),
-              color: colors.PRIMARY_TEXT,
-              fontFamily: fonts.WORK_SANS_BOLD,
-              borderWidth: 1.2,
-              borderRadius: 4,
-              borderColor: colors.INACTIVE
-            }}
-            placeholderTextColor={colors.DISABLED}
-          />
-        </Container>
+      <Fragment>
         <Container
           style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center'
+            height: '100%',
+            paddingLeft: RFValue(20),
+            paddingRight: RFValue(20)
           }}
         >
+          <ProgressBar
+            progress={1 / 5}
+            color={colors.PRIMARY}
+            style={{
+              height: RFValue(5),
+              backgroundColor: '#F2F2F7',
+              borderRadius: 4,
+              marginBottom: RFValue(30)
+            }}
+          />
+
+          <Title
+            style={{
+              fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+              fontSize: RFValue(Math.ceil(fonts.LARGE_SIZE)),
+              color: colors.PRIMARY,
+              textTransform: 'capitalize',
+              lineHeight: RFValue(30)
+            }}
+          >
+            {t(`signup.OTPScreen.subTitle`)}
+          </Title>
+
+          <Title
+            style={{
+              fontFamily: fonts.WORK_SANS_BOLD,
+              fontSize: RFValue(Math.ceil(fonts.LARGE_SIZE * 1.6)),
+              color: colors.PRIMARY_TEXT,
+              lineHeight: RFValue(30),
+              marginTop: 20
+            }}
+          >
+            {t(`signup.OTPScreen.title`)}
+          </Title>
+
           <Paragraph
             style={{
               fontFamily: fonts.WORK_SANS_REGULAR,
               fontSize: RFValue(fonts.LARGE_SIZE),
-              color: colors.SECONDARY_TEXT
+              color: colors.SECONDARY_TEXT,
+              lineHeight: RFValue(22)
             }}
           >
-            {t(`signup.screenThree.didNotReceiveCode`)}
+            {t(`signup.OTPScreen.paragraph`) + ' (305) 408-1935.'}
           </Paragraph>
 
-          {timeLeft ? (
-            <Title
+          <Container style={{ height: RFValue(100), marginTop: 10 }}>
+            <OTPInputView
+              pinCount={4}
+              autoFocusOnLoad={true}
+              keyboardType="number-pad"
+              placeholderCharacter="0"
+              onCodeFilled={handleSubmit}
+              codeInputFieldStyle={{
+                width: RFValue(65),
+                height: RFValue(70),
+                fontSize: RFValue(fonts.LARGE_SIZE * 2),
+                color: colors.PRIMARY_TEXT,
+                fontFamily: fonts.WORK_SANS_BOLD,
+                borderWidth: 1.2,
+                borderRadius: 4,
+                borderColor: colors.INACTIVE
+              }}
+              placeholderTextColor={colors.DISABLED}
+            />
+          </Container>
+          <Container
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <Paragraph
               style={{
-                fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                fontFamily: fonts.WORK_SANS_REGULAR,
                 fontSize: RFValue(fonts.LARGE_SIZE),
-                color: colors.PRIMARY_TEXT,
-                marginLeft: 5,
-                padding: 5,
-                paddingTop: 2
+                color: colors.SECONDARY_TEXT
               }}
             >
-              {timeLeft}
-            </Title>
-          ) : (
-            <Button
-              mode="text"
-              color={colors.WHITE}
-              uppercase={false}
-              onPress={resendOtp}
-              labelStyle={{
-                fontFamily: fonts.WORK_SANS_SEMI_BOLD,
-                fontSize: RFValue(fonts.LARGE_SIZE),
-                color: colors.PRIMARY_TEXT,
-                marginLeft: 5,
-                padding: 5,
-                paddingTop: 2,
-                textTransform: 'capitalize'
-              }}
-            >
-              {t(`signup.screenThree.resend`)}
-            </Button>
-          )}
-        </Container>
+              {t(`signup.OTPScreen.didNotReceiveCode`)}
+            </Paragraph>
 
-        <Container
-          style={{
-            flex: 1,
-            justifyContent: 'flex-end',
-            paddingBottom: RFValue(safeAreaBottom + 60)
-          }}
-        >
-          <GradientButton loading={state.loading} onPress={handleSubmit}>
-            {t(`signup.screenThree.${state.loading ? 'loading' : 'submit'}`)}
-          </GradientButton>
+            {timeLeft ? (
+              <Title
+                style={{
+                  fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                  fontSize: RFValue(fonts.LARGE_SIZE),
+                  color: colors.PRIMARY_TEXT,
+                  marginLeft: 5,
+                  padding: 5,
+                  paddingTop: 2
+                }}
+              >
+                {timeLeft}
+              </Title>
+            ) : (
+              <Button
+                mode="text"
+                color={colors.WHITE}
+                uppercase={false}
+                onPress={resendOtp}
+                labelStyle={{
+                  fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                  fontSize: RFValue(fonts.LARGE_SIZE),
+                  color: colors.PRIMARY_TEXT,
+                  marginLeft: 5,
+                  padding: 5,
+                  paddingTop: 2,
+                  textTransform: 'capitalize'
+                }}
+              >
+                {t(`signup.OTPScreen.resend`)}
+              </Button>
+            )}
+          </Container>
+
+          <Container
+            style={{
+              flex: 1,
+              justifyContent: 'flex-end',
+              paddingBottom: RFValue(safeAreaBottom + 60)
+            }}
+          >
+            <GradientButton loading={state.loading} onPress={handleSubmit}>
+              {t(`signup.OTPScreen.${state.loading ? 'loading' : 'submit'}`)}
+            </GradientButton>
+          </Container>
         </Container>
-      </Container>
+        <Snackbar
+          duration={Snackbar.DURATION_SHORT}
+          visible={state.inputError}
+          onDismiss={handleInputError}
+          action={{ label: 'Dismiss', onPress: handleInputError }}
+        >
+          {t(`signup.OTPScreen.inputError`)}
+        </Snackbar>
+      </Fragment>
     </TouchableWithoutFeedback>
   );
 }
