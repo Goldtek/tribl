06fed5d5532f70176eb5cd732ee50763d4a75178
josemak25 <amakirij@gmail.com>
2020-07-23 00:44:37 +0100

    use bottom lables for bottom navigator from community eng locale file

--- src/navigator/communityNavigator/index.tsx
+++ src/navigator/communityNavigator/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Screens from '../../screens';
+import { useTranslation } from 'react-i18next';
+import { useThemeContext } from '../../theme';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 const CommunityStack = createStackNavigator();
 
 export default function CommunityNavigator() {
+  const { t } = useTranslation();
+  const { fonts } = useThemeContext();
+
   return (
     <CommunityStack.Navigator
       initialRouteName="CommunityScreen"
       headerMode="screen"
+      screenOptions={{
+        headerTitleStyle: {
+          fontFamily: fonts.WORK_SANS_BOLD,
+          fontSize: RFValue(fonts.LARGE_SIZE * 1.2),
+          textTransform: 'capitalize'
+        }
+      }}
     >
       <CommunityStack.Screen
         name="CommunityScreen"
         component={Screens.CommunityScreen}
-        options={{ headerTitle: 'community' }}
+        options={{ headerTitle: t(`community.headerTitle`) }}
       />
     </CommunityStack.Navigator>
   );
