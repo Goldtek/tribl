76c4b04b90932599c5d7640eef917fe7b4772d7c
josemak25 <amakirij@gmail.com>
2020-07-23 00:43:55 +0100

    add community eng locale translation to default internationalization export file

--- src/internationalization/locales/en/index.ts
+++ src/internationalization/locales/en/index.ts
@@ -1,11 +1,13 @@
 import walkThrough from './walkThrough.json';
 import signup from './signup.json';
+import community from './community.json';
 
 export default {
   en: {
     translation: {
       signup,
       walkThrough,
+      community,
       change_language_english: 'In english',
       change_language_french: 'In french'
     }
