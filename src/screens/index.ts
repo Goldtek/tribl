3c41f0826b018321b9ca4ca131e278507fa80fc1
josemak25 <amakirij@gmail.com>
2020-07-23 00:26:35 +0100

    remove tribl screen from screen default export

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,12 +2,10 @@ import WalkThroughScreen from './walkThrough';
 import CommunityScreen from './community';
 import ProfileScreen from './profile';
 import InboxScreen from './inbox';
-import TriblScreen from './tribl';
 
 export default {
   WalkThroughScreen,
   CommunityScreen,
   ProfileScreen,
-  InboxScreen,
-  TriblScreen
+  InboxScreen
 };
