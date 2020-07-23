be19d53882249e638faf27d51cd31b5210c3ff61
josemak25 <amakirij@gmail.com>
2020-07-23 23:43:07 +0100

    add code push configs and fixed app native module crashes

--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import { enableScreens } from 'react-native-screens';
 import loadResources from './src/libs/loadResources';
+import codePush from 'react-native-code-push';
 import AppRouter from './src';
 import './src/internationalization';
 
 enableScreens();
 
-export default function App() {
+function App() {
   const [isAppReady, setIsAppReady] = useState(false);
 
   useEffect(() => {
@@ -21,3 +22,30 @@ export default function App() {
 
   return isAppReady ? <AppRouter /> : null;
 }
+
+// Prompt the user when an update is available
+// and then display a "downloading" modal
+codePush.sync(
+  {},
+  (status) => {
+    switch (status) {
+      case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+        // Show "downloading" modal
+        break;
+      case codePush.SyncStatus.INSTALLING_UPDATE:
+        // Hide "downloading" modal
+        break;
+    }
+  },
+  ({ receivedBytes, totalBytes }) => {
+    /* Update download modal progress */
+    console.log({ receivedBytes, totalBytes });
+  }
+);
+
+export default codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  updateDialog: { title: 'An update is available!' },
+  appendReleaseDescription: true,
+  installMode: codePush.InstallMode.ON_NEXT_RESUME
+})(App);
