b202074fbb9f44e07a62ea9dedc43eff94024ea2
josemak25 <amakirij@gmail.com>
2020-07-24 16:37:38 +0100

    add recycle list view layout provider

--- src/utils/LayoutUtil.ts
+++ src/utils/LayoutUtil.ts
@@ -8,6 +8,13 @@ import { DEVICE_FULL_WIDTH } from './device';
  *****************************************************************************
  */
 
+export const ViewTypes = {
+  COMMUNITY_HEADER: 'COMMUNITY_HEADER',
+  RECOMMENDED_MEMBER: 'RECOMMENDED_MEMBER',
+  RECOMMENDED_COMMUNITY: 'RECOMMENDED_COMMUNITY',
+  COMMUNITY_ACTIVITY: 'COMMUNITY_ACTIVITY'
+};
+
 // COUNTRY LIST VIEW PROVIDER
 export const getCountryLayout = () => {
   return new LayoutProvider(
@@ -18,3 +25,49 @@ export const getCountryLayout = () => {
     }
   );
 };
+
+// COMMUNITY SCREEN RECOMMENDED MEMBERS & COMMUNITIES LIST VIEW PROVIDER
+export default function layoutProvider() {
+  return new LayoutProvider(
+    (index) => {
+      switch (index) {
+        case 0:
+          return ViewTypes.COMMUNITY_HEADER;
+
+        case 1:
+          return ViewTypes.RECOMMENDED_MEMBER;
+
+        case 2:
+          return ViewTypes.RECOMMENDED_COMMUNITY;
+
+        case 3:
+          return ViewTypes.COMMUNITY_HEADER;
+
+        default:
+          return index;
+      }
+    },
+    (type, dim) => {
+      switch (type) {
+        case ViewTypes.COMMUNITY_HEADER:
+          dim.width = RFValue(Math.round(DEVICE_FULL_WIDTH));
+          dim.height = RFValue(100);
+          break;
+
+        case ViewTypes.RECOMMENDED_MEMBER:
+          dim.width = RFValue(Math.round(DEVICE_FULL_WIDTH));
+          dim.height = RFValue(200);
+          break;
+
+        case ViewTypes.RECOMMENDED_COMMUNITY:
+          dim.width = RFValue(Math.round(DEVICE_FULL_WIDTH));
+          dim.height = RFValue(300);
+          break;
+
+        default:
+          dim.width = 0;
+          dim.height = 0;
+      }
+    }
+  );
+}
