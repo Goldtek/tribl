e580c2fa3b5f2083f53c0f1003e8bf1906fb51c7
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-26 23:59:04 +0100

    remove textContaner, cardContainer

--- src/components/popularCommunity/styles.ts
+++ src/components/popularCommunity/styles.ts
@@ -1,19 +1,10 @@
 import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
-export const CommunityContainer = styled.View`
-  flex: 1;
-  flex-direction: row;
-`;
-
 export const TextConatiner = styled.View`
-  padding-left: ${RFValue(10)}px;
-  align-self: center;
+  flex: 1;
+  height: 100%;
+  margin-left: ${RFValue(10)}px;
+  justify-content: flex-end;
+  padding-bottom: ${RFValue(5)}px;
 `;
-
-export const CardContainer = styled.View`
-  height: ${RFValue(100)}px;
-  background-color: ${({ theme }) => theme.colors.OFFWHITE};
-`;
-
-export const CardContent = styled.View``;
