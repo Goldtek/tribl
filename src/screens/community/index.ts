096c68ebb30217df76f1a3dadcc56b4f6d4bdda4
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-27 00:02:13 +0100

    change singlecommunityscreen to communitydetailscreen

--- src/screens/community/index.ts
+++ src/screens/community/index.ts
@@ -1,9 +1,9 @@
 import HomeScreen from './home';
 import SearchScreen from './search';
-import SingleCommunityScreen from './communityDetails';
+import CommunityDetailScreen from './detail';
 
 export default {
   HomeScreen,
   SearchScreen,
-  SingleCommunityScreen
+  CommunityDetailScreen
 };
