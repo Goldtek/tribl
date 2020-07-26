643f5d30cc9e7914a6ca8a797d53196c95cbed2a
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-26 10:27:43 +0100

    create community details screen

---
+++ src/screens/community/communityDetails/index.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import { NavigationInterface } from '../../types';
+import { Card, Title, Paragraph } from 'react-native-paper';
+import { ScrollView } from 'react-native';
+import FastImage from 'react-native-fast-image';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useThemeContext } from '../../../theme';
+import MembersCard from '../../../components/recommendedUser';
+import MembersData from '../../../libs/recommendedUsers/index.json';
+import { Container, CardContainer, TextContainer } from './styles';
+
+interface SingleCommunityScreenProp extends NavigationInterface {}
+
+export default function SingleCommunity(props: SingleCommunityScreenProp) {
+  const { colors, fonts } = useThemeContext();
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <Container>
+        <Card style={{ marginTop: RFValue(10) }}>
+          <Card.Content>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              source={{
+                uri: 'https://www.linkpicture.com/q/Rectangle-159.png',
+                priority: FastImage.priority.high
+              }}
+              style={{
+                width: '100%',
+                height: RFValue(100)
+              }}
+            />
+          </Card.Content>
+        </Card>
+        <Card style={{ marginTop: RFValue(10) }}>
+          <CardContainer>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              source={{
+                uri: 'https://www.linkpicture.com/q/Rectangle-159.png',
+                priority: FastImage.priority.high
+              }}
+              style={{
+                width: RFValue(100),
+                height: RFValue(100)
+              }}
+            />
+            <TextContainer>
+              <Title
+                style={{
+                  fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                  fontSize: fonts.LARGE_SIZE,
+                  textTransform: 'capitalize',
+                  lineHeight: RFValue(19)
+                }}
+              >
+                black lives matter
+              </Title>
+              <Paragraph
+                style={{
+                  fontSize: fonts.MEDIUM_SIZE - 1,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  lineHeight: RFValue(13),
+                  color: colors.DARK_TEXT
+                }}
+              >
+                100k members
+              </Paragraph>
+              <Paragraph
+                style={{
+                  fontSize: fonts.MEDIUM_SIZE - 1,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  lineHeight: RFValue(13),
+                  color: colors.PRIMARY_TEXT
+                }}
+              >
+                We are a global community of block migrants and locals looking
+                to make connections
+              </Paragraph>
+            </TextContainer>
+            <Paragraph
+              style={{
+                marginLeft: 'auto',
+                marginRight: RFValue(10),
+                color: colors.PRIMARY,
+                fontSize: fonts.MEDIUM_SIZE,
+                fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                alignSelf: 'flex-start',
+                textTransform: 'uppercase',
+                marginTop: RFValue(20)
+              }}
+            >
+              join
+            </Paragraph>
+          </CardContainer>
+        </Card>
+        <Card style={{ marginTop: RFValue(10) }}>
+          <Card.Content>
+            <Title
+              style={{
+                color: colors.PRIMARY_TEXT,
+                fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                fontSize: fonts.LARGE_SIZE + 1,
+                textTransform: 'capitalize'
+              }}
+            >
+              recommended members
+            </Title>
+            <ScrollView
+              horizontal={true}
+              alwaysBounceHorizontal={false}
+              showsHorizontalScrollIndicator={false}
+              style={{ marginTop: RFValue(15), paddingLeft: RFValue(10) }}
+            >
+              {MembersData.map((members, index) => (
+                <MembersCard
+                  key={index}
+                  avatar={members.avatar}
+                  name={members.name}
+                  address={members.address}
+                />
+              ))}
+            </ScrollView>
+          </Card.Content>
+        </Card>
+      </Container>
+    </ScrollView>
+  );
+}
