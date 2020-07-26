45e6cc300c2e273fc969ff0c212c5225aedca9f5
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-27 00:04:58 +0100

    restyle community slide

--- src/screens/community/search/widgets/communitySlide/styles.ts
+++ src/screens/community/search/widgets/communitySlide/styles.ts
@@ -3,30 +3,19 @@ import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-  padding-top: ${RFValue(30)}px;
-  height: 100%;
+  padding-top: ${RFValue(20)}px;
 `;
 
 export const CommunityWrapper = styled.View`
-  flex: 1;
   flex-direction: row;
-`;
-
-export const TitleWrapper = styled.View`
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: ${RFValue(10)}px;
   padding-left: ${RFValue(15)}px;
-  align-self: center;
-`;
-
-export const ViewWrapper = styled.View`
-  flex-direction: row;
-  align-self: center;
-  margin-left: auto;
 `;
 
 export const PopularContainer = styled.View`
   flex: 1;
   flex-direction: column;
-  padding-left: ${RFValue(25)}px;
-  padding-right: ${RFValue(10)}px;
   padding-top: ${RFValue(20)}px;
 `;
