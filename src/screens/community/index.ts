81b9a342b051fd479f74426e8cc17c991a4ac910
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-26 10:26:56 +0100

    add singleCommunityScreen t community screen

--- src/screens/community/index.ts
+++ src/screens/community/index.ts
@@ -1,7 +1,9 @@
 import HomeScreen from './home';
 import SearchScreen from './search';
+import SingleCommunityScreen from './communityDetails';
 
 export default {
   HomeScreen,
-  SearchScreen
+  SearchScreen,
+  SingleCommunityScreen
 };
