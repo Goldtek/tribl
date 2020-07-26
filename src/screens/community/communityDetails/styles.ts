916f97ec29c7f90a21eb7ee0d70139e0ad0931be
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-26 10:28:12 +0100

    style community details screen

---
+++ src/screens/community/communityDetails/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const CardContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const TextContainer = styled.View`
+  width: ${RFValue(200)}px;
+`;
