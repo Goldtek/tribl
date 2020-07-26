0be1c8a8a5975e7d8ba9b7e5df74c5dbff649e5e
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-27 00:04:17 +0100

    restyle search screen

--- src/screens/community/search/styles.ts
+++ src/screens/community/search/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${({ theme }) => theme.colors.OFFWHITE};
-  height: 100%;
+  padding-top: ${RFValue(20)}px;
+  background-color: ${({ theme }) => theme.colors.GREY};
 `;
