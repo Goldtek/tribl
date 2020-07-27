8438a6494542db44310f839cc0e00c36e2f57ac5
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-27 14:13:05 +0100

    add memberdetailscreen to community screen

--- src/screens/community/index.ts
+++ src/screens/community/index.ts
@@ -1,9 +1,11 @@
 import HomeScreen from './home';
 import SearchScreen from './search';
 import CommunityDetailScreen from './detail';
+import MemberDetailScreen from './memberPassport';
 
 export default {
   HomeScreen,
   SearchScreen,
-  CommunityDetailScreen
+  CommunityDetailScreen,
+  MemberDetailScreen
 };
