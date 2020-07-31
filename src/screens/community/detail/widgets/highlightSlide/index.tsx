b66098a5b724ebe802a3d3f2210ad58a0f6f5aaa
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-31 08:04:55 +0100

    create tab highlight slide

---
+++ src/screens/community/detail/widgets/highlightSlide/index.tsx
@@ -0,0 +1,162 @@
+import React from 'react';
+import { NavigationInterface } from '../../../../types';
+import { Card, Title, Paragraph, Button } from 'react-native-paper';
+import { useTranslation } from 'react-i18next';
+import { ScrollView } from 'react-native';
+import FastImage from 'react-native-fast-image';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useThemeContext } from '../../../../../theme';
+import MembersCard from '../../../../../components/recommendedUser';
+import MembersData from '../../../../../libs/recommendedUsers/index.json';
+import TagData from '../../../../../libs/tags/index.json';
+
+import {
+  Container,
+  CardContainer,
+  TextContainer,
+  JoinContainer,
+  TagContainer,
+  Tags,
+  TagText
+} from './styles';
+
+interface SingleCommunityScreenProp extends NavigationInterface {}
+
+export default function SingleCommunity(props: SingleCommunityScreenProp) {
+  const { colors, fonts } = useThemeContext();
+  const { t } = useTranslation();
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <Container>
+        <Card style={{ marginTop: RFValue(5) }}>
+          <Card.Content>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              source={{
+                uri: 'https://www.linkpicture.com/q/Rectangle-159.png',
+                priority: FastImage.priority.high
+              }}
+              style={{
+                width: '100%',
+                height: RFValue(100)
+              }}
+            />
+          </Card.Content>
+        </Card>
+        <Card style={{ marginTop: RFValue(5) }}>
+          <CardContainer>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              source={{
+                uri: 'https://www.linkpicture.com/q/Rectangle-159.png',
+                priority: FastImage.priority.high
+              }}
+              style={{
+                width: RFValue(100),
+                height: RFValue(100)
+              }}
+            />
+            <TextContainer>
+              <Title
+                style={{
+                  fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                  fontSize: fonts.LARGE_SIZE,
+                  textTransform: 'capitalize',
+                  lineHeight: RFValue(19)
+                }}
+              >
+                black lives matter
+              </Title>
+              <Paragraph
+                style={{
+                  fontSize: fonts.MEDIUM_SIZE - 1,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  lineHeight: RFValue(13),
+                  color: colors.SECONDARY_TEXT
+                }}
+              >
+                100k {t(`community.tabPanel.member`)}
+              </Paragraph>
+              <Paragraph
+                style={{
+                  fontSize: fonts.MEDIUM_SIZE - 1,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  lineHeight: RFValue(13),
+                  color: colors.PRIMARY_TEXT
+                }}
+              >
+                We are a global community of block migrants and locals looking
+                to make connections
+              </Paragraph>
+            </TextContainer>
+            <JoinContainer>
+              <Paragraph
+                style={{
+                  color: colors.WHITE,
+                  fontSize: fonts.MEDIUM_SIZE,
+                  fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                  textTransform: 'uppercase'
+                }}
+              >
+                {t(`community.tabPanel.join`)}
+              </Paragraph>
+            </JoinContainer>
+          </CardContainer>
+          <TagContainer>
+            <Title
+              style={{
+                fontFamily: fonts.WORK_SANS_BOLD,
+                fontSize: RFValue(fonts.MEDIUM_SIZE),
+                color: colors.PRIMARY_TEXT,
+                textTransform: 'uppercase'
+              }}
+            >
+              {t(`community.tabPanel.tag`)}
+            </Title>
+
+            <Tags>
+              {TagData.map((identity) => (
+                <TagText key={identity}>{identity}</TagText>
+              ))}
+            </Tags>
+          </TagContainer>
+        </Card>
+        <Card style={{ marginTop: RFValue(5) }}>
+          <Card.Content style={{ paddingLeft: 0 }}>
+            <Title
+              style={{
+                fontFamily: fonts.WORK_SANS_BOLD,
+                fontSize: RFValue(fonts.LARGE_SIZE),
+                color: colors.PRIMARY_TEXT,
+                textTransform: 'capitalize',
+                marginTop: 0,
+                marginBottom: 0,
+                paddingLeft: 15
+              }}
+            >
+              {t(`community.tabPanel.nearby`)}
+            </Title>
+            <ScrollView
+              horizontal={true}
+              alwaysBounceHorizontal={false}
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{ marginTop: RFValue(15) }}
+            >
+              {MembersData.map((members, index) => (
+                <MembersCard
+                  key={index}
+                  index={index}
+                  lastChild={MembersData.length - 1}
+                  avatar={members.avatar}
+                  name={members.name}
+                  address={members.address}
+                />
+              ))}
+            </ScrollView>
+          </Card.Content>
+        </Card>
+      </Container>
+    </ScrollView>
+  );
+}
