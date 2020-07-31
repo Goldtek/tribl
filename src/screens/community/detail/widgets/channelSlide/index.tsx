3cd20e5e934f34de76d2eb2896e4731581ccbed2
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-31 14:33:58 +0100

    refactor code

--- src/screens/community/detail/widgets/channelSlide/index.tsx
+++ src/screens/community/detail/widgets/channelSlide/index.tsx
@@ -1,19 +1,13 @@
-import React, { Fragment } from 'react';
-import { FlatList, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { FlatList } from 'react-native';
 import { NavigationInterface } from '../../../../types';
-import { Paragraph, Divider } from 'react-native-paper';
-import { AntDesign } from '@expo/vector-icons';
-import { useThemeContext } from '../../../../../theme';
-import { RFValue } from 'react-native-responsive-fontsize';
 import ChannelData from '../../../../../libs/channels/index.json';
-import RenderItems from './widget';
+import RenderItems from './widget/channel';
 
 // DEFINE SCREEN PROP TYPES
 interface ScreenProp extends NavigationInterface {}
 
 export default function ChannelScreen(props: ScreenProp) {
-  const { colors, fonts } = useThemeContext();
-
   return (
     <FlatList
       renderItem={({ item, index }) => (
