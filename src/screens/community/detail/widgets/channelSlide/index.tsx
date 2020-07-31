ab0e5e9a6b1c3403cf9cdeb1850dfa4c2d2c2782
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-07-31 10:36:26 +0100

    refactor code

--- src/screens/community/detail/widgets/channelSlide/index.tsx
+++ src/screens/community/detail/widgets/channelSlide/index.tsx
@@ -6,6 +6,7 @@ import { AntDesign } from '@expo/vector-icons';
 import { useThemeContext } from '../../../../../theme';
 import { RFValue } from 'react-native-responsive-fontsize';
 import ChannelData from '../../../../../libs/channels/index.json';
+import RenderItems from './widget';
 
 // DEFINE SCREEN PROP TYPES
 interface ScreenProp extends NavigationInterface {}
@@ -13,48 +14,10 @@ interface ScreenProp extends NavigationInterface {}
 export default function ChannelScreen(props: ScreenProp) {
   const { colors, fonts } = useThemeContext();
 
-  const RenderItems = (props: any) => {
-    const { name } = props;
-    return (
-      <Fragment>
-        <TouchableOpacity
-          onPress={() => {}}
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            paddingTop: RFValue(25),
-            paddingBottom: RFValue(25),
-            padding: RFValue(20),
-            backgroundColor: colors.WHITE
-          }}
-        >
-          <Paragraph
-            style={{
-              fontSize: RFValue(fonts.LARGE_SIZE),
-              fontFamily: fonts.WORK_SANS_REGULAR,
-              lineHeight: RFValue(16),
-              color: colors.PRIMARY_TEXT
-            }}
-          >
-            {name}
-          </Paragraph>
-          <AntDesign name="caretright" size={18} color={colors.PRIMARY_TEXT} />
-        </TouchableOpacity>
-        <Divider />
-      </Fragment>
-    );
-  };
-
   return (
     <FlatList
-      renderItem={({ item, index, separators }) => (
-        <RenderItems
-          key={index}
-          onShowUnderlay={separators.highlight}
-          onHideUnderlay={separators.unhighlight}
-          {...item}
-          {...props}
-        />
+      renderItem={({ item, index }) => (
+        <RenderItems key={index} {...item} {...props} />
       )}
       data={ChannelData}
       keyExtractor={(_item, index) => index.toString()}
