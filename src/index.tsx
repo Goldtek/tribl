3705968a62d6221d0a406d11d11d8cd7a46e3ba2
josemak25 <amakirij@gmail.com>
2020-08-02 18:20:15 +0100

    add linking on react native lottie view

--- src/index.tsx
+++ src/index.tsx
@@ -11,41 +11,41 @@ import Router from './router';
 export default function AppRouter() {
   useEffect(() => {
     (async () => {
-      // const update = await Updates.checkForUpdateAsync();
-      // if (update.isAvailable) {
-      //   await Updates.fetchUpdateAsync();
-      //   Alert.alert('UPDATE AVAILABLE', 'DOWNLOADING UPDATES NOW...');
-      //   // ... notify user of update ...
-      //   await Updates.reloadAsync();
-      // }
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        Alert.alert('UPDATE AVAILABLE', 'DOWNLOADING UPDATES NOW...');
+        // ... notify user of update ...
+        await Updates.reloadAsync();
+      }
       // Prompt the user when an update is available
       // and then display a "downloading" modal
     })();
 
-    codePush.sync(
-      {
-        deploymentKey: '23b6df88-75df-4a81-be10-dbb5798089f3',
-        updateDialog: { title: 'An update is available!' },
-        installMode: codePush.InstallMode.IMMEDIATE
-      },
-      (status) => {
-        switch (status) {
-          case codePush.SyncStatus.DOWNLOADING_PACKAGE:
-            // Show "downloading" modal
-            Alert.alert('DOWNLOADING_PACKAGE', JSON.stringify(status));
+    // codePush.sync(
+    //   {
+    //     deploymentKey: '23b6df88-75df-4a81-be10-dbb5798089f3',
+    //     updateDialog: { title: 'An update is available!' },
+    //     installMode: codePush.InstallMode.IMMEDIATE
+    //   },
+    //   (status) => {
+    //     switch (status) {
+    //       case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+    //         // Show "downloading" modal
+    //         Alert.alert('DOWNLOADING_PACKAGE', JSON.stringify(status));
 
-            break;
-          case codePush.SyncStatus.INSTALLING_UPDATE:
-            // Hide "downloading" modal
-            Alert.alert('INSTALLING_UPDATE', JSON.stringify(status));
-            break;
-        }
-      },
-      ({ receivedBytes, totalBytes }) => {
-        /* Update download modal progress */
-        console.log({ receivedBytes, totalBytes });
-      }
-    );
+    //         break;
+    //       case codePush.SyncStatus.INSTALLING_UPDATE:
+    //         // Hide "downloading" modal
+    //         Alert.alert('INSTALLING_UPDATE', JSON.stringify(status));
+    //         break;
+    //     }
+    //   },
+    //   ({ receivedBytes, totalBytes }) => {
+    //     /* Update download modal progress */
+    //     console.log({ receivedBytes, totalBytes });
+    //   }
+    // );
   }, []);
 
   return (
