3d8d049399731eeb4e7f104e4ede79614be8d6e4
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-05 12:31:23 +0100

    craete and style new message screen

---
+++ src/screens/inbox/newMessage/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationInterface } from '../../types';
+import { Button } from 'react-native-paper';
+
+// IMPORT FOR ALL CUSTOM STYLES
+import { Container, Welcome } from './styles';
+
+// DEFINE SCREEN PROP TYPES
+interface ScreenProp extends NavigationInterface {}
+
+export default function NewMessageScreen(props: ScreenProp) {
+  return (
+    <Container>
+      <Button
+        icon="camera"
+        mode="contained"
+        onPress={() => console.log('Pressed')}
+      >
+        Press me
+      </Button>
+      <Welcome>New Message Screen</Welcome>
+    </Container>
+  );
+}
