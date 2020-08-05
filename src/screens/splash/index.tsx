466daa648dfea715ba83652c183370df4532ab05
josemak25 <amakirij@gmail.com>
2020-08-05 22:41:12 +0100

    add splash screen component and styles and also fixed check on user first time luanch calcualtions

---
+++ src/screens/splash/index.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { NavigationInterface } from '../types';
+import Storage from '../../storage';
+
+// IMPORT FOR ALL CUSTOM STYLES
+import { Container } from './styles';
+
+// DEFINE SCREEN PROP TYPES
+interface ScreenProp extends NavigationInterface {}
+
+export default function SplashScreen(props: ScreenProp) {
+  const { navigation } = props;
+
+  useEffect(() => {
+    (async () => {
+      const initialLaunch = await Storage.checkInitialLaunch();
+      if (!initialLaunch) {
+        navigation.replace('SignupScreen');
+      }
+    })();
+  }, []);
+
+  return <Container />;
+}
