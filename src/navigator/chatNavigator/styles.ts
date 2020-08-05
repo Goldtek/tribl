4000716e568dabd0eb4faa87fe5323175d58075d
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-05 12:14:35 +0100

    style chat navigator

---
+++ src/navigator/chatNavigator/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const IconContainer = styled.View`
+  flex: 1;
+  width: ${RFValue(100)}px;
+  height: ${RFValue(100)}px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Label = styled.Text`
+  font-size: ${({ theme }) => RFValue(theme.fonts.MEDIUM_SIZE - 2)}px;
+  font-family: ${({ theme }) => theme.fonts.WORK_SANS_SEMI_BOLD};
+  color: ${({ theme }) => theme.colors.SECONDARY_TEXT};
+  padding-top: 3px;
+  text-align: center;
+  text-transform: capitalize;
+`;
