d00f50e6d58839ba5611acab38db16d977f101db
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-05 12:16:03 +0100

    add inbox screen to screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,9 +1,7 @@
 import WalkThroughScreen from './walkThrough';
 import PassportScreen from './passport';
-import InboxScreen from './inbox';
 
 export default {
   WalkThroughScreen,
-  PassportScreen,
-  InboxScreen
+  PassportScreen
 };
