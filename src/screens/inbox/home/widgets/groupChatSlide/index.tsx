de9707c1d753ef509b6172897f8f9ed2c75c5b96
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-05 12:29:39 +0100

    craete and style group chat slide

---
+++ src/screens/inbox/home/widgets/groupChatSlide/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import { NavigationInterface } from '../../../../types';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useThemeContext } from '../../../../../theme';
+import MemberCard from '../chatMemberCard';
+import MembersData from '../../../../../libs/memberChat/index.json';
+
+// DEFINE SCREEN PROP TYPES
+interface ScreenProp extends NavigationInterface { }
+
+export default function ChannelScreen(props: ScreenProp) {
+  const { navigation } = props;
+
+  const { colors, fonts } = useThemeContext();
+
+  const _renderItem = ({ item }: any) => (
+    <MemberCard key={item.id} {...item} {...props} />
+  );
+
+  return (
+    <FlatList
+      data={MembersData}
+      contentContainerStyle={{
+        flexGrow: 1,
+        marginTop: RFValue(20),
+        paddingBottom: RFValue(120)
+      }}
+      showsVerticalScrollIndicator={false}
+      renderItem={_renderItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+}
