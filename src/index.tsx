5104fe2d608a32d4e700e7414ac8e5bad3b7de92
josemak25 <amakirij@gmail.com>
2020-08-05 22:48:36 +0100

    add root toaster and react native potal as HOC to app base route

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import * as Updates from 'expo-updates';
-import codePush from 'react-native-code-push';
+import { RootToaster } from './components/rootToaster';
+import { Host } from 'react-native-portalize';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Alert } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
@@ -21,31 +22,6 @@ export default function AppRouter() {
       // Prompt the user when an update is available
       // and then display a "downloading" modal
     })();
-
-    // codePush.sync(
-    //   {
-    //     deploymentKey: '23b6df88-75df-4a81-be10-dbb5798089f3',
-    //     updateDialog: { title: 'An update is available!' },
-    //     installMode: codePush.InstallMode.IMMEDIATE
-    //   },
-    //   (status) => {
-    //     switch (status) {
-    //       case codePush.SyncStatus.DOWNLOADING_PACKAGE:
-    //         // Show "downloading" modal
-    //         Alert.alert('DOWNLOADING_PACKAGE', JSON.stringify(status));
-
-    //         break;
-    //       case codePush.SyncStatus.INSTALLING_UPDATE:
-    //         // Hide "downloading" modal
-    //         Alert.alert('INSTALLING_UPDATE', JSON.stringify(status));
-    //         break;
-    //     }
-    //   },
-    //   ({ receivedBytes, totalBytes }) => {
-    //     /* Update download modal progress */
-    //     console.log({ receivedBytes, totalBytes });
-    //   }
-    // );
   }, []);
 
   return (
@@ -53,7 +29,10 @@ export default function AppRouter() {
       <SafeAreaProvider>
         <ThemeProvider>
           <StatusBar translucent animated style="dark" />
-          <Router />
+          <RootToaster />
+          <Host>
+            <Router />
+          </Host>
         </ThemeProvider>
       </SafeAreaProvider>
     </ApolloProvider>
