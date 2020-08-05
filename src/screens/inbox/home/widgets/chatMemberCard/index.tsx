637350f18ec4cf2cd32ba087d20e77ac5d5837ef
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-05 12:26:27 +0100

    create and style member chat card

---
+++ src/screens/inbox/home/widgets/chatMemberCard/index.tsx
@@ -0,0 +1,106 @@
+import React, { Fragment } from 'react';
+import { Title, Text, TouchableRipple } from 'react-native-paper';
+import FastImage from 'react-native-fast-image';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { NavigationInterface } from '../../../../types';
+import { useThemeContext } from '../../../../../theme';
+
+// IMPORT FOR ALL CUSTOM STYLES
+import { NameContainer, TimeStamp, BadgeWrapper } from './styles';
+
+// DEFINE SCREEN PROP TYPES
+interface MemberProp extends NavigationInterface {
+  avatar: string;
+  lastSeen: string;
+  name: string;
+  text: string;
+  time: string;
+  badge: number;
+}
+
+function Member(props: MemberProp) {
+  const { colors, fonts } = useThemeContext();
+  const {
+    avatar = 'https://picsum.photos/700',
+    name = 'Paul Maet',
+    text = 'Girl, I saw your message...',
+    time = ' 3: 02pm',
+    badge
+  } = props;
+
+  return (
+    <TouchableRipple
+      style={{
+        height: RFValue(60),
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: RFValue(20),
+        paddingLeft: RFValue(15),
+        paddingRight: RFValue(15)
+      }}
+      onPress={() => {}}
+    >
+      <Fragment>
+        <FastImage
+          resizeMode={FastImage.resizeMode.contain}
+          source={{
+            uri: avatar,
+            priority: FastImage.priority.high
+          }}
+          style={{
+            width: RFValue(50),
+            height: RFValue(50),
+            borderRadius: RFValue(4)
+          }}
+        />
+        <NameContainer>
+          <Title
+            style={{
+              color: colors.PRIMARY_TEXT,
+              fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+              fontSize: RFValue(fonts.LARGE_SIZE),
+              textTransform: 'capitalize'
+            }}
+          >
+            {name}
+          </Title>
+          <Text
+            style={{
+              color: colors.SECONDARY_TEXT,
+              fontFamily: fonts.WORK_SANS_REGULAR,
+              fontSize: RFValue(fonts.MEDIUM_SIZE)
+            }}
+          >
+            {text}
+          </Text>
+        </NameContainer>
+        <TimeStamp>
+          <Text
+            style={{
+              color: colors.SECONDARY_TEXT,
+              fontFamily: fonts.WORK_SANS_REGULAR,
+              fontSize: RFValue(fonts.MEDIUM_SIZE)
+            }}
+          >
+            {time}
+          </Text>
+          {badge ? (
+            <BadgeWrapper>
+              <Text
+                style={{
+                  color: colors.WHITE,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.SMALL_SIZE)
+                }}
+              >
+                {badge}
+              </Text>
+            </BadgeWrapper>
+          ) : null}
+        </TimeStamp>
+      </Fragment>
+    </TouchableRipple>
+  );
+}
+
+export default React.memo(Member);
