573dd72b927b5a99fa5b3b90aa2101c7a9b230e1
josemak25 <amakirij@gmail.com>
2020-08-06 11:56:09 +0100

    renamed chat navigator

--- src/navigator/chatNavigator/index.tsx
+++ src/navigator/chatNavigator/index.tsx
@@ -1,77 +1,156 @@
-import React from 'react';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import CommunityIcon from '../../../assets/icons/communityIcon';
-import PassportIcon from '../../../assets/icons/passportIcon';
-import InboxIcon from '../../../assets/icons/inboxIcon';
-import CommunityNavigator from '../communityNavigator';
+import React, { useState } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import { useTranslation } from 'react-i18next';
 import { useThemeContext } from '../../theme';
-import ChatNavigator from './chatNavigator';
-import Screens from '../../screens';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Screens from '../../screens/inbox';
+import { Searchbar } from 'react-native-paper';
+import { Feather } from '@expo/vector-icons';
+import { TouchableHighlight } from 'react-native';
+import hexToRGB from '../../utils/hexToRGB';
+import { NavigationInterface } from '../../screens/types';
+import { GLOBAL_HEADER_STYLE } from '../../constants';
+import GradientButton from '../../components/gradientButton';
 
-// IMPORT FOR ALL CUSTOM STYLES
-import { IconContainer, Label } from './styles';
+const ChatStack = createStackNavigator();
 
-export type TabBarIconTypes = { color: string };
+interface ChatNavigatorProps extends NavigationInterface {}
 
-const BottomTab = createMaterialBottomTabNavigator();
-
-export default function BottomNavigator() {
-  const { colors } = useThemeContext();
+export default function ChatNavigator(props: ChatNavigatorProps) {
+  const { navigation } = props;
+  const { colors, fonts } = useThemeContext();
+  const [search, setSearch] = useState('');
   const { t } = useTranslation();
 
+  const onChangeSearch = (query: any) => setSearch(query);
+
   return (
-    <BottomTab.Navigator
-      activeColor={colors.PRIMARY}
-      inactiveColor={colors.SECONDARY_TEXT}
-      barStyle={{ backgroundColor: colors.WHITE }}
+    <ChatStack.Navigator
       initialRouteName="InboxScreen"
-      labeled={false}
+      headerMode="screen"
+      screenOptions={{ headerStyle: { height: RFValue(90) } }}
     >
-      <BottomTab.Screen
-        name="CommunityScreen"
-        component={CommunityNavigator}
-        options={{
-          tabBarIcon: ({ color }: TabBarIconTypes) => (
-            <IconContainer>
-              <CommunityIcon fillColor={color} />
-              <Label style={{ color }}>
-                {t(`community.bottomLabels.community`)}
-              </Label>
-            </IconContainer>
-          )
-        }}
-      />
-
-      <BottomTab.Screen
+      <ChatStack.Screen
         name="InboxScreen"
-        component={ChatNavigator}
+        component={Screens.InboxScreen}
         options={{
-          tabBarIcon: ({ color }: TabBarIconTypes) => (
-            <IconContainer>
-              <InboxIcon fillColor={color} />
-              <Label style={{ color }}>
-                {t(`community.bottomLabels.inbox`)}
-              </Label>
-            </IconContainer>
-          )
+          headerTitle: t(`community.chat.messageTitle`),
+          headerLeft: (props) => (
+            <TouchableHighlight
+              {...props}
+              onPress={navigation.toggleDrawer}
+              underlayColor={hexToRGB(colors.PRIMARY, 0.1)}
+              style={{
+                height: RFValue(40),
+                width: RFValue(40),
+                borderRadius: RFValue(20),
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <Feather
+                name="menu"
+                size={RFValue(25)}
+                color={colors.PRIMARY_TEXT}
+              />
+            </TouchableHighlight>
+          ),
+          headerRight: () => (
+            <GradientButton
+              gradientContainerstyle={{
+                width: RFValue(30),
+                height: RFValue(30)
+              }}
+              onPress={() => navigation.navigate('ChatScreen')}
+              labelStyle={{ paddingLeft: 3 }}
+            >
+              <Feather name="message-square" color={colors.WHITE} size={20} />
+            </GradientButton>
+          ),
+          headerTitleContainerStyle: { alignItems: 'center' },
+          headerLeftContainerStyle: { marginLeft: 5 },
+          headerRightContainerStyle: {
+            marginRight: 10
+          },
+          headerTitleStyle: {
+            textTransform: 'uppercase',
+            fontSize: RFValue(fonts.LARGE_SIZE + 1),
+            fontFamily: fonts.WORK_SANS_REGULAR
+          }
         }}
       />
 
-      <BottomTab.Screen
-        name="PassportScreen"
-        component={Screens.PassportScreen}
+      <ChatStack.Screen
+        name="NewMessageScreen"
+        component={Screens.NewMessageScreen}
         options={{
-          tabBarIcon: ({ color }: TabBarIconTypes) => (
-            <IconContainer>
-              <PassportIcon fillColor={color} />
-              <Label style={{ color }}>
-                {t(`community.bottomLabels.passport`)}
-              </Label>
-            </IconContainer>
-          )
+          headerTitle: () => null,
+          headerBackTitleVisible: false,
+          headerTintColor: colors.PRIMARY,
+          headerLeftContainerStyle: { paddingLeft: 10 },
+          headerRight: () => (
+            <Searchbar
+              placeholder={t(`community.tabPanel.placeholder`)}
+              onChange={onChangeSearch}
+              value={search}
+              style={{
+                height: '70%',
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: RFValue(fonts.LARGE_SIZE),
+                color: colors.SECONDARY_TEXT,
+                elevation: 0,
+                borderColor: colors.INACTIVE,
+                borderRadius: 4,
+                borderWidth: 1
+              }}
+              iconColor={colors.PRIMARY_TEXT}
+            />
+          ),
+          headerRightContainerStyle: {
+            width: '80%',
+            marginRight: RFValue(10),
+            marginLeft: RFValue(20)
+          }
         }}
       />
-    </BottomTab.Navigator>
+
+      <ChatStack.Screen
+        name="NewChatSearch"
+        component={Screens.NewChatSearchScreen}
+        options={{
+          headerTitle: 'black lives matter',
+          headerTitleStyle: {
+            color: colors.PRIMARY_TEXT,
+            fontSize: RFValue(fonts.LARGE_SIZE),
+            fontFamily: fonts.WORK_SANS_BOLD,
+            textTransform: 'capitalize'
+          },
+          headerBackTitleVisible: false,
+          headerTintColor: colors.PRIMARY,
+          headerLeftContainerStyle: { paddingLeft: 10 },
+          headerRightContainerStyle: { marginRight: 10 },
+          headerStyle: GLOBAL_HEADER_STYLE
+        }}
+      />
+
+      <ChatStack.Screen
+        name="ChatScreen"
+        component={Screens.ChatScreen}
+        options={{
+          headerTitle: t(`community.chat.chatTitle`),
+          headerTitleStyle: {
+            color: colors.PRIMARY_TEXT,
+            fontSize: RFValue(fonts.LARGE_SIZE),
+            fontFamily: fonts.WORK_SANS_BOLD,
+            textTransform: 'capitalize'
+          },
+          headerBackTitleVisible: false,
+          headerTintColor: colors.PRIMARY,
+          headerLeftContainerStyle: { paddingLeft: 10 },
+          headerRightContainerStyle: { marginRight: 10 },
+          headerStyle: GLOBAL_HEADER_STYLE
+        }}
+      />
+    </ChatStack.Navigator>
   );
 }
