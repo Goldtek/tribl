2d3353c8dcc6d1a8078c3faadb5b8b2e6a6d24d3
josemak25 <amakirij@gmail.com>
2020-08-07 15:52:53 +0100

    add side bar navigator and caht navigator to open chat screen

--- src/router.tsx
+++ src/router.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SignupNavigator from './navigator/signupNavigator';
-import SideDrawerNavigator from './navigator/sideNavigator';
 import { useThemeContext } from './theme';
 import Screens from './screens';
+import DrawerStackNavigator from './navigator/sideNavigator';
 
 const RootStack = createStackNavigator();
 
@@ -39,7 +39,7 @@ export default function AppNavigator() {
 
         <RootStack.Screen
           name="CommunityScreen"
-          component={SideDrawerNavigator}
+          component={DrawerStackNavigator}
         />
       </RootStack.Navigator>
     </NavigationContainer>
