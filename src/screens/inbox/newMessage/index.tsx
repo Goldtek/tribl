970e2b23ff8b6edbb32108f0ed2235dbbe23558c
josemak25 <amakirij@gmail.com>
2020-08-07 15:49:25 +0100

    add change to chat slides

--- src/screens/inbox/newMessage/index.tsx
+++ src/screens/inbox/newMessage/index.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationInterface } from '../../types';
-import { Button } from 'react-native-paper';
+import { useTranslation } from 'react-i18next';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useThemeContext } from '../../../theme';
+import { Searchbar } from 'react-native-paper';
+import TabSlide from './widgets/tabs';
 
 // IMPORT FOR ALL CUSTOM STYLES
-import { Container, Welcome } from './styles';
+import { Container, Cover } from './styles';
 
 // DEFINE SCREEN PROP TYPES
 interface ScreenProp extends NavigationInterface {}
 
-export default function NewMessageScreen(props: ScreenProp) {
+export default function ChatScreen(props: ScreenProp) {
+  const { colors, fonts } = useThemeContext();
+  const { t } = useTranslation();
+  const [search, setSearch] = useState('');
+  const onChangeSearch = (query: string) => setSearch(query);
+
   return (
     <Container>
-      <Button
-        icon="camera"
-        mode="contained"
-        onPress={() => console.log('Pressed')}
-      >
-        Press me
-      </Button>
-      <Welcome>New Message Screen</Welcome>
+      <Cover>
+        <Searchbar
+          placeholder={t(`community.chat.search`)}
+          onChangeText={onChangeSearch}
+          value={search}
+          style={{
+            marginLeft: RFValue(10),
+            marginRight: RFValue(10),
+            fontFamily: fonts.WORK_SANS_REGULAR,
+            fontSize: RFValue(fonts.LARGE_SIZE),
+            color: colors.SECONDARY_TEXT,
+            elevation: 0,
+            borderColor: colors.INACTIVE,
+            borderRadius: 4,
+            borderWidth: 1
+          }}
+          iconColor={colors.PRIMARY_TEXT}
+        />
+      </Cover>
+      <TabSlide />
     </Container>
   );
 }
