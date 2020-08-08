6748d775d714b3c1b84be8fbe050616a36dd23c6
josemak25 <amakirij@gmail.com>
2020-08-08 23:50:34 +0100

    add upcoming screen to default screen export

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,9 +1,11 @@
 import WalkThroughScreen from './walkThrough';
 import PassportScreen from './passport';
+import UpcomingScreen from './upcoming';
 import SplashScreen from './splash';
 
 export default {
   WalkThroughScreen,
   PassportScreen,
+  UpcomingScreen,
   SplashScreen
 };
