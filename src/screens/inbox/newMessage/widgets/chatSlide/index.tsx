e897ff078bd18f517d4b827962d213280db6c4fb
josemak25 <amakirij@gmail.com>
2020-08-08 23:46:16 +0100

    add fix to chat slide and chat slide styles

--- src/screens/inbox/newMessage/widgets/chatSlide/index.tsx
+++ src/screens/inbox/newMessage/widgets/chatSlide/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useMemo } from 'react';
 import { FlatList } from 'react-native';
 import { NavigationInterface } from '../../../../types';
 import { Divider, TouchableRipple, Text } from 'react-native-paper';
@@ -15,67 +15,72 @@ import { GroupWrapper, Container } from './styles';
 interface ScreenProp extends NavigationInterface {}
 
 export default function ChatSlide(props: ScreenProp) {
-  const { navigation } = props;
-
   const { colors, fonts } = useThemeContext();
 
   const _renderItem = ({ item }: any) => (
     <MemberCard key={item.id} {...item} {...props} />
   );
 
-  const _separator = () => (
-    <Divider
-      style={{
-        height: 1.5,
-        backgroundColor: hexToRGB(colors.INACTIVE, 0.5),
-        marginHorizontal: RFValue(20)
-      }}
-    />
-  );
+  const _separator = () =>
+    useMemo(
+      () => (
+        <Divider
+          style={{
+            height: 1.5,
+            backgroundColor: hexToRGB(colors.INACTIVE, 0.5),
+            marginHorizontal: RFValue(20)
+          }}
+        />
+      ),
+      []
+    );
+
+  const _listHeaderComponent = () =>
+    useMemo(
+      () => (
+        <TouchableRipple
+          style={{
+            backgroundColor: colors.WHITE,
+            paddingHorizontal: RFValue(20),
+            paddingBottom: RFValue(5)
+          }}
+          rippleColor={hexToRGB(colors.PRIMARY, 0.3)}
+          onPress={() => {}}
+        >
+          <Fragment>
+            <Container>
+              <GroupWrapper>
+                <FontAwesome name="group" size={30} color={colors.PRIMARY} />
+              </GroupWrapper>
+              <Text
+                style={{
+                  color: colors.PRIMARY_TEXT,
+                  fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                  fontSize: RFValue(fonts.LARGE_SIZE),
+                  paddingLeft: RFValue(15),
+                  textTransform: 'capitalize'
+                }}
+              >
+                new group
+              </Text>
+            </Container>
+            <Divider />
+          </Fragment>
+        </TouchableRipple>
+      ),
+      []
+    );
 
   return (
-    <Fragment>
-      <TouchableRipple
-        style={{
-          backgroundColor: colors.WHITE,
-          paddingHorizontal: RFValue(20),
-          paddingBottom: RFValue(5),
-          height: RFValue(60)
-        }}
-        rippleColor={hexToRGB(colors.PRIMARY, 0.3)}
-        onPress={() => {}}
-      >
-        <Fragment>
-          <Container>
-            <GroupWrapper>
-              <FontAwesome name="group" size={30} color={colors.PRIMARY} />
-            </GroupWrapper>
-            <Text
-              style={{
-                color: colors.PRIMARY_TEXT,
-                fontFamily: fonts.WORK_SANS_SEMI_BOLD,
-                fontSize: RFValue(fonts.LARGE_SIZE),
-                paddingLeft: RFValue(15),
-                textTransform: 'capitalize'
-              }}
-            >
-              new group
-            </Text>
-          </Container>
-          <Divider />
-        </Fragment>
-      </TouchableRipple>
-      <FlatList
-        data={MembersData}
-        contentContainerStyle={{
-          flexGrow: 1
-        }}
-        style={{ backgroundColor: colors.WHITE, marginTop: 20 }}
-        ItemSeparatorComponent={_separator}
-        showsVerticalScrollIndicator={false}
-        renderItem={_renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    </Fragment>
+    <FlatList
+      data={MembersData}
+      renderItem={_renderItem}
+      ListHeaderComponent={_listHeaderComponent}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={_separator}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ flexGrow: 1, paddingBottom: 20 }}
+      style={{ backgroundColor: colors.WHITE, paddingTop: 10 }}
+    />
   );
 }
--- src/screens/inbox/newMessage/widgets/chatSlide/styles.ts
+++ src/screens/inbox/newMessage/widgets/chatSlide/styles.ts
@@ -1,16 +1,17 @@
 import styled from 'styled-components/native';
-import hexToRGB from '../../../../../utils/hexToRGB';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex-direction: row;
   align-items: center;
+  padding-top: 20px;
   margin-bottom: 10px;
 `;
 
 export const GroupWrapper = styled.View`
   background-color: ${({ theme }) => theme.colors.GREY};
-  width: 70px;
-  height: 50px;
+  width: ${RFValue(70)}px;
+  height: ${RFValue(50)}px;
   border-radius: 5px;
   align-items: center;
   justify-content: center;
