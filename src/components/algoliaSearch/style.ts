cb1032c8dfb16042553d83405f6670c663462b8d
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-14 16:09:48 +0100

    restyle algolia search component

--- src/components/algoliaSearch/style.ts
+++ src/components/algoliaSearch/style.ts
@@ -4,5 +4,6 @@ import { RFValue } from 'react-native-responsive-fontsize';
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.WHITE};
   margin-top: ${RFValue(5)}px;
-  padding: ${RFValue(10)}px 0;
+  padding: ${RFValue(10)}px ${RFValue(20)}px;
+  width: 100%;
 `;
