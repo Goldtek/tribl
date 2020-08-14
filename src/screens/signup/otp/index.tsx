0569d17129f4102107b9f57b78bc934e5e38c959
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-14 14:20:21 +0100

    remove console.tron

--- src/screens/signup/otp/index.tsx
+++ src/screens/signup/otp/index.tsx
@@ -75,7 +75,6 @@ export default function OTPScreen(props: ScreenProp) {
         const { data } = await verifyOtp();
 
         if (data?.validateOtp) {
-          console.tron(data?.validateOtp);
           Storage.setCredentialInstance(data?.validateOtp);
           navigation.reset({
             index: 0,
