ac0ad029521cdd588ec94507b0d243f7f50d56f3
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-14 14:18:06 +0100

    add algolia search

--- src/screens/inbox/newMessage/index.tsx
+++ src/screens/inbox/newMessage/index.tsx
@@ -1,23 +1,21 @@
-import React, { useState } from 'react';
-import { NavigationInterface } from '../../types';
+import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { useTranslation } from 'react-i18next';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useThemeContext } from '../../../theme';
-import { Searchbar } from 'react-native-paper';
+import { NavigationInterface } from '../../types';
 import TabSlide from './widgets/tabs';
+import AlgoliaSearch from '../../../components/algoliaSearch';
+import Highlight from '../../../components/algoliaInboxCard';
+import AlgoliaList from '../../../components/algoliaInboxList';
 
 // IMPORT FOR ALL CUSTOM STYLES
-import { Container, Cover } from './styles';
+import { Container } from './styles';
 
 // DEFINE SCREEN PROP TYPES
 interface ScreenProp extends NavigationInterface {}
 
 export default function ChatScreen(props: ScreenProp) {
-  const { colors, fonts } = useThemeContext();
-  const { t } = useTranslation();
-  const [search, setSearch] = useState('');
-  const onChangeSearch = (query: string) => setSearch(query);
+  const { colors } = useThemeContext();
 
   return (
     <SafeAreaView
@@ -28,25 +26,9 @@ export default function ChatScreen(props: ScreenProp) {
       }}
     >
       <Container>
-        <Cover>
-          <Searchbar
-            placeholder={t(`community.chat.search`)}
-            onChangeText={onChangeSearch}
-            value={search}
-            style={{
-              marginLeft: RFValue(10),
-              marginRight: RFValue(10),
-              fontFamily: fonts.WORK_SANS_REGULAR,
-              fontSize: RFValue(fonts.LARGE_SIZE),
-              color: colors.SECONDARY_TEXT,
-              elevation: 0,
-              borderColor: colors.INACTIVE,
-              borderRadius: 4,
-              borderWidth: 1
-            }}
-            iconColor={colors.PRIMARY_TEXT}
-          />
-        </Cover>
+        <AlgoliaSearch indexName="tribl_passport_staging">
+          <AlgoliaList />
+        </AlgoliaSearch>
         <TabSlide />
       </Container>
     </SafeAreaView>
