9ae0b05d717470c6a1b72d0cbd3c326fcc3620f1
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-14 14:19:58 +0100

    remove unused styled

--- src/screens/inbox/newMessage/styles.ts
+++ src/screens/inbox/newMessage/styles.ts
@@ -4,9 +4,3 @@ import { RFValue } from 'react-native-responsive-fontsize';
 export const Container = styled.View`
   flex: 1;
 `;
-
-export const Cover = styled.View`
-  background-color: ${({ theme }) => theme.colors.WHITE};
-  margin-top: ${RFValue(5)}px;
-  padding: ${RFValue(10)}px 0;
-`;
