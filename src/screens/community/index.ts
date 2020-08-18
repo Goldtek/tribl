292a3174440f8f19b6f05ddedee0ecb8891cdbbe
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-18 14:45:17 +0100

    add screen to community screens

--- src/screens/community/index.ts
+++ src/screens/community/index.ts
@@ -3,11 +3,13 @@ import SearchScreen from './search';
 import CommunityDetailScreen from './detail';
 import MemberDetailScreen from './memberPassport';
 import NewMessageScreen from '../inbox/newMessage';
+import MyConnectionScreen from './myConnections';
 
 export default {
   HomeScreen,
   SearchScreen,
   CommunityDetailScreen,
   MemberDetailScreen,
-  NewMessageScreen
+  NewMessageScreen,
+  MyConnectionScreen
 };
