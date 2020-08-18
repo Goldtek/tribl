402cc6f8d54a12199bf475a366c51ee4244b802b
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-18 14:47:35 +0100

    create my connections screen

---
+++ src/screens/community/myConnections/index.tsx
@@ -0,0 +1,85 @@
+import React, { Fragment } from 'react';
+import { NavigationInterface } from '../../types';
+import { Text, TouchableRipple, Title } from 'react-native-paper';
+import { useTranslation } from 'react-i18next';
+import { useSafeArea } from 'react-native-safe-area-context';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { StatusBar, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useThemeContext } from '../../../theme';
+import Header from '../../../components/header';
+import { Entypo } from '@expo/vector-icons';
+import AlgoliaSearch from '../../../components/algoliaSearch';
+import AlgoliaList from '../../../components/algoliaInboxList';
+import MembersData from '../../../libs/members/index.json';
+import Connection from './widget';
+
+import { Container } from './styles';
+
+// DEFINE SCREEN PROP TYPES
+interface MyConnectionScreenProp extends NavigationInterface {}
+
+export default function ProfileScreen(props: MyConnectionScreenProp) {
+  const { colors, fonts } = useThemeContext();
+  const { top } = useSafeArea();
+  const { t } = useTranslation();
+  const navigation = useNavigation();
+
+  const _renderItem = ({ item }: any) => (
+    <Connection key={item.id} {...item} {...props} />
+  );
+
+  return (
+    <Fragment>
+      <StatusBar backgroundColor={colors.WHITE} barStyle="dark-content" />
+      <Header
+        title={() => (
+          <Text
+            style={{
+              color: colors.PRIMARY_TEXT,
+              fontSize: RFValue(fonts.LARGE_SIZE),
+              fontFamily: fonts.WORK_SANS_BOLD,
+              textTransform: 'capitalize'
+            }}
+          >
+            {t(`community.sideNav.connection`)}
+          </Text>
+        )}
+        headerLeft={() => (
+          <TouchableRipple onPress={() => props.navigation.goBack()}>
+            <Entypo name="chevron-left" size={25} color={colors.PRIMARY} />
+          </TouchableRipple>
+        )}
+        style={{ paddingTop: top }}
+      />
+      <Container>
+        <AlgoliaSearch indexName="tribl_passport_staging">
+          <AlgoliaList />
+        </AlgoliaSearch>
+        <Title
+          style={{
+            color: colors.PRIMARY_TEXT,
+            fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+            fontSize: RFValue(fonts.LARGE_SIZE),
+            marginTop: RFValue(20),
+            marginLeft: RFValue(10),
+            textTransform: 'capitalize'
+          }}
+        >
+          {t(`community.tabPanel.memberTitle`)}
+        </Title>
+        <FlatList
+          data={MembersData}
+          contentContainerStyle={{
+            flexGrow: 1,
+            marginTop: RFValue(10),
+            paddingBottom: RFValue(120)
+          }}
+          showsVerticalScrollIndicator={false}
+          renderItem={_renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </Container>
+    </Fragment>
+  );
+}
