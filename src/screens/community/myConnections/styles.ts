2706d3db586625360507d2fd2cdba8bc7f5cc186
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-18 14:48:28 +0100

    style my connections screen

---
+++ src/screens/community/myConnections/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.WHITE};
+  padding: 15px 0;
+  margin-top: 10px;
+`;
