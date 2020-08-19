364ce6375099ad91f2d8832441b843445b035765
josemak25 <amakirij@gmail.com>
2020-08-19 15:53:49 +0100

    add new screen types to global stack screen typings

--- src/screens/types.d.ts
+++ src/screens/types.d.ts
@@ -4,21 +4,22 @@ import { StackActionHelpers } from '@react-navigation/native';
 
 // App Navigation prop types
 export type RootStackParamList = {
+  CommunitySearchScreen: undefined;
   SelectCountryScreen: undefined;
   CreateAccountScreen: undefined;
   AvatarUploadScreen: undefined;
   IdentifyUserScreen: undefined;
   UserLocationScreen: undefined;
   WalkThroughScreen: undefined;
+  NewMessageScreen: undefined;
   CommunityScreen: undefined;
+  NewChatSearch: undefined;
   ProfileScreen: undefined;
   SignupScreen: undefined;
   InboxScreen: undefined;
   TriblScreen: undefined;
   LoginScreen: undefined;
   OTPScreen: undefined;
-  NewMessageScreen: undefined;
-  NewChatSearch: undefined;
   ChatScreen: { title: string };
   CommunityDetailScreen: {};
   MemberDetailScreen: {};
