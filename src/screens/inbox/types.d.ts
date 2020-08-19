796b64c1bd685d4865abd156df00915628613863
josemak25 <amakirij@gmail.com>
2020-08-19 16:09:13 +0100

    add chat props types for direct messages and group messages

---
+++ src/screens/inbox/types.d.ts
@@ -0,0 +1,44 @@
+interface User {
+  _id: string;
+  name: string;
+  avatar: string;
+}
+
+export interface MessageInterface {
+  _id: string;
+  text: string;
+  createdAt: Date;
+  user: User;
+  image?: string;
+  video?: string;
+  audio?: string;
+  system?: boolean;
+  sent?: boolean;
+  received?: boolean;
+  pending?: boolean;
+  quickReplies?: QuickReplies;
+}
+
+interface Reply {
+  title: string;
+  value: string;
+  messageId?: string;
+}
+
+interface QuickReplies {
+  type: 'radio' | 'checkbox';
+  values: Reply[];
+  keepIt?: boolean;
+}
+
+export interface GroupInterface {
+  id: string;
+  avatar: string;
+  name: string;
+  unseenCount: number;
+  displayMessage: string;
+  lastMessageTime: Date;
+  members: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
