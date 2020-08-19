8bfc08cd25db0b21f016fda0c3c577911bbf1ddf
josemak25 <amakirij@gmail.com>
2020-08-19 16:06:14 +0100

    add firestore methods and types

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,109 +1,109 @@
-import firestore, {
-  FirebaseFirestoreTypes
-} from '@react-native-firebase/firestore';
-import fireauth from '@react-native-firebase/auth';
-import oldDatabase from '@react-native-firebase/database';
-import { ROOM_TYPES, ChatRoom, UserConversation } from './types';
+import firestore from '@react-native-firebase/firestore';
 
-const database = firestore();
-const auth = fireauth();
-const tribl = database.collection('tribl-chat-app');
-const batch = database.batch();
+import { firechat, database, fireAuth } from './config';
+
+import { ROOM_TYPES, ChatRoom } from './types';
+import { GroupInterface } from '../screens/inbox/types';
+
+const batch = firechat.batch();
 
 // ------ Setup -------- //
 
-export default class Firechat {
-  constructor() {}
+class Firechat {
+  private userId: string = '';
 
   async signIn(firebaseToken: string) {
-    return auth.signInWithCustomToken(firebaseToken);
-  }
-
-  // THIS METHOD CREATES A CHANNEL AND GROUP
-  async createRoom(roomId: string, payload: ChatRoom) {
-    const timestamp = firestore.FieldValue.serverTimestamp();
-
-    // get chat collection via roomID
-    const chatroom = database.collection(payload.roomType).doc(roomId);
-
-    // make a write batch for chatroom collection via roomId
-    batch.set(
-      chatroom,
-      {
-        members: { [payload.userId]: true },
-        createdAt: timestamp,
-        updatedAt: timestamp
-      },
-      { merge: true }
-    );
-
-    const receivers = this.getUserConversations(payload);
-
-    receivers.forEach((receiver) =>
-      batch.set(receiver, { [payload.roomType]: [roomId] })
-    );
-
-    return batch.commit();
-  }
-
-  // THIS METHOD ADDS A NEW MEMBER TO CHANNEL OR GROUP
-  async addMemberToRoom(roomId: string, payload: ChatRoom) {
-    return tribl
-      .doc(`${payload.roomType}/${roomId}`)
-      .collection('members')
-      .add({ [payload.userId]: true });
-  }
-
-  // THIS METHOD SENDS A NEW MESSAGE
-  async sendMessage(roomId: string, payload: ChatRoom) {
-    const conversationRoom = tribl.doc(`${ROOM_TYPES.CONVERSATIONS}/${roomId}`);
-    const timestamp = firestore.FieldValue.serverTimestamp();
-
-    conversationRoom.update({
-      displayMessage: 'Hello world',
-      lastMessageTime: timestamp,
-      unseenCount: 10
+    fireAuth.signInWithCustomToken(firebaseToken);
+    fireAuth.onAuthStateChanged((user) => {
+      if (user) this.userId = user.uid;
     });
-
-    conversationRoom
-      .collection('messages')
-      .add({ ...payload, createdAt: timestamp });
   }
 
-  // THIS METHOD GETS USERS CONVERSATIONS
-  getUserConversations(payload: ChatRoom) {
-    // get user chat history via userId
-    const userConversions = database
-      .collection(ROOM_TYPES.USER_CONVERSATIONS)
-      .doc(payload.userId);
+  // // THIS METHOD CREATES A CHANNEL AND GROUP
+  // async createRoom(roomId: string, payload: ChatRoom) {
+  //   const timestamp = firestore.FieldValue.serverTimestamp();
 
-    // get receiver chat history via receiverId
-    const receiverConversions = payload.receivers?.map((receiver) =>
-      database.collection(ROOM_TYPES.USER_CONVERSATIONS).doc(receiver)
-    ) as FirebaseFirestoreTypes.DocumentReference[];
+  //   // get chat collection via roomID
+  //   const chatroom = firechat.collection(payload.roomType).doc(roomId);
 
-    return [userConversions, ...receiverConversions];
-  }
+  //   // make a write batch for chatroom collection via roomId
+  //   batch.set(
+  //     chatroom,
+  //     {
+  //       members: { [this.userId]: true },
+  //       createdAt: timestamp,
+  //       updatedAt: timestamp
+  //     },
+  //     { merge: true }
+  //   );
 
-  // THIS METHOD CREATES USER CONVERSATION REF OBJECT
-  async createUserConversation(userId: string, payload: UserConversation) {
-    return tribl
-      .doc(`${ROOM_TYPES.CHANNELS}/${userId}`)
-      .update({
-        [payload.conversationType]: firestore.FieldValue.arrayUnion(
-          payload.conversationId
-        )
-      })
-      .catch((error) => {
-        tribl.doc(`${ROOM_TYPES.CHANNELS}/${userId}`).set({
-          [payload.conversationType]: [payload.conversationId]
-        });
-      });
-  }
+  //   const receivers = this.getUserConversations(payload);
+
+  //   receivers.forEach((receiver) =>
+  //     batch.set(receiver, { [payload.roomType]: [roomId] })
+  //   );
+
+  //   return batch.commit();
+  // }
+
+  // // THIS METHOD ADDS A NEW MEMBER TO CHANNEL OR GROUP
+  // async addMemberToRoom(roomId: string, payload: ChatRoom) {
+  //   return tribl
+  //     .doc(`${payload.roomType}/${roomId}`)
+  //     .collection('members')
+  //     .add({ [this.userId]: true });
+  // }
+
+  // // THIS METHOD SENDS A NEW MESSAGE
+  // async sendMessage(roomId: string, payload: ChatRoom) {
+  //   const conversationRoom = tribl.doc(`${ROOM_TYPES.CONVERSATIONS}/${roomId}`);
+  //   const timestamp = firestore.FieldValue.serverTimestamp();
+
+  //   conversationRoom.update({
+  //     displayMessage: 'Hello world',
+  //     lastMessageTime: timestamp,
+  //     unseenCount: 10
+  //   });
+
+  //   conversationRoom
+  //     .collection('messages')
+  //     .add({ ...payload, createdAt: timestamp });
+  // }
+
+  // // THIS METHOD GETS USERS CONVERSATIONS
+  // getUserConversations(payload: ChatRoom) {
+  //   // get user chat history via userId
+  //   const userConversions = firechat
+  //     .collection(ROOM_TYPES.USER_CONVERSATIONS)
+  //     .doc(this.userId);
+
+  //   // get receiver chat history via receiverId
+  //   const receiverConversions = payload.receivers?.map((receiver) =>
+  //     firechat.collection(ROOM_TYPES.USER_CONVERSATIONS).doc(receiver)
+  //   ) as FirebaseFirestoreTypes.DocumentReference[];
+
+  //   return [userConversions, ...receiverConversions];
+  // }
+
+  // // THIS METHOD CREATES USER CONVERSATION REF OBJECT
+  // async createUserConversation(userId: string, payload: UserConversation) {
+  //   return tribl
+  //     .doc(`${ROOM_TYPES.CHANNELS}/${userId}`)
+  //     .update({
+  //       [payload.conversationType]: firestore.FieldValue.arrayUnion(
+  //         payload.conversationId
+  //       )
+  //     })
+  //     .catch((error) => {
+  //       tribl.doc(`${ROOM_TYPES.CHANNELS}/${userId}`).set({
+  //         [payload.conversationType]: [payload.conversationId]
+  //       });
+  //     });
+  // }
 
   // USER ONLINE STATUS METHOD TO TRACK USER PRESENCE
   async onlineStatus(userId: string) {
-    const userStatusRef = oldDatabase().ref(`/status/${userId}`);
+    const userStatusRef = database.ref(`/status/${userId}`);
 
     // Set the user online status to be through
     userStatusRef.set({
@@ -116,4 +116,46 @@ export default class Firechat {
       last_changed: firestore.FieldValue.serverTimestamp()
     });
   }
+
+  // THIS METHOD GETS USERS CONVERSATIONS
+  async getUserGroupMessages() {
+    // get user chat history via userId
+    const groups = await firechat
+      .collection(ROOM_TYPES.USER_CONVERSATIONS)
+      .doc(this.userId)
+      .get();
+
+    if (!groups.exists) return null;
+
+    const { groupMessages } = groups.data() as {
+      groupMessages: string[];
+      channelLists: string[];
+    };
+
+    const groupMessagesData = await firechat
+      .collection(ROOM_TYPES.GROUPS)
+      .where(firestore.FieldPath.documentId(), 'in', groupMessages)
+      .get();
+
+    const userGroup = groupMessagesData.docs.map(
+      (group) => group.data() as GroupInterface
+    );
+
+    return userGroup;
+  }
+
+  // USER ONLINE STATUS METHOD TO TRACK USER PRESENCE
+  async getChatMessages(chatId: string) {
+    const chatMessages = await firechat
+      .doc(ROOM_TYPES.CONVERSATIONS)
+      .collection(chatId)
+      .doc('messages')
+      .get();
+
+    const messages = chatMessages.data();
+
+    console.tron({ messages });
+  }
 }
+
+export default new Firechat();
