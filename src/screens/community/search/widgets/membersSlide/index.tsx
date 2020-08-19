85489df3e02052a53dd44aa34473e08e0ce379de
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-20 00:56:28 +0100

    add mdal on click of view all

--- src/screens/community/search/widgets/membersSlide/index.tsx
+++ src/screens/community/search/widgets/membersSlide/index.tsx
@@ -21,7 +21,7 @@ import { Container, RecommendedList, RecommendedListHeader } from './styles';
 // DEFINE SCREEN PROP TYPES
 interface ScreenProp extends NavigationInterface {}
 
-export default function SearchScreen(props: ScreenProp) {
+export default function MemberSearch(props: ScreenProp) {
   const { colors, fonts } = useThemeContext();
   const { t } = useTranslation();
   const navigation = useNavigation();
