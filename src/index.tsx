0e7c6fe24b804682ad8763d4ae17a6e5e30dc8a3
josemak25 <amakirij@gmail.com>
2020-08-20 13:43:44 +0100

    add OTA update fix to use expo servers

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,22 @@
-import React, { useEffect } from 'react';
-import * as Updates from 'expo-updates';
+import React from 'react';
 import { RootToaster } from './components/rootToaster';
-import { Host } from 'react-native-portalize';
+import { Host as PortalHost } from 'react-native-portalize';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Alert } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import ApolloProvider from './graphql';
 import ThemeProvider from './theme';
 import Router from './router';
 
 export default function AppRouter() {
-  useEffect(() => {
-    (async () => {
-      const update = await Updates.checkForUpdateAsync();
-      if (update.isAvailable) {
-        await Updates.fetchUpdateAsync();
-        Alert.alert('UPDATE AVAILABLE', 'DOWNLOADING UPDATES NOW...');
-        // ... notify user of update ...
-        await Updates.reloadAsync();
-      }
-      // Prompt the user when an update is available
-      // and then display a "downloading" modal
-    })();
-  }, []);
-
   return (
     <ApolloProvider>
       <SafeAreaProvider>
         <ThemeProvider>
           <StatusBar translucent animated style="dark" />
           <RootToaster />
-          <Host>
+          <PortalHost>
             <Router />
-          </Host>
+          </PortalHost>
         </ThemeProvider>
       </SafeAreaProvider>
     </ApolloProvider>
