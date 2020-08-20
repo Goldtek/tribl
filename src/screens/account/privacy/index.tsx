0350e0b029f358dafdbcd2c181cf4e132960db59
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-20 02:04:57 +0100

    add switch button

--- src/screens/account/privacy/index.tsx
+++ src/screens/account/privacy/index.tsx
@@ -1,13 +1,14 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { NavigationInterface } from '../../types';
 import { Text, TouchableRipple, Divider } from 'react-native-paper';
+import { Switch } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useThemeContext } from '../../../theme';
 import { AntDesign } from '@expo/vector-icons';
 
 // IMPORT FOR ALL CUSTOM STYLES
-import { Container } from './styles';
+import { Container, ToggleContainer, ToggleCover } from './styles';
 
 // DEFINE SCREEN PROP TYPES
 interface MyConnectionScreenProp extends NavigationInterface {}
@@ -15,6 +16,8 @@ interface MyConnectionScreenProp extends NavigationInterface {}
 export default function ProfileScreen(props: MyConnectionScreenProp) {
   const { colors, fonts } = useThemeContext();
   const { t } = useTranslation();
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   return (
     <Fragment>
       <Container>
@@ -192,6 +195,40 @@ export default function ProfileScreen(props: MyConnectionScreenProp) {
           </Fragment>
         </TouchableRipple>
         <Divider style={{ backgroundColor: colors.INPUT }} />
+        <ToggleContainer>
+          <Text
+            style={{
+              fontFamily: fonts.WORK_SANS_REGULAR,
+              fontSize: fonts.LARGE_SIZE,
+              color: colors.PRIMARY_TEXT,
+              textTransform: 'capitalize'
+            }}
+          >
+            {t(`community.accountSettings.passport`)}
+          </Text>
+          <ToggleCover>
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: fonts.LARGE_SIZE,
+                color: colors.PRIMARY_TEXT,
+                textTransform: 'capitalize',
+                marginRight: RFValue(20)
+              }}
+            >
+              {isEnabled
+                ? t(`community.accountSettings.private`)
+                : t(`community.accountSettings.public`)}
+            </Text>
+            <Switch
+              trackColor={{ false: colors.DISABLED, true: colors.ONLINE }}
+              thumbColor={colors.WHITE}
+              ios_backgroundColor={colors.DISABLED}
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+            />
+          </ToggleCover>
+        </ToggleContainer>
       </Container>
     </Fragment>
   );
