14081bf723de219a0ca7b775b667a0a1d69cd4da
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-20 02:05:15 +0100

    style switch button

--- src/screens/account/privacy/styles.ts
+++ src/screens/account/privacy/styles.ts
@@ -7,3 +7,16 @@ export const Container = styled.View`
   padding-bottom: ${RFValue(15)}px;
   margin-top: ${RFValue(10)}px;
 `;
+
+export const ToggleContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: ${RFValue(50)}px;
+  padding: 0 ${RFValue(25)}px;
+`;
+
+export const ToggleCover = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
