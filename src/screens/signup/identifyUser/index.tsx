5a71d01943be24d25627a515327273a4eb8969f3
josemak25 <amakirij@gmail.com>
2020-08-24 16:44:25 +0100

    add cloudinary method to upload user avatar on passport creation

--- src/screens/signup/identifyUser/index.tsx
+++ src/screens/signup/identifyUser/index.tsx
@@ -36,7 +36,7 @@ export default function IdentifyUserScreen(props: ScreenProp) {
   const [addUserDetails] = useMutation(ADD_USER_DETAILS, {
     variables: {
       details: {
-        identities: [...Array.from(state.selectedIdentities.values())]
+        identity: [...Array.from(state.selectedIdentities.values())]
       }
     }
   });
