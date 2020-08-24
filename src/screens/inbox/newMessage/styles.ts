304b053d3d0a8cde3db2d60f46991df90871375a
josemak25 <amakirij@gmail.com>
2020-08-24 16:47:13 +0100

    fix new message screen designs

--- src/screens/inbox/newMessage/styles.ts
+++ src/screens/inbox/newMessage/styles.ts
@@ -1,6 +1,30 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
   padding: 15px 0;
+`;
+
+export const FilterContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin: 20px 15px 10px 15px;
+`;
+
+export const GroupContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding-top: 20px;
+  margin-bottom: 10px;
+`;
+
+export const GroupWrapper = styled.View`
+  background-color: ${({ theme }) => theme.colors.GREY};
+  width: ${RFValue(70)}px;
+  height: ${RFValue(50)}px;
+  border-radius: 5px;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 2px 4px 4px ${({ theme }) => theme.colors.INACTIVE};
 `;
