3dae4be9cae0a6c941564ce4f2db9ae7edbb9c45
josemak25 <amakirij@gmail.com>
2020-08-28 12:40:39 +0100

    add connection chat screen

--- src/screens/inbox/connectionChat/index.tsx
+++ src/screens/inbox/connectionChat/index.tsx
@@ -4,60 +4,80 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { GiftedChat, Send } from 'react-native-gifted-chat';
 import { Platform } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import uniquePushId from 'unique-push-id';
 import { useThemeContext } from '../../../theme';
 import { Ionicons } from '@expo/vector-icons';
 import { MessageInterface } from '../types';
 import { fireAuth } from '../../../firebase/config';
 import Firechat from '../../../firebase';
+import { MyPassportInterface } from '../../../graphql/types';
+import { GET_USER_PASSPORT } from '../../../graphql/server/query';
+import { useQuery } from '@apollo/react-hooks';
+import { ROOM_TYPES } from '../../../firebase/types';
+import { CONVERSATION_TYPES } from '../../../firebase/types';
 
 // DEFINE SCREEN PROP TYPES
 interface ScreenProp extends NavigationInterface {
   route: {
-    params: { title: string; avatar: string; chatId: string };
+    params: { title: string; avatar: string; receiverId: string };
   };
 }
 
-export default function ChatScreen(props: ScreenProp) {
+export default function ConnectionChatScreen(props: ScreenProp) {
   const { colors, fonts } = useThemeContext();
-  const { chatId } = props.route.params;
 
   const userId = fireAuth.currentUser?.uid as string;
+  const chatId = uniquePushId();
+
+  const { data: userData } = useQuery<MyPassportInterface>(GET_USER_PASSPORT);
+
+  const userDetails = userData?.myPassport;
+
+  const [firstTime, setFirstTime] = useState(true);
 
   const [messages, setMessages] = useState<MessageInterface[]>([]);
 
   useEffect(() => {
-    if (!chatId) return;
+    const chatMessages = Firechat.getChatMessages(chatId);
 
-    Firechat.getChatMessages(chatId);
+    const unsubscribe = chatMessages.onSnapshot({
+      next: (snapshot) => {
+        const conversations = snapshot.docs.map((documentSnapshot) => {
+          return documentSnapshot.data() as MessageInterface;
+        });
 
-    setMessages([
-      {
-        _id: '1',
-        text: 'Hello bro',
-        createdAt: new Date(),
-        user: {
-          _id: '2',
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any'
-        }
-      },
-      {
-        _id: '20',
-        text: 'Hey man',
-        createdAt: new Date(),
-        user: {
-          _id: userId,
-          name: 'React',
-          avatar: 'https://placeimg.com/140/140/any'
-        }
+        setMessages(GiftedChat.append(messages, conversations));
       }
-    ]);
+    });
+
+    return () => unsubscribe();
   }, []);
 
-  const onSend = useCallback((messages: MessageInterface[] = []) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages)
-    );
+  const onSend = useCallback(async (messages: MessageInterface[] = []) => {
+    const [message] = messages;
+
+    if (firstTime) {
+      setFirstTime(false);
+
+      setMessages((prevMessages) => GiftedChat.append(prevMessages, messages));
+
+      await Firechat.createRoom(chatId, {
+        message: message,
+        name: props.route.params.title,
+        receivers: [
+          { ...props.route.params },
+          {
+            title: `${userDetails?.firstName} ${userDetails?.lastName}`,
+            avatar: `${userDetails?.avatar}`,
+            receiverId: userId
+          }
+        ],
+        roomType: ROOM_TYPES.GROUPS,
+        conversationType: CONVERSATION_TYPES.DIRECT_MESSAGES
+      });
+    }
+
+    await Firechat.sendMessage(chatId, message);
   }, []);
 
   return (
@@ -65,7 +85,11 @@ export default function ChatScreen(props: ScreenProp) {
       <GiftedChat
         placeholder="Start typing ..."
         messages={messages}
-        user={{ _id: userId }}
+        user={{
+          _id: userId,
+          avatar: userDetails?.avatar,
+          name: `${userDetails?.firstName} ${userDetails?.lastName}`
+        }}
         alwaysShowSend
         onSend={onSend}
         renderSend={(props) => (
