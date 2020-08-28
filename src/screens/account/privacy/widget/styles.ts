8b9adbd319dab7c0bd1fb013a4b8d9e47cfe3deb
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-28 04:28:52 +0100

    add padding to privacy option

--- src/screens/account/privacy/widget/styles.ts
+++ src/screens/account/privacy/widget/styles.ts
@@ -3,3 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.RED};
 `;
+
+export const PrivacyOption = styled.View`
+  padding: 0 20px;
+`;
