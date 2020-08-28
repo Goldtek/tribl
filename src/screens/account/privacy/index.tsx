01dd01c4f50e39523c55ba4cd64c0f9f1355fe25
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-08-28 04:27:05 +0100

    get privacy data from backend

--- src/screens/account/privacy/index.tsx
+++ src/screens/account/privacy/index.tsx
@@ -1,12 +1,15 @@
 import React, { Fragment, useState, useCallback } from 'react';
 import { NavigationInterface } from '../../types';
 import { AntDesign } from '@expo/vector-icons';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { Text, TouchableRipple, Divider } from 'react-native-paper';
 import { Switch } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useThemeContext } from '../../../theme';
 import PrivacyModal from './widget';
+import { GET_USER_PASSPORT } from '../../../graphql/server/query';
+import { UPDATE_PASSPORT } from '../../../graphql/server/mutations';
 
 // IMPORT FOR ALL CUSTOM STYLES
 import { Container, ToggleContainer, ToggleCover } from './styles';
@@ -19,174 +22,123 @@ export default function ProfileScreen(props: MyConnectionScreenProp) {
   const { t } = useTranslation();
   const [isVisible, setIsVisible] = useState(false);
   const [isEnabled, setIsEnabled] = useState(false);
-  const [text, setText] = useState({ value: '' });
+  const [index, setIndex] = useState(Number || undefined);
+
+  const { data } = useQuery(GET_USER_PASSPORT);
+
+  const userDetails = data?.myPassport;
+  const privacySetting = userDetails?.privacy;
+
+  const [privacy, setPrivacy] = useState({
+    identity: privacySetting?.identity,
+    locality: privacySetting?.locality,
+    interest: privacySetting?.interest,
+    age: privacySetting?.age,
+    selectedData: null
+  });
+  const selectedData = privacy.selectedData;
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
   const showPrivacyModal = useCallback(
-    (isVisible: boolean) => () => {
+    (isVisible: boolean, index?: any) => () => {
       setIsVisible(isVisible);
+      setIndex(index);
+      if (index == 0) {
+        setPrivacy({
+          ...privacy,
+          selectedData: privacy?.identity
+        });
+      }
       return true;
     },
     []
   );
 
-  const getText = (childData: any) => {
-    setText({
-      ...text,
-      value: childData
-    });
+  const getPrivacySetting = (childData: any) => {
+    if (index == 0) {
+      setPrivacy({
+        ...privacy,
+        identity: childData
+      });
+    }
+    if (index == 1) {
+      setPrivacy({
+        ...privacy,
+        locality: childData,
+        selectedData: privacySetting?.locality
+      });
+    }
+    if (index == 2) {
+      setPrivacy({
+        ...privacy,
+        interest: childData,
+        selectedData: privacySetting?.interest
+      });
+    }
+    if (index == 3) {
+      setPrivacy({
+        ...privacy,
+        age: childData,
+        selectedData: privacySetting?.age
+      });
+    }
   };
+
+  const PrivacyItems = [
+    t(`community.accountSettings.identity`),
+    t(`community.accountSettings.locality`),
+    t(`community.accountSettings.interest`),
+    t(`community.accountSettings.age`)
+  ];
 
   return (
     <Fragment>
       <Container>
-        <TouchableRipple
-          onPress={() => {}}
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            height: RFValue(50),
-            paddingHorizontal: RFValue(25)
-          }}
-        >
+        {PrivacyItems.map((item: string, index: number) => (
           <Fragment>
-            <Text
+            <TouchableRipple
+              key={index}
+              onPress={showPrivacyModal(true, index)}
               style={{
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: fonts.LARGE_SIZE,
-                color: colors.PRIMARY_TEXT,
-                textTransform: 'capitalize'
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                height: RFValue(50),
+                paddingHorizontal: RFValue(25)
               }}
             >
-              {t(`community.accountSettings.identity`)}
-            </Text>
-            <AntDesign
-              name="caretright"
-              size={20}
-              color={colors.PRIMARY_TEXT}
-            />
+              <Fragment>
+                <Text
+                  style={{
+                    fontFamily: fonts.WORK_SANS_REGULAR,
+                    fontSize: fonts.LARGE_SIZE,
+                    color: colors.PRIMARY_TEXT,
+                    textTransform: 'capitalize'
+                  }}
+                >
+                  {item}
+                </Text>
+                {item == 'identity' ? (
+                  <Text>{privacy.identity}</Text>
+                ) : item == 'locality' ? (
+                  <Text>{privacy.locality}</Text>
+                ) : item == 'interest' ? (
+                  <Text>{privacy.interest}</Text>
+                ) : item == 'age' ? (
+                  <Text>{privacy.age}</Text>
+                ) : null}
+                <AntDesign
+                  name="caretright"
+                  size={20}
+                  color={colors.PRIMARY_TEXT}
+                />
+              </Fragment>
+            </TouchableRipple>
+            <Divider style={{ backgroundColor: colors.INPUT }} />
           </Fragment>
-        </TouchableRipple>
-        <Divider style={{ backgroundColor: colors.INPUT }} />
+        ))}
         <TouchableRipple
           onPress={() => {}}
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            height: RFValue(50),
-            paddingHorizontal: RFValue(25)
-          }}
-        >
-          <Fragment>
-            <Text
-              style={{
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: fonts.LARGE_SIZE,
-                color: colors.PRIMARY_TEXT,
-                textTransform: 'capitalize'
-              }}
-            >
-              {t(`community.accountSettings.locality`)}
-            </Text>
-            <AntDesign
-              name="caretright"
-              size={20}
-              color={colors.PRIMARY_TEXT}
-            />
-          </Fragment>
-        </TouchableRipple>
-        <Divider style={{ backgroundColor: colors.INPUT }} />
-        <TouchableRipple
-          onPress={() => {}}
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            height: RFValue(50),
-            paddingHorizontal: RFValue(25)
-          }}
-        >
-          <Fragment>
-            <Text
-              style={{
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: fonts.LARGE_SIZE,
-                color: colors.PRIMARY_TEXT,
-                textTransform: 'capitalize'
-              }}
-            >
-              {t(`community.accountSettings.interest`)}
-            </Text>
-            <AntDesign
-              name="caretright"
-              size={20}
-              color={colors.PRIMARY_TEXT}
-            />
-          </Fragment>
-        </TouchableRipple>
-        <Divider style={{ backgroundColor: colors.INPUT }} />
-        <TouchableRipple
-          onPress={() => {}}
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            height: RFValue(50),
-            paddingHorizontal: RFValue(25)
-          }}
-        >
-          <Fragment>
-            <Text
-              style={{
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: fonts.LARGE_SIZE,
-                color: colors.PRIMARY_TEXT,
-                textTransform: 'capitalize'
-              }}
-            >
-              {t(`community.accountSettings.age`)}
-            </Text>
-            <AntDesign
-              name="caretright"
-              size={20}
-              color={colors.PRIMARY_TEXT}
-            />
-          </Fragment>
-        </TouchableRipple>
-        <Divider style={{ backgroundColor: colors.INPUT }} />
-        <TouchableRipple
-          onPress={() => {}}
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            height: RFValue(50),
-            paddingHorizontal: RFValue(25)
-          }}
-        >
-          <Fragment>
-            <Text
-              style={{
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: fonts.LARGE_SIZE,
-                color: colors.PRIMARY_TEXT,
-                textTransform: 'capitalize'
-              }}
-            >
-              {t(`community.accountSettings.link`)}
-            </Text>
-            <AntDesign
-              name="caretright"
-              size={20}
-              color={colors.PRIMARY_TEXT}
-            />
-          </Fragment>
-        </TouchableRipple>
-        <Divider style={{ backgroundColor: colors.INPUT }} />
-        <TouchableRipple
-          onPress={showPrivacyModal(true)}
           style={{
             flexDirection: 'row',
             justifyContent: 'space-between',
@@ -206,7 +158,7 @@ export default function ProfileScreen(props: MyConnectionScreenProp) {
             >
               {t(`community.accountSettings.blocked`)}
             </Text>
-            <Text>{text.value}</Text>
+            <Text></Text>
             <AntDesign
               name="caretright"
               size={20}
@@ -215,6 +167,37 @@ export default function ProfileScreen(props: MyConnectionScreenProp) {
           </Fragment>
         </TouchableRipple>
         <Divider style={{ backgroundColor: colors.INPUT }} />
+        <TouchableRipple
+          onPress={() => {}}
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            height: RFValue(50),
+            paddingHorizontal: RFValue(25)
+          }}
+        >
+          <Fragment>
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: fonts.LARGE_SIZE,
+                color: colors.PRIMARY_TEXT,
+                textTransform: 'capitalize'
+              }}
+            >
+              {t(`community.accountSettings.passport`)}
+            </Text>
+            <Text></Text>
+            <AntDesign
+              name="caretright"
+              size={20}
+              color={colors.PRIMARY_TEXT}
+            />
+          </Fragment>
+        </TouchableRipple>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+
         <ToggleContainer>
           <Text
             style={{
@@ -250,10 +233,13 @@ export default function ProfileScreen(props: MyConnectionScreenProp) {
           </ToggleCover>
         </ToggleContainer>
       </Container>
+
       <PrivacyModal
         closePrivacyModal={showPrivacyModal(false)}
         isVisible={isVisible}
-        parentCallback={getText}
+        privacyValue={getPrivacySetting}
+        index={index}
+        selectedData={selectedData}
       />
     </Fragment>
   );
