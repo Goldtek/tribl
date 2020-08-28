1d9c320f4f103564366b2a6e34b90f90052d1707
josemak25 <amakirij@gmail.com>
2020-08-28 12:45:49 +0100

    add placeholder loader for direct messages slider

--- src/screens/inbox/home/widgets/directMessageSlide/index.tsx
+++ src/screens/inbox/home/widgets/directMessageSlide/index.tsx
@@ -1,48 +1,79 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { FlatList } from 'react-native';
-import { Text } from 'react-native-paper';
 import { RFValue } from 'react-native-responsive-fontsize';
 import MemberCard from '../chatMemberCard';
 import { NavigationInterface } from '../../../../types';
 import Firechat from '../../../../../firebase';
 import { useThemeContext } from '../../../../../theme';
 import { GroupInterface } from '../../../types';
+import { fireAuth } from '../../../../../firebase/config';
+import ChatCardSkeleton from '../../../../../components/chatCardSkeleton';
 
 import { Container } from './styles';
+import { Text } from 'react-native-paper';
 
 // DEFINE SCREEN PROP TYPES
 interface ScreenProp extends NavigationInterface {}
 
 export default function DirectDMScreen(props: ScreenProp) {
   const { colors, fonts } = useThemeContext();
+  const userId = fireAuth.currentUser?.uid as string;
+
+  const [state, setState] = useState(true);
 
   const [directMessages, setDirectMessages] = useState<GroupInterface[]>([]);
 
   useEffect(() => {
     (async () => {
       const directMessages = await Firechat.getUserDirectMessages();
-      if (directMessages) setDirectMessages(directMessages);
+      if (!directMessages) setState(false);
+
+      const unsubscribe = directMessages?.onSnapshot({
+        next: (snapshot) => {
+          const groupConversations = snapshot.docs.map((documentSnapshot) => {
+            const message = documentSnapshot.data() as GroupInterface;
+
+            const [directMessageIcons] = message.members.filter(
+              ({ receiverId }) => receiverId !== userId
+            );
+
+            return {
+              ...message,
+              name: directMessageIcons.title,
+              avatar: directMessageIcons.avatar
+            };
+          });
+
+          setDirectMessages(groupConversations);
+        }
+      });
+
+      return () => unsubscribe && unsubscribe();
     })();
   }, []);
 
-  const _renderItem = ({ item }: any) => (
-    <MemberCard key={item.id} {...item} {...props} />
+  const _renderItem = ({ item }: { item: GroupInterface }) => (
+    <MemberCard key={item.id} {...item} />
   );
 
   const renderEmptyList = useMemo(
-    () => () => (
-      <Container>
+    () => () =>
+      state ? (
+        <Container>
+          <ChatCardSkeleton skeletonSize={3} />
+        </Container>
+      ) : (
         <Text
           style={{
-            color: colors.PRIMARY_TEXT,
-            fontFamily: fonts.WORK_SANS_REGULAR,
-            fontSize: RFValue(fonts.LARGE_SIZE)
+            fontSize: RFValue(fonts.LARGE_SIZE),
+            fontFamily: fonts.WORK_SANS_BOLD,
+            margin: RFValue(20),
+            textAlign: 'center'
           }}
         >
-          No chats yet
+          You currently don't have any messages
         </Text>
-      </Container>
-    ),
+      ),
     []
   );
 
