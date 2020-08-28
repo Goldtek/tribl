a1819a12d8462e0ca32f4f8313dde8f9555a9ce1
josemak25 <amakirij@gmail.com>
2020-08-28 12:32:06 +0100

    add connection card screen component

--- src/libs/updates/index.ts
+++ src/libs/updates/index.ts
@@ -2,23 +2,23 @@ import * as Updates from 'expo-updates';
 import { Alert } from 'react-native';
 
 export default async function checkAppUpdates() {
-  Alert.alert(
-    'UPDATE CHECK',
-    'NOW CHECKING FOR UPDATE',
-    [
-      {
-        text: 'Ask me later',
-        onPress: () => console.log('Ask me later pressed')
-      },
-      {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel'
-      },
-      { text: 'OK', onPress: () => console.log('OK Pressed') }
-    ],
-    { cancelable: true }
-  );
+  // Alert.alert(
+  //   'UPDATE CHECK',
+  //   'NOW CHECKING FOR UPDATE',
+  //   [
+  //     {
+  //       text: 'Ask me later',
+  //       onPress: () => console.log('Ask me later pressed')
+  //     },
+  //     {
+  //       text: 'Cancel',
+  //       onPress: () => console.log('Cancel Pressed'),
+  //       style: 'cancel'
+  //     },
+  //     { text: 'OK', onPress: () => console.log('OK Pressed') }
+  //   ],
+  //   { cancelable: true }
+  // );
 
   const update = await Updates.checkForUpdateAsync();
   if (update.isAvailable) {
