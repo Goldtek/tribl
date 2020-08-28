5edc0f9ee9317a85ea0c1f23183a704b97251225
josemak25 <amakirij@gmail.com>
2020-08-28 12:23:17 +0100

    removed unused codes from popular community skeleton loader

--- src/components/popularCommunitySkeleton/index.tsx
+++ src/components/popularCommunitySkeleton/index.tsx
@@ -1,27 +1,18 @@
 import React, { Fragment } from 'react';
-import { Title, Paragraph, TouchableRipple } from 'react-native-paper';
-import { RFValue } from 'react-native-responsive-fontsize';
-import FastImage from 'react-native-fast-image';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
-import { useTranslation } from 'react-i18next';
-import { useThemeContext } from '../../theme';
-import { useNavigation } from '@react-navigation/native';
-
-// IMPORT FOR ALL CUSTOM STYLES
-import { TextConatiner, Container } from './styles';
 
 // DEFINE SCREEN PROP TYPES
 interface PopularUserSkeletonProp {
-  skelentonSize?: number;
+  skeletonSize?: number;
 }
 
 export default function PopularCommunitySkeleton({
-  skelentonSize = 1
+  skeletonSize = 1
 }: PopularUserSkeletonProp) {
   return (
     <Fragment>
-      {[...Array(skelentonSize)].map((_, index) => (
-        <SkeletonPlaceholder>
+      {[...Array(skeletonSize)].map((_, index) => (
+        <SkeletonPlaceholder key={index.toString()}>
           <SkeletonPlaceholder.Item
             marginTop={10}
             marginLeft={15}
