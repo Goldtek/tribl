a12a264342cd3a90042c1b9794da51cfe29edd97
josemak25 <amakirij@gmail.com>
2020-09-03 19:34:08 +0100

    add app update modal fix

--- src/libs/updates/styles.ts
+++ src/libs/updates/styles.ts
@@ -14,22 +14,22 @@ export const Container = styled(BlurView)`
 export const BlurContents = styled.View`
   flex: 1;
   align-items: center;
-  padding: 30px 30px 0px 30px;
+  justify-content: center;
+  padding: 0px 30px 0px 30px;
   border: transparent;
 `;
 
 export const BlurContentsContainer = styled(Surface)`
-  height: 76%;
   background-color: ${({ theme }) => theme.colors.GREY};
   padding: 20px 10px;
   border-radius: 4px;
 `;
 
 export const ButtonContainer = styled.View`
-  flex: 1;
   flex-direction: row;
   justify-content: space-between;
   align-items: flex-end;
+  margin-top: 30px;
 `;
 
 export const ImageContainer = styled.View`
