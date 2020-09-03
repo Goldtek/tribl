87131631e95fc5ab4bd4620b3beea86c02c87dee
josemak25 <amakirij@gmail.com>
2020-09-03 13:07:18 +0100

    add global error boundary HOC to app default router

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,25 @@ import { RootToaster } from './components/rootToaster';
 import { Host as PortalHost } from 'react-native-portalize';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import GlobalErrorBoundary from './libs/error';
 import ApolloProvider from './graphql';
 import ThemeProvider from './theme';
 import Router from './router';
 
 export default function AppRouter() {
   return (
-    <ApolloProvider>
-      <SafeAreaProvider>
-        <ThemeProvider>
-          <StatusBar translucent animated style="dark" />
-          <RootToaster />
-          <PortalHost>
-            <Router />
-          </PortalHost>
-        </ThemeProvider>
-      </SafeAreaProvider>
-    </ApolloProvider>
+    <GlobalErrorBoundary>
+      <ApolloProvider>
+        <SafeAreaProvider>
+          <ThemeProvider>
+            <StatusBar translucent animated style="dark" />
+            <RootToaster />
+            <PortalHost>
+              <Router />
+            </PortalHost>
+          </ThemeProvider>
+        </SafeAreaProvider>
+      </ApolloProvider>
+    </GlobalErrorBoundary>
   );
 }
