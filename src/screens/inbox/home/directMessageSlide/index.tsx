0af56284e700b6f60736deddc13abfbbe08f37da
josemak25 <amakirij@gmail.com>
2020-09-15 13:08:45 +0100

    add fix for direct message slide and direct message card

---
+++ src/screens/inbox/home/directMessageSlide/index.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import { FlatList } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { Text } from 'react-native-paper';
+import DirectChatCard from './widget';
+import { NavigationInterface } from '../../../types';
+import Firechat from '../../../../firebase';
+import { useThemeContext } from '../../../../theme';
+import { ConversationInterface } from '../../types';
+import ChatCardSkeleton from '../../../../components/chatCardSkeleton';
+import { ROOM_TYPES } from '../../../../firebase/types';
+
+import { Container } from './styles';
+
+// DEFINE SCREEN PROP TYPES
+interface ScreenProp extends NavigationInterface {}
+
+export default function DirectDMScreen(props: ScreenProp) {
+  const { fonts } = useThemeContext();
+
+  const [chatHistory, setChatHistory] = useState(true);
+
+  const [directMessages, setDirectMessages] = useState<ConversationInterface[]>(
+    []
+  );
+
+  useEffect(() => {
+    let unsubscribe: any = null;
+
+    (async () => {
+      const userConservations = await Firechat.getUserConversations(
+        ROOM_TYPES.CONVERSATIONS
+      );
+
+      unsubscribe = userConservations?.onSnapshot({
+        next: async (snapshot) => {
+          if (!snapshot.docs.length) return setChatHistory(false);
+
+          const conversationIds = snapshot.docs.map((document) => document.id);
+
+          const userDirectMessages = await Firechat.getConversationMessages(
+            conversationIds
+          );
+
+          userDirectMessages?.onSnapshot({
+            next: (snapshot) => {
+              const directMessages = snapshot.docs.map((document) => {
+                const message = document.data() as ConversationInterface;
+                return { ...message, id: document.id };
+              });
+
+              setDirectMessages(directMessages);
+            }
+          });
+        }
+      });
+    })();
+
+    return () => unsubscribe && unsubscribe();
+  }, []);
+
+  const _renderItem = ({ item }: { item: ConversationInterface }) => (
+    <DirectChatCard key={item.id} {...item} />
+  );
+
+  const renderEmptyList = useMemo(
+    () => () => (
+      <Container>
+        <ChatCardSkeleton skeletonSize={3} />
+      </Container>
+    ),
+    []
+  );
+
+  return chatHistory ? (
+    <FlatList
+      data={directMessages}
+      contentContainerStyle={{
+        flexGrow: 1,
+        marginTop: RFValue(20),
+        paddingBottom: RFValue(20)
+      }}
+      ListEmptyComponent={renderEmptyList}
+      showsVerticalScrollIndicator={false}
+      renderItem={_renderItem}
+      keyExtractor={(item) => item.id}
+    />
+  ) : (
+    <Text
+      style={{
+        fontSize: RFValue(fonts.LARGE_SIZE),
+        fontFamily: fonts.WORK_SANS_BOLD,
+        margin: RFValue(20),
+        textAlign: 'center'
+      }}
+    >
+      You currently don't have any messages
+    </Text>
+  );
+}
