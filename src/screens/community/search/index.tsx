33b5bc0daf9010cee9ef4662e7d44627ec425f70
josemak25 <amakirij@gmail.com>
2020-09-16 10:31:22 +0100

    add fix to community search view slider and implement no bounce on community slider

--- src/screens/community/search/index.tsx
+++ src/screens/community/search/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { NavigationInterface } from '../../types';
 import { useTranslation } from 'react-i18next';
-import { TabView, SceneMap, TabBar, ScrollPager } from 'react-native-tab-view';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import { DEVICE_FULL_WIDTH } from '../../../utils/device';
 import { Title } from 'react-native-paper';
 import { RFValue } from 'react-native-responsive-fontsize';
-import { Platform } from 'react-native';
 import { useThemeContext } from '../../../theme';
 import communitySlide from './widgets/communitySlide';
 import { StatusBar } from 'expo-status-bar';
@@ -25,6 +24,7 @@ export default function SearchScreen(props: ScreenProp) {
   const {
     params: { index }
   } = props.route;
+
   const { colors, fonts } = useThemeContext();
   const { t } = useTranslation();
   const [changeCommunitySearchIndex] = useMutation(ADD_COMMUNITY_SEARCH_INDEX);
@@ -86,37 +86,18 @@ export default function SearchScreen(props: ScreenProp) {
     );
   };
 
-  const renderPager = (props: any) => <ScrollPager {...props} />;
-
   return (
     <Container>
       <StatusBar translucent animated style="dark" />
-      {Platform.select({
-        ios: (
-          <TabView
-            lazy
-            renderScene={renderScene}
-            renderPager={renderPager}
-            renderTabBar={renderTabBar}
-            onIndexChange={handleIndexChange}
-            navigationState={{ index: tabIndex, routes }}
-            initialLayout={{ width: DEVICE_FULL_WIDTH }}
-            swipeEnabled={false}
-          />
-        ),
-
-        android: (
-          <TabView
-            lazy
-            renderScene={renderScene}
-            renderTabBar={renderTabBar}
-            onIndexChange={handleIndexChange}
-            navigationState={{ index: tabIndex, routes }}
-            initialLayout={{ width: DEVICE_FULL_WIDTH }}
-            swipeEnabled={false}
-          />
-        )
-      })}
+      <TabView
+        lazy
+        renderScene={renderScene}
+        renderTabBar={renderTabBar}
+        onIndexChange={handleIndexChange}
+        navigationState={{ index: tabIndex, routes }}
+        initialLayout={{ width: DEVICE_FULL_WIDTH }}
+        swipeEnabled={false}
+      />
     </Container>
   );
 }
