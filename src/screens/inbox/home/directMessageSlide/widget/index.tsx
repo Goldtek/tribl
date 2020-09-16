b36db6de356ee75495f6f9db4f182eb19ee4a5ba
josemak25 <amakirij@gmail.com>
2020-09-16 11:32:36 +0100

    add user id filter to each message returned from firestore

--- src/screens/inbox/home/directMessageSlide/widget/index.tsx
+++ src/screens/inbox/home/directMessageSlide/widget/index.tsx
@@ -10,6 +10,7 @@ import { useThemeContext } from '../../../../../theme';
 import formatMessageTime from '../../../../../utils/timesince';
 import { GET_SINGLE_PASSPORT } from '../../../../../graphql/server/query';
 import { UserPassportInterface } from '../../../../../graphql/types';
+import { fireAuth } from '../../../../../firebase/config';
 
 // IMPORT FOR ALL CUSTOM STYLES
 import { NameContainer, TimeStamp, BadgeWrapper } from './styles';
@@ -23,8 +24,10 @@ function DirectChatCard(props: DirectChatProp) {
 
   const { id: chatId, lastMessage, members } = props;
 
+  const userId = fireAuth.currentUser?.uid;
+
   const [sender, receiver] = members.sort((a) => {
-    if (a.id !== lastMessage.receiverId) return -1;
+    if (a.id !== userId) return -1;
     return 0;
   });
 
@@ -39,7 +42,7 @@ function DirectChatCard(props: DirectChatProp) {
     navigation.navigate('DirectChatScreen', {
       title: `${receiverPassport?.firstName} ${receiverPassport?.lastName}`,
       avatar: receiverPassport?.avatar,
-      receiverId: lastMessage.receiverId,
+      receiverId: receiverPassport?.id,
       chatId
     });
   }, [loading]);
@@ -99,13 +102,16 @@ function DirectChatCard(props: DirectChatProp) {
                 {`${receiverPassport?.firstName} ${receiverPassport?.lastName}`}
               </Title>
               <Text
+                numberOfLines={1}
                 style={{
                   color: colors.SECONDARY_TEXT,
                   fontFamily: fonts.WORK_SANS_REGULAR,
                   fontSize: RFValue(fonts.MEDIUM_SIZE)
                 }}
               >
-                {lastMessage.text.substr(0, 30)}...
+                {lastMessage.text.length >= 30
+                  ? `${lastMessage.text.substr(0, 30)}...`
+                  : lastMessage.text}
               </Text>
             </Fragment>
           ) : (
