879aebc17fea50b80cf53359e05994d63292fb1e
josemak25 <amakirij@gmail.com>
2020-09-20 19:32:01 +0100

    extend _neo4j date typings

--- src/graphql/index.tsx
+++ src/graphql/index.tsx
@@ -21,7 +21,6 @@ const httpLink = new HttpLink({
 // Create a WebSocket link:
 const wsLink = new WebSocketLink({
   uri: `wss://${ENVIRONMENT_VARIABLES.TRIBL_SERVER_BASE_URI}`,
-  lazy: true,
   options: { reconnect: true }
 });
 
@@ -31,6 +30,7 @@ const link = split(
   // split based on operation type
   ({ query }) => {
     const definition = getMainDefinition(query);
+
     return (
       definition.kind === 'OperationDefinition' &&
       definition.operation === 'subscription'
