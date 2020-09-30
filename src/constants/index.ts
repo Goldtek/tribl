75f690a55a68717061c2577e08188b7b9dd549c9
josemak25 <amakirij@gmail.com>
2020-09-30 09:55:09 +0100

    add width to header global style constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -18,7 +18,7 @@ export const LANGUAGE_DEFAULT: string = 'en';
 // APP HEADER SETTING
 export const GLOBAL_HEADER_STYLE = {
   shadowOpacity: 0,
-  shadowOffset: { height: 0 },
+  shadowOffset: { height: 0, width: 0 },
   shadowRadius: 0,
   elevation: 0
 };
