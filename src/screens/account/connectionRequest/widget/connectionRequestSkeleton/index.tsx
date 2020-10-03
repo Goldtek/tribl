9e18276e98fca996932aa2fc823e347d0bb4df9c
josemak25 <amakirij@gmail.com>
2020-10-03 17:16:05 +0100

    add fix to connection request skeleton

--- src/screens/account/connectionRequest/widget/connectionRequestSkeleton/index.tsx
+++ src/screens/account/connectionRequest/widget/connectionRequestSkeleton/index.tsx
@@ -8,95 +8,36 @@ interface ConnectionRequestkeletonProps {
 }
 
 export default function ConnectionRequestSkeleton({
-  skeletonSize = 1
+  skeletonSize = 3
 }: ConnectionRequestkeletonProps) {
   return (
     <Fragment>
       {[...Array(skeletonSize)].map((_, index) => (
         <SkeletonPlaceholder key={index.toString()}>
           <SkeletonPlaceholder.Item
-            marginTop={RFValue(30)}
+            marginTop={RFValue(10)}
             marginLeft={RFValue(15)}
             flexDirection="row"
             alignItems="center"
           >
             <SkeletonPlaceholder.Item
-              width={RFValue(70)}
-              height={RFValue(70)}
-              borderRadius={RFValue(15)}
+              width={RFValue(50)}
+              height={RFValue(50)}
+              borderRadius={RFValue(5)}
             />
             <SkeletonPlaceholder.Item
               justifyContent="center"
-              marginLeft={RFValue(40)}
+              marginLeft={RFValue(10)}
             >
               <SkeletonPlaceholder.Item
-                width={RFValue(150)}
-                height={RFValue(20)}
+                width={RFValue(130)}
+                height={RFValue(15)}
                 borderRadius={RFValue(4)}
                 marginTop={RFValue(10)}
               />
               <SkeletonPlaceholder.Item
-                width={RFValue(150)}
-                height={RFValue(20)}
-                borderRadius={RFValue(4)}
-                marginTop={RFValue(10)}
-              />
-            </SkeletonPlaceholder.Item>
-          </SkeletonPlaceholder.Item>
-          <SkeletonPlaceholder.Item
-            marginTop={RFValue(30)}
-            marginLeft={RFValue(15)}
-            flexDirection="row"
-            alignItems="center"
-          >
-            <SkeletonPlaceholder.Item
-              width={RFValue(70)}
-              height={RFValue(70)}
-              borderRadius={RFValue(15)}
-            />
-            <SkeletonPlaceholder.Item
-              justifyContent="center"
-              marginLeft={RFValue(40)}
-            >
-              <SkeletonPlaceholder.Item
-                width={RFValue(150)}
-                height={RFValue(20)}
-                borderRadius={RFValue(4)}
-                marginTop={RFValue(10)}
-              />
-              <SkeletonPlaceholder.Item
-                width={RFValue(150)}
-                height={RFValue(20)}
-                borderRadius={RFValue(4)}
-                marginTop={RFValue(10)}
-              />
-            </SkeletonPlaceholder.Item>
-          </SkeletonPlaceholder.Item>
-
-          <SkeletonPlaceholder.Item
-            marginTop={RFValue(30)}
-            marginLeft={RFValue(15)}
-            flexDirection="row"
-            alignItems="center"
-          >
-            <SkeletonPlaceholder.Item
-              width={RFValue(70)}
-              height={RFValue(70)}
-              borderRadius={RFValue(15)}
-            />
-            <SkeletonPlaceholder.Item
-              justifyContent="center"
-              marginLeft={RFValue(40)}
-            >
-              <SkeletonPlaceholder.Item
-                width={RFValue(150)}
-                height={RFValue(20)}
-                borderRadius={RFValue(4)}
-                marginTop={RFValue(10)}
-              />
-              <SkeletonPlaceholder.Item
-                width={RFValue(150)}
-                height={RFValue(20)}
+                width={RFValue(100)}
+                height={RFValue(8)}
                 borderRadius={RFValue(4)}
                 marginTop={RFValue(10)}
               />
