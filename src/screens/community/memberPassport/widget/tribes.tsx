e590c76831790161545dd8ff9da110bf4b7efcb0
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-10-14 22:06:22 +0100

    create tribes component

---
+++ src/screens/community/memberPassport/widget/tribes.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { TouchableRipple } from 'react-native-paper';
+import { RFValue } from 'react-native-responsive-fontsize';
+import FastImage from 'react-native-fast-image';
+import { useThemeContext } from '../../../../theme';
+import { useNavigation } from '@react-navigation/native';
+
+// DEFINE SCREEN PROP TYPES
+interface MyCommunityProp {
+  name: string;
+  membersCount: string;
+  id: string;
+  avatar: string;
+  isMember: boolean;
+  interests: [];
+  description: string;
+}
+
+export default function MyCommunity(props: MyCommunityProp) {
+  const { colors } = useThemeContext();
+  const navigation = useNavigation();
+
+  const { avatar, name } = props;
+
+  const handleNavigation = () =>
+    navigation.navigate('CommunityDetailScreen', {
+      title: name,
+      details: props
+    });
+
+  return (
+    <TouchableRipple
+      onPress={handleNavigation}
+      rippleColor={colors.PRIMARY}
+      style={{
+        height: RFValue(50),
+        width: RFValue(50),
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: RFValue(10)
+      }}
+    >
+      <FastImage
+        resizeMode={FastImage.resizeMode.cover}
+        source={{
+          uri: avatar,
+          priority: FastImage.priority.high
+        }}
+        style={{ width: '100%', height: '100%', borderRadius: RFValue(4) }}
+      />
+    </TouchableRipple>
+  );
+}
