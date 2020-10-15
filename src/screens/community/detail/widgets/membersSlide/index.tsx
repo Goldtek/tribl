0319e66de3846a6bad31783c5bf64bbf084272d8
josemak25 <amakirij@gmail.com>
2020-10-15 17:46:18 +0100

    add typings to member slide

--- src/screens/community/detail/widgets/membersSlide/index.tsx
+++ src/screens/community/detail/widgets/membersSlide/index.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { FlatList } from 'react-native';
-import { NavigationInterface } from '../../../../types';
 import { Title } from 'react-native-paper';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from '@apollo/react-hooks';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useThemeContext } from '../../../../../theme';
 import MemberCard from './widget/member';
+import { NavigationInterface } from '../../../../types';
 import AlgoliaSearch from '../../../../../components/algoliaSearch';
 import AlgoliaList from '../../../../../components/algoliaCommunityMembersList';
 import { GET_COMMUNITY_MEMBERS } from '../../../../../graphql/server/query';
+import {
+  CommunityInterface,
+  PassportInterface
+} from '../../../../../graphql/types';
 import ENVIRONMENT_VARIABLES from '../../../../../config';
+import { CommunityMembersRequestInterface } from '../../../../../graphql/types';
 
 // IMPORT FOR ALL CUSTOM STYLES
 import { Container } from './styles';
 
 // DEFINE SCREEN PROP TYPES
 interface MemberSlideProp extends NavigationInterface {
-  route: { communityDetails: { id: string } };
+  route: { communityDetails: CommunityInterface };
 }
 
 export default function MemberSlide(props: MemberSlideProp) {
@@ -26,13 +31,16 @@ export default function MemberSlide(props: MemberSlideProp) {
   const { colors, fonts } = useThemeContext();
   const { t } = useTranslation();
 
-  const { data } = useQuery(GET_COMMUNITY_MEMBERS, {
-    variables: { id: communityDetails.id }
-  });
+  const { data } = useQuery<CommunityMembersRequestInterface>(
+    GET_COMMUNITY_MEMBERS,
+    { variables: { id: communityDetails.id } }
+  );
 
-  const participants = data?.communityMembers || [];
+  const participants = data?.communityMembers;
 
-  const _renderItem = ({ item }: any) => <MemberCard key={item.id} {...item} />;
+  const _renderItem = ({ item }: { item: PassportInterface }) => (
+    <MemberCard key={item.id} {...item} />
+  );
 
   return (
     <Container>
