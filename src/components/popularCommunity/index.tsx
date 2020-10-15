833cd92c309b0c4e939c9caee05daa63e9b8ba03
josemak25 <amakirij@gmail.com>
2020-10-15 10:29:20 +0100

    add fix for popular community typings fix

--- src/components/popularCommunity/index.tsx
+++ src/components/popularCommunity/index.tsx
@@ -18,7 +18,7 @@ interface PopularCommunityProp {
   avatar: string;
   name: string;
   id: string;
-  membersCount: string;
+  membersCount: number;
   isMember: boolean;
   interests: [];
   description: string;
@@ -100,7 +100,9 @@ function PopularCommunity(props: PopularCommunityProp) {
               color: colors.SECONDARY_TEXT
             }}
           >
-            {membersCount} {t(`community.tabPanel.member`)}
+            {membersCount <= 1
+              ? `${membersCount} ${t(`community.tabPanel.member`)}`
+              : `${membersCount} ${t(`community.tabPanel.member`)}s`}
           </Paragraph>
           {isMember || member ? (
             <Paragraph
