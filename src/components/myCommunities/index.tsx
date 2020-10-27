a7a1fe37ab957729aae9a12ed25ff3d7d1af29ff
josemak25 <amakirij@gmail.com>
2020-10-27 08:31:08 +0100

    add get communites members on my communities component

--- src/components/myCommunities/index.tsx
+++ src/components/myCommunities/index.tsx
@@ -2,21 +2,35 @@ import React from 'react';
 import { TouchableRipple } from 'react-native-paper';
 import { RFValue } from 'react-native-responsive-fontsize';
 import FastImage from 'react-native-fast-image';
+import { useQuery } from '@apollo/react-hooks';
 import { useThemeContext } from '../../theme';
 import { useNavigation } from '@react-navigation/native';
-import { CommunityInterface } from '../../graphql/types';
+import { CommunityInterface, MyPassportInterface } from '../../graphql/types';
+import {
+  GET_COMMUNITY_CHANNELS,
+  GET_COMMUNITY_MEMBERS,
+  GET_USER_PASSPORT
+} from '../../graphql/server/query';
 
 export default function MyCommunity(props: CommunityInterface) {
   const { colors } = useThemeContext();
   const navigation = useNavigation();
 
-  const { avatar, name } = props;
+  const { avatar, name, id } = props;
 
-  const handleNavigation = () =>
+  const { data: userData } = useQuery<MyPassportInterface>(GET_USER_PASSPORT);
+
+  useQuery(GET_COMMUNITY_MEMBERS, { variables: { id } });
+  useQuery(GET_COMMUNITY_CHANNELS, {
+    variables: { communityId: id, userId: userData?.myPassport.id }
+  });
+
+  const handleNavigation = () => {
     navigation.navigate('CommunityDetailScreen', {
       title: name,
       details: props
     });
+  };
 
   return (
     <TouchableRipple
