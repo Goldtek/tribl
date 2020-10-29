61cdd1da8827ce74fe162f4fe5b8c0b6fb772e87
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-10-29 14:10:45 +0100

    integrate react native ux cam

--- App.tsx
+++ App.tsx
@@ -2,10 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { enableScreens } from 'react-native-screens';
 import loadResources from './src/libs/loadResources';
 import './src/internationalization';
+//@ts-ignore
+import RNUxcam from 'react-native-ux-cam';
 import AppRouter from './src';
 import './src/config';
 
 enableScreens();
+
+RNUxcam.optIntoSchematicRecordings(); // Add this line to enable iOS screen recordings
+RNUxcam.startWithKey('2yh4dpwexde1d1y');
 
 export default function App() {
   const [isAppReady, setIsAppReady] = useState(false);
