fde058b50d5ca27ce27be849ce2b6ae9a268ab01
josemak25 <amakirij@gmail.com>
2020-11-14 17:18:06 +0100

    add mix panel configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,8 @@
 import Reactotron from 'reactotron-react-native';
+//@ts-ignore
+import RNUxcam from 'react-native-ux-cam';
 import ENVIRONMENT_VARIABLES from 'react-native-config';
+import MixpanelAnalytics from '../libs/mixpanel';
 
 declare global {
   interface Console {
@@ -15,5 +18,13 @@ if (__DEV__) {
   //@ts-ignore
   console.tron = Reactotron.log;
 }
+
+RNUxcam.optIntoSchematicRecordings();
+RNUxcam.startWithKey(ENVIRONMENT_VARIABLES.TRIBL_UX_CAM);
+RNUxcam.setAutomaticScreenNameTagging(false);
+
+export const Mixpanel = new MixpanelAnalytics(
+  ENVIRONMENT_VARIABLES.TRIBL_MIX_PANEL
+);
 
 export default ENVIRONMENT_VARIABLES;
