fb3de8cba23134ea66a272da3f067ba30bd841ae
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-11-25 00:00:29 +0100

    use RFValues

--- assets/icons/star.tsx
+++ assets/icons/star.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
-import Svg, { SvgProps, Path } from 'react-native-svg';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Svg, { Path } from 'react-native-svg';
 import { IconProps } from './types';
 
 function SvgComponent(props: IconProps) {
   return (
-    <Svg width={24} height={17} viewBox="0 0 139 132" fill="none" {...props}>
+    <Svg
+      width={RFValue(20)}
+      height={RFValue(13)}
+      viewBox="0 0 139 132"
+      fill="none"
+      {...props}
+    >
       <Path
         d="M69.5 0l16.277 50.096h52.675L95.837 81.058l16.277 50.096L69.5 100.193l-42.614 30.961 16.277-50.096L.548 50.096h52.675L69.5 0z"
         fill="#A3AAC2"
--- assets/icons/tribeAdmin.tsx
+++ assets/icons/tribeAdmin.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
-import Svg, { SvgProps, Path } from 'react-native-svg';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Svg, { Path } from 'react-native-svg';
 import { IconProps } from './types';
 
 function SvgComponent(props: IconProps) {
   return (
-    <Svg width={39} height={29} viewBox="0 0 179 269" fill="none" {...props}>
+    <Svg
+      width={RFValue(34)}
+      height={RFValue(24)}
+      viewBox="0 0 179 269"
+      fill="none"
+      {...props}
+    >
       <Path d="M0 0h179v157.771L89.5 269 0 157.771V0z" fill="#535D7E" />
     </Svg>
   );
