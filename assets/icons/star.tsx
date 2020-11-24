139c94ce6133023b5cb0956a6307d4061fbf774a
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-11-24 23:51:19 +0100

    change color

--- assets/icons/star.tsx
+++ assets/icons/star.tsx
@@ -7,7 +7,7 @@ function SvgComponent(props: IconProps) {
     <Svg width={24} height={17} viewBox="0 0 139 132" fill="none" {...props}>
       <Path
         d="M69.5 0l16.277 50.096h52.675L95.837 81.058l16.277 50.096L69.5 100.193l-42.614 30.961 16.277-50.096L.548 50.096h52.675L69.5 0z"
-        fill="#718CFB"
+        fill="#A3AAC2"
       />
     </Svg>
   );
