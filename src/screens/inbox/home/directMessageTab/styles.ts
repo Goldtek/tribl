3686188846fe92185dd497564e3839ff7e05cd4c
josemak25 <amakirij@gmail.com>
2020-11-30 09:52:06 +0100

    remove message request from inbox navigator

--- src/screens/inbox/home/directMessageTab/styles.ts
+++ src/screens/inbox/home/directMessageTab/styles.ts
@@ -1,6 +1,25 @@
 import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { TouchableRipple } from 'react-native-paper';
 
 export const Container = styled.View`
   padding: ${RFValue(15)}px;
+`;
+
+export const MessageRequestContainer = styled(TouchableRipple)`
+  height: ${RFValue(30)}px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 10px;
+  margin-top: 5px;
+  border-top-width: 1px;
+  border-bottom-width: 1px;
+  border-color: ${({ theme }) => theme.colors.SECONDARY_TEXT};
+`;
+
+export const MessageRequestBadgeContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
