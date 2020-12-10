6dc63d498e6ad18ea4033f2a874dbe3ab414543b
josemak25 <amakirij@gmail.com>
2020-12-10 12:11:47 +0100

    add user online presence fix

--- App.tsx
+++ App.tsx
@@ -6,11 +6,14 @@ import {
 } from 'react-native';
 //@ts-ignore
 import RNUxcam from 'react-native-ux-cam';
+import database from '@react-native-firebase/database';
 import { enableScreens } from 'react-native-screens';
+import firestore from '@react-native-firebase/firestore';
 import loadResources from './src/libs/loadResources';
 import './src/internationalization';
 import AppRouter from './src';
 import { Mixpanel } from './src/config';
+import { fireAuth } from './src/firebase/config';
 
 enableScreens();
 
@@ -23,9 +26,7 @@ export default function App() {
   let uxcamEvent: EventSubscription;
   useEffect(() => {
     _uxcamSessionStartListener();
-    return () => {
-      uxcamEvent.remove();
-    };
+    return () => uxcamEvent.remove();
   }, []);
 
   //Setup listener
@@ -43,6 +44,23 @@ export default function App() {
       }
     });
   }
+
+  useEffect(() => {
+    // Assuming user is logged in
+    const userId = fireAuth.currentUser?.uid;
+
+    const reference = database().ref(`/presence/${userId}`);
+
+    // Set the /users/:userId value to true
+    reference.set({ lastSeen: Date.now(), status: 'ONLINE' });
+
+    // Remove the node whenever the client disconnects
+    return () => {
+      reference
+        .onDisconnect()
+        .update({ lastSeen: Date.now(), status: 'OFFLINE' });
+    };
+  }, []);
 
   const loadApp = async () => {
     await loadResources();
