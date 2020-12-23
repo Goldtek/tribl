3ba252770e3d59db3d181eb5771ee711bc064cbf
josemak25 <amakirij@gmail.com>
2020-12-23 22:16:48 +0100

    remove close modal method on all algolia search card components

---
+++ src/components/algoliaList/index.tsx
@@ -0,0 +1,106 @@
+import React, { useMemo, Fragment } from 'react';
+import {
+  connectInfiniteHits,
+  connectStateResults
+} from 'react-instantsearch-native';
+import { Divider, Text } from 'react-native-paper';
+import { FlatList } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+import hexToRGB from '../../utils/hexToRGB';
+import { useThemeContext } from '../../theme';
+import Highlight from '../algoliaHighlight';
+
+function AlgoliaList(props: any) {
+  const { colors, fonts } = useThemeContext();
+  const { hits, hasMore, refineNext } = props;
+
+  const _separator = useMemo(
+    () => () => (
+      <Divider
+        style={{
+          height: 1.5,
+          backgroundColor: hexToRGB(colors.INACTIVE, 0.5)
+        }}
+      />
+    ),
+    []
+  );
+
+  const _renderItem = useMemo(
+    () => ({ item }: any) => <Highlight attribute="id" hit={item} />,
+    []
+  );
+
+  const Results = connectStateResults(
+    ({ searchState, searchResults, children }: any) => {
+      return searchResults && searchResults.nbHits !== 0 ? (
+        children
+      ) : searchState && !searchState.query ? (
+        <Fragment>
+          {[...Array(5)].map((_, index) => (
+            <SkeletonPlaceholder key={`skeleton${index}`}>
+              <SkeletonPlaceholder.Item
+                flexDirection="row"
+                alignItems="center"
+                margin={10}
+              >
+                <SkeletonPlaceholder.Item
+                  width={60}
+                  height={60}
+                  borderRadius={4}
+                />
+                <SkeletonPlaceholder.Item marginLeft={20}>
+                  <SkeletonPlaceholder.Item
+                    width={150}
+                    height={20}
+                    borderRadius={4}
+                  />
+                  <SkeletonPlaceholder.Item
+                    marginTop={6}
+                    width={80}
+                    height={20}
+                    borderRadius={4}
+                  />
+                </SkeletonPlaceholder.Item>
+              </SkeletonPlaceholder.Item>
+            </SkeletonPlaceholder>
+          ))}
+        </Fragment>
+      ) : (
+        <Text
+          style={{
+            fontSize: RFValue(fonts.LARGE_SIZE),
+            fontFamily: fonts.WORK_SANS_BOLD,
+            margin: RFValue(20),
+            textAlign: 'center'
+          }}
+        >
+          No results have been found for {searchState.query}.
+        </Text>
+      );
+    }
+  );
+
+  return (
+    <Results>
+      <FlatList
+        data={hits}
+        renderItem={_renderItem}
+        keyExtractor={(item) => item.objectID}
+        ItemSeparatorComponent={_separator}
+        showsVerticalScrollIndicator={false}
+        onEndReachedThreshold={0.5}
+        removeClippedSubviews={true}
+        scrollEventThrottle={16}
+        onEndReached={() => hasMore && refineNext()}
+        contentContainerStyle={{
+          paddingTop: RFValue(10),
+          paddingBottom: RFValue(60)
+        }}
+      />
+    </Results>
+  );
+}
+
+export default connectInfiniteHits(AlgoliaList);
