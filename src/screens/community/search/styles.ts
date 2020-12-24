8fb14156cd40aaae14e5cd8c3f6998293be99bc3
josemak25 <amakirij@gmail.com>
2020-12-24 04:03:00 +0100

    add fix to algolia search screen modal and fixed algolia search tab key routing

--- src/screens/community/search/styles.ts
+++ src/screens/community/search/styles.ts
@@ -5,3 +5,14 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.GREY};
   padding-top: ${RFValue(15)}px;
 `;
+
+export const SearchInput = styled.View`
+  height: ${RFValue(40)}px;
+  flex-direction: row;
+  align-items: center;
+  border-color: ${({ theme }) => theme.colors.INACTIVE};
+  border-radius: 4px;
+  border-width: 1px;
+  margin: 0px 10px;
+  padding: 0px 10px;
+`;
