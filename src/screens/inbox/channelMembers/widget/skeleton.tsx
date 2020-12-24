3e4c4b7d4d0939679f97e2839fd53201a0896398
josemak25 <amakirij@gmail.com>
2020-12-24 08:29:45 +0100

    add fix to channel members loading screen

--- src/screens/inbox/channelMembers/widget/skeleton.tsx
+++ src/screens/inbox/channelMembers/widget/skeleton.tsx
@@ -4,72 +4,34 @@ import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 export default function Skeleton() {
   return (
     <Fragment>
-      <SkeletonPlaceholder>
-        <SkeletonPlaceholder.Item
-          flexDirection="row"
-          alignItems="center"
-          margin={10}
-        >
-          <SkeletonPlaceholder.Item width={50} height={50} borderRadius={10} />
-          <SkeletonPlaceholder.Item marginLeft={20}>
+      {[...Array(5)].map((_, index) => (
+        <SkeletonPlaceholder key={`skeleton_${index}`}>
+          <SkeletonPlaceholder.Item
+            flexDirection="row"
+            alignItems="center"
+            margin={10}
+          >
             <SkeletonPlaceholder.Item
-              width={120}
-              height={15}
-              borderRadius={4}
+              width={70}
+              height={70}
+              borderRadius={10}
             />
-            <SkeletonPlaceholder.Item
-              marginTop={6}
-              width={80}
-              height={15}
-              borderRadius={4}
-            />
+            <SkeletonPlaceholder.Item marginLeft={20}>
+              <SkeletonPlaceholder.Item
+                width={180}
+                height={25}
+                borderRadius={4}
+              />
+              <SkeletonPlaceholder.Item
+                marginTop={6}
+                width={80}
+                height={20}
+                borderRadius={4}
+              />
+            </SkeletonPlaceholder.Item>
           </SkeletonPlaceholder.Item>
-        </SkeletonPlaceholder.Item>
-      </SkeletonPlaceholder>
-      <SkeletonPlaceholder>
-        <SkeletonPlaceholder.Item
-          flexDirection="row"
-          alignItems="center"
-          margin={10}
-        >
-          <SkeletonPlaceholder.Item width={50} height={50} borderRadius={10} />
-          <SkeletonPlaceholder.Item marginLeft={20}>
-            <SkeletonPlaceholder.Item
-              width={120}
-              height={15}
-              borderRadius={4}
-            />
-            <SkeletonPlaceholder.Item
-              marginTop={6}
-              width={80}
-              height={15}
-              borderRadius={4}
-            />
-          </SkeletonPlaceholder.Item>
-        </SkeletonPlaceholder.Item>
-      </SkeletonPlaceholder>
-      <SkeletonPlaceholder>
-        <SkeletonPlaceholder.Item
-          flexDirection="row"
-          alignItems="center"
-          margin={10}
-        >
-          <SkeletonPlaceholder.Item width={50} height={50} borderRadius={10} />
-          <SkeletonPlaceholder.Item marginLeft={20}>
-            <SkeletonPlaceholder.Item
-              width={120}
-              height={15}
-              borderRadius={4}
-            />
-            <SkeletonPlaceholder.Item
-              marginTop={6}
-              width={80}
-              height={15}
-              borderRadius={4}
-            />
-          </SkeletonPlaceholder.Item>
-        </SkeletonPlaceholder.Item>
-      </SkeletonPlaceholder>
+        </SkeletonPlaceholder>
+      ))}
     </Fragment>
   );
 }
