e9dfa25b97da49b4d5196d69397ef1d80e83f39e
josemak25 <amakirij@gmail.com>
2020-12-24 06:37:29 +0100

    fix channel members pacakge name and implemented pagiantion on members listing

---
+++ src/screens/inbox/channelMembers/index.tsx
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import { FlatList, RefreshControl, SafeAreaView } from 'react-native';
+import { Text } from 'react-native-paper';
+import { useQuery } from '@apollo/react-hooks';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useThemeContext } from '../../../theme';
+import {
+  GET_CHANNEL_MEMBERS,
+  GET_USER_PASSPORT
+} from '../../../graphql/server/query';
+import ActiveMember from './widget';
+import Skeleton from './widget/skeleton';
+import {
+  PassportInterface,
+  ChannelMembersRequestInterface
+} from '../../../graphql/types';
+import { ChatScreenProps } from '../../types';
+import { PAGINATION_DEFAULT } from '../../../constants';
+
+// DEFINE SCREEN PROP TYPES
+interface ModalProp {
+  route: { params: ChatScreenProps };
+}
+
+function ChannelMembers(props: ModalProp) {
+  const { colors, fonts } = useThemeContext();
+
+  const { data: userData } = useQuery(GET_USER_PASSPORT);
+  const [state, setState] = useState({
+    refreshing: false,
+    callOnScrollEnd: false
+  });
+  const userDetails = userData?.myPassport;
+  const channelId = props.route?.params?.channelId;
+  const userId = userDetails?.id;
+
+  const {
+    loading: channelLoading,
+    data: channelData,
+    refetch,
+    fetchMore
+  } = useQuery<ChannelMembersRequestInterface>(GET_CHANNEL_MEMBERS, {
+    variables: { channelId }
+  });
+
+  const channelMembers = channelData?.channelMembers;
+
+  const memberList = channelMembers?.slice().sort((a, b) => {
+    if (a.firstName < b.firstName) return -1;
+    if (a.firstName > b.firstName) return 1;
+    return 0;
+  });
+
+  const onRefresh = async () => {
+    setState({ ...state, refreshing: true });
+    await refetch();
+    setState({ ...state, refreshing: false });
+  };
+
+  const handleEndReach = async () => {
+    if (!state.callOnScrollEnd) return;
+
+    fetchMore({
+      variables: {
+        offset: channelMembers?.length,
+        first: PAGINATION_DEFAULT
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        setState({ ...state, callOnScrollEnd: false });
+
+        if (!fetchMoreResult) return prev;
+
+        return Object.assign({}, prev, {
+          channelMembers: [...fetchMoreResult.channelMembers]
+        });
+      }
+    });
+  };
+
+  const filterMembers = memberList?.filter((member) => member.id !== userId);
+
+  const _renderItem = ({ item }: { item: PassportInterface }) => (
+    <ActiveMember key={item.id} {...item} />
+  );
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={filterMembers || []}
+        ListEmptyComponent={
+          channelLoading ? (
+            <Skeleton />
+          ) : (
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                fontSize: RFValue(fonts.MEDIUM_SIZE),
+                color: colors.PRIMARY_TEXT,
+                textAlign: 'center'
+              }}
+            >
+              There are no members in this channel
+            </Text>
+          )
+        }
+        keyExtractor={({ id }) => id}
+        onMomentumScrollEnd={handleEndReach}
+        scrollEventThrottle={16}
+        onEndReachedThreshold={0.5}
+        removeClippedSubviews={true}
+        onEndReached={() => setState({ ...state, callOnScrollEnd: true })}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: RFValue(20),
+          paddingTop: RFValue(20)
+        }}
+        renderItem={_renderItem}
+        refreshControl={
+          <RefreshControl
+            refreshing={state.refreshing}
+            onRefresh={onRefresh}
+            tintColor={colors.BLACK}
+            colors={[colors.BLACK]}
+          />
+        }
+      />
+    </SafeAreaView>
+  );
+}
+
+export default React.memo(ChannelMembers);
---
+++ src/screens/inbox/channelMembers/widget/index.tsx
@@ -0,0 +1,226 @@
+import React, { Fragment, useState } from 'react';
+import * as Sentry from '@sentry/react-native';
+import { Title, Paragraph, TouchableRipple, Button } from 'react-native-paper';
+import { useMutation } from '@apollo/react-hooks';
+import { RFValue } from 'react-native-responsive-fontsize';
+import FastImage from 'react-native-fast-image';
+import { useTranslation } from 'react-i18next';
+import { useThemeContext } from '../../../../theme';
+import { REQUEST_CONNECTION } from '../../../../graphql/server/mutations';
+import { PassportInterface } from '../../../../graphql/types';
+import { rootNavigator } from '../../../../constants';
+import hexToRGB from '../../../../utils/hexToRGB';
+import { fireAuth } from '../../../../firebase/config';
+import { logEvent } from '../../../../utils/uxcamHelper';
+
+// IMPORT FOR ALL CUSTOM STYLES
+import { TextContainer } from './styles';
+
+// DEFINE SCREEN PROP TYPES
+interface ChannelUserProp extends PassportInterface {}
+
+function ChannelMember(props: ChannelUserProp) {
+  const { colors, fonts } = useThemeContext();
+  const { t } = useTranslation();
+
+  const userId = fireAuth.currentUser?.uid;
+
+  const { ...member } = props;
+
+  const {
+    id,
+    avatar,
+    firstName,
+    lastName,
+    connected,
+    currentLocation,
+    phoneNumber,
+    conversation
+  } = member;
+
+  const [pending, setPending] = useState(false);
+
+  const [requestConnection, { loading }] = useMutation(REQUEST_CONNECTION, {
+    variables: { payload: { phoneNumber } }
+  });
+
+  const handleRequest = async () => {
+    logEvent('request connection', { from: 'passport' });
+    try {
+      await requestConnection();
+      setPending(true);
+    } catch (error) {
+      Sentry.captureException(error);
+    }
+  };
+
+  const handleNavigation = () => {
+    rootNavigator.navigate('MemberDetailScreen', {
+      title: `${firstName} ${lastName}`,
+      details: member
+    });
+  };
+
+  const handleMessageNavigation = () => {
+    const senderId = conversation?.messageRequest.senderId;
+    const messageRequest = conversation?.messageRequest;
+    const isRequestApproved = conversation?.messageRequest?.approvedAt;
+    const approveRequest =
+      senderId !== userId && messageRequest && !isRequestApproved;
+
+    if (approveRequest) {
+      return rootNavigator.navigate('MessageRequestChatScreen', {
+        title: `${firstName} ${lastName}`,
+        chatId: conversation?.id,
+        senderId: id,
+        ...member
+      });
+    }
+
+    rootNavigator.navigate(
+      conversation?.id ? 'DirectChatScreen' : 'ConnectionChatScreen',
+      {
+        title: `${firstName} ${lastName}`,
+        chatId: conversation?.id,
+        receiverId: id,
+        ...member
+      }
+    );
+  };
+
+  const state = currentLocation[0]?.state;
+  const country = currentLocation[0]?.country;
+  const city = currentLocation[0]?.city;
+
+  return (
+    <TouchableRipple
+      onPress={handleNavigation}
+      rippleColor={hexToRGB(colors.PRIMARY, 0.3)}
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 15,
+        paddingVertical: 12
+      }}
+    >
+      <Fragment>
+        <FastImage
+          resizeMode={FastImage.resizeMode.contain}
+          source={{
+            uri: avatar,
+            priority: FastImage.priority.high
+          }}
+          style={{
+            width: RFValue(50),
+            height: RFValue(50),
+            borderRadius: RFValue(5)
+          }}
+        />
+        <TextContainer>
+          <Title
+            style={{
+              color: colors.PRIMARY_TEXT,
+              fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+              fontSize: RFValue(fonts.LARGE_SIZE),
+              textTransform: 'capitalize'
+            }}
+          >
+            {`${firstName} ${lastName}`}
+          </Title>
+          {city && state ? (
+            <Paragraph
+              style={{
+                fontSize: RFValue(fonts.LARGE_SIZE - 2),
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                lineHeight: RFValue(15),
+                color: colors.SECONDARY_TEXT,
+                textTransform: 'capitalize'
+              }}
+            >
+              {`${state}, ${country}`}
+            </Paragraph>
+          ) : (
+            <Paragraph
+              style={{
+                fontSize: RFValue(fonts.LARGE_SIZE - 2),
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                lineHeight: RFValue(15),
+                color: colors.SECONDARY_TEXT,
+                textTransform: 'capitalize'
+              }}
+            >
+              {`${city}, ${state}`}
+            </Paragraph>
+          )}
+        </TextContainer>
+        {connected == 'PENDING' || pending ? (
+          <Button
+            mode="text"
+            disabled={true}
+            uppercase={false}
+            labelStyle={{
+              fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+              fontSize: RFValue(fonts.SMALL_SIZE),
+              textTransform: 'capitalize',
+              color: colors.PRIMARY_TEXT,
+              marginHorizontal: 0
+            }}
+            contentStyle={{
+              backgroundColor: colors.DISABLED,
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+            style={{ borderRadius: 5, width: RFValue(60) }}
+          >
+            {t(`community.recommended.pending`)}
+          </Button>
+        ) : connected == 'CONNECTED' || connected == 'ACCEPTED' ? (
+          <Button
+            mode="text"
+            uppercase={false}
+            labelStyle={{
+              fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+              fontSize: RFValue(fonts.SMALL_SIZE),
+              textTransform: 'capitalize',
+              color: colors.WHITE,
+              marginHorizontal: 0
+            }}
+            contentStyle={{
+              backgroundColor: colors.PRIMARY,
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+            style={{ borderRadius: 5, width: RFValue(60) }}
+            onPress={handleMessageNavigation}
+          >
+            {t(`community.recommended.message`)}
+          </Button>
+        ) : (
+          <Button
+            loading={loading}
+            mode="contained"
+            uppercase={false}
+            labelStyle={{
+              fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+              fontSize: RFValue(fonts.SMALL_SIZE),
+              textTransform: 'capitalize',
+              color: colors.WHITE
+            }}
+            contentStyle={{
+              backgroundColor: colors.PRIMARY,
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+            style={{ borderRadius: 5, width: RFValue(60) }}
+            onPress={handleRequest}
+          >
+            {t(`community.recommended.add`)}+
+          </Button>
+        )}
+      </Fragment>
+    </TouchableRipple>
+  );
+}
+
+export default React.memo(ChannelMember, () => false);
