d3c60b4cadfde88d3865dc8134798c08aca11fd8
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-12-31 09:07:57 +0100

    passport avatar modal

---
+++ src/screens/community/memberPassport/widget/viewPassort/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
+import FastImage from 'react-native-fast-image';
+import { TouchableWithoutFeedback, KeyboardAvoidingView } from 'react-native';
+import { DEVICE_OS } from '../../../../../utils/device';
+
+// IMPORT FOR ALL CUSTOM STYLES
+import { Container, BlurContentsContainer, BlurContents } from './styles';
+
+// DEFINE SCREEN PROP TYPES
+interface ViewPassortAvatarProp {
+  onPress(): void;
+  avatar: string;
+}
+
+function ViewPasssportAvatar(props: ViewPassortAvatarProp) {
+  return (
+    <Container
+      blurType="light"
+      blurAmount={5}
+      reducedTransparencyFallbackColor="white"
+    >
+      <TouchableWithoutFeedback onPress={props.onPress}>
+        <KeyboardAvoidingView
+          behavior={DEVICE_OS == 'ios' ? 'padding' : 'height'}
+          style={{ flex: 1 }}
+        >
+          <BlurContents>
+            <BlurContentsContainer style={{ elevation: 6 }}>
+              <FastImage
+                resizeMode={FastImage.resizeMode.stretch}
+                source={{
+                  uri: props.avatar,
+                  priority: FastImage.priority.high
+                }}
+                style={{
+                  width: RFValue(300),
+                  height: RFValue(220),
+                  borderRadius: 4
+                }}
+              />
+            </BlurContentsContainer>
+          </BlurContents>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    </Container>
+  );
+}
+
+export default React.memo(ViewPasssportAvatar);
