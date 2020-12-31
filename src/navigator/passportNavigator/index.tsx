1d7c28f3601718790819798738bdfeb5f5bffb06
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-12-31 08:49:28 +0100

    add community and connection List screens to community and passport navigator

--- src/navigator/passportNavigator/index.tsx
+++ src/navigator/passportNavigator/index.tsx
@@ -7,6 +7,8 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import Screens from '../../screens/account';
 import PassportScreen from '../../screens/passport/index';
 import BirthplaceScreen from '../../screens/passport/widgets/birthplaceScreen';
+import ConnectionListScren from '../../screens/passport/widgets/connectionListScreen';
+import CommunityListScren from '../../screens/passport/widgets/communityListScreen';
 import { Feather } from '@expo/vector-icons';
 import hexToRGB from '../../utils/hexToRGB';
 import { NavigationInterface } from '../../screens/types';
@@ -42,11 +44,49 @@ export default function AccountNavigator(props: AccountNavigatorProps) {
         options={{ headerShown: false }}
       />
       <passportStack.Screen
+        name="ConnectionListScreen"
+        component={ConnectionListScren}
+        options={{
+          //@ts-ignore
+          headerTitle: t(`community.sideNav.connection`),
+          headerTitleStyle: {
+            color: colors.PRIMARY_TEXT,
+            fontSize: RFValue(fonts.LARGE_SIZE),
+            fontFamily: fonts.WORK_SANS_BOLD,
+            textTransform: 'capitalize'
+          },
+          headerBackTitleVisible: false,
+          headerTintColor: colors.PRIMARY,
+          headerLeftContainerStyle: { paddingLeft: 10 },
+          headerRightContainerStyle: { marginRight: 10 },
+          headerStyle: GLOBAL_HEADER_STYLE
+        }}
+      />
+      <passportStack.Screen
         name="BirthPlaceScreen"
         component={BirthplaceScreen}
         options={{
           //@ts-ignore
           headerTitle: t(`community.accountSettings.privacy`),
+          headerTitleStyle: {
+            color: colors.PRIMARY_TEXT,
+            fontSize: RFValue(fonts.LARGE_SIZE),
+            fontFamily: fonts.WORK_SANS_BOLD,
+            textTransform: 'capitalize'
+          },
+          headerBackTitleVisible: false,
+          headerTintColor: colors.PRIMARY,
+          headerLeftContainerStyle: { paddingLeft: 10 },
+          headerRightContainerStyle: { marginRight: 10 },
+          headerStyle: GLOBAL_HEADER_STYLE
+        }}
+      />
+      <passportStack.Screen
+        name="CommunityListScreen"
+        component={CommunityListScren}
+        options={{
+          //@ts-ignore
+          headerTitle: t(`community.memberPassport.myTribes`),
           headerTitleStyle: {
             color: colors.PRIMARY_TEXT,
             fontSize: RFValue(fonts.LARGE_SIZE),
