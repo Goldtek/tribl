d44938acb51241f4634b313c3a92f490fefc8ddc
josemak25 <amakirij@gmail.com>
2020-12-31 12:24:34 +0100

    refactor algolia search screen to sidebar navigator

--- src/navigator/communityNavigator/index.tsx
+++ src/navigator/communityNavigator/index.tsx
@@ -3,7 +3,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Image, Platform, TouchableHighlight } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { TransitionPresets } from '@react-navigation/stack';
 import { useThemeContext } from '../../theme';
 import { RFValue } from 'react-native-responsive-fontsize';
 import Screens from '../../screens/community';
@@ -363,75 +362,6 @@ export default function CommunityNavigator(props: CommunityNavigatorProps) {
           ),
           headerBackTitleVisible: false,
           headerTintColor: colors.PRIMARY,
-          headerRightContainerStyle: { marginRight: 10 },
-          headerStyle: GLOBAL_HEADER_STYLE
-        })}
-      />
-
-      <CommunityStack.Screen
-        name="CommunityAlgoliaScreen"
-        component={Screens.AlgoliaScreen}
-        options={{
-          ...TransitionPresets.ModalTransition,
-          headerTitle: () => (
-            <Image
-              source={require('../../../assets/images/logo.png')}
-              style={{
-                width: RFValue(50),
-                height: RFValue(50),
-                resizeMode: 'contain'
-              }}
-            />
-          ),
-          headerBackTitleVisible: false,
-          headerTintColor: colors.PRIMARY,
-          headerTitleContainerStyle: { alignItems: 'center' },
-          headerLeftContainerStyle: { marginLeft: 5 }
-        }}
-      />
-      <CommunityStack.Screen
-        name="UserCommunityListScreen"
-        component={CommunityListScren}
-        options={({ route }) => ({
-          //@ts-ignore
-          headerTitle: `${route.params?.title}'s ${t(
-            `community.memberPassport.tribe`
-          )}`,
-          headerTitleStyle: {
-            color: colors.PRIMARY_TEXT,
-            fontSize: RFValue(fonts.LARGE_SIZE),
-            fontFamily: fonts.WORK_SANS_BOLD,
-            textTransform: 'capitalize'
-          },
-          headerBackTitleVisible: false,
-          headerTintColor: colors.PRIMARY,
-          headerLeftContainerStyle: { paddingLeft: 10 },
-          headerRightContainerStyle: { marginRight: 10 },
-          headerStyle: GLOBAL_HEADER_STYLE
-        })}
-      />
-      <CommunityStack.Screen
-        name="UserConnectionListScreen"
-        component={ConnectionListScren}
-        options={({ route }) => ({
-          //@ts-ignore
-          headerTitle: `${route.params?.title}'s ${t(
-            `community.sideNav.userConnection`
-          )}`,
-          headerTitleStyle: {
-            color: colors.PRIMARY_TEXT,
-            fontSize: RFValue(fonts.LARGE_SIZE),
-            fontFamily: fonts.WORK_SANS_BOLD,
-            textTransform: 'capitalize'
-          },
-          headerTitleContainerStyle: {
-            flex: 1,
-            paddingLeft: DEVICE_OS === 'ios' ? 20 : 0
-          },
-
-          headerBackTitleVisible: false,
-          headerTintColor: colors.PRIMARY,
-          headerLeftContainerStyle: { paddingLeft: 10 },
           headerRightContainerStyle: { marginRight: 10 },
           headerStyle: GLOBAL_HEADER_STYLE
         })}
