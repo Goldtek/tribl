ee5fe4a140ec5d6dfaf467aeee30025c1f1e15c5
Ore Akintobi <oreakintobi@gmail.com>
2021-01-06 16:41:57 +0100

    built custom side drawer component to take drawer functionality off react navigation state

---
+++ src/navigator/sideNavigator/customDrawerComponent2/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const DrawerFooter = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  margin-bottom: ${RFValue(20)}px;
+`;
+
+export const ProfileContainer = styled.View`
+  padding-left: ${RFValue(15)}px;
+  margin-top: ${RFValue(20)}px;
+  margin-bottom: ${RFValue(30)}px;
+`;
