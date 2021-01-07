d6fb2e2890516847b07f16b49df3dd244152271d
Ore Akintobi <oreakintobi@gmail.com>
2021-01-07 13:05:33 +0100

    added side menu and privacy policy screen to navigator. also  added string of new screen to RootStackParamScreensList

--- src/screens/account/index.ts
+++ src/screens/account/index.ts
@@ -2,10 +2,12 @@ import MyConnectionScreen from '../account/myConnections';
 import ConnectionRequestScreen from '../account/connectionRequest';
 import AccountSettingScreen from '../account/accountSettings';
 import PrivacyScreen from '../account/privacy';
+import PrivacyPolicyScreen from '../account/privacyPolicy';
 
 export default {
   MyConnectionScreen,
   ConnectionRequestScreen,
   AccountSettingScreen,
-  PrivacyScreen
+  PrivacyScreen,
+  PrivacyPolicyScreen
 };
