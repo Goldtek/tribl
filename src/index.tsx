7724bfc6e5872df49829fd16086f0f180699638f
josemak25 <amakirij@gmail.com>
2021-01-09 08:56:27 +0100

    add stream provider to app root router

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { RootToaster } from './components/rootToaster';
 import { Host as PortalHost } from 'react-native-portalize';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { StatusBar } from 'expo-status-bar';
 import GlobalErrorBoundary from './libs/error';
+import { StatusBar } from 'expo-status-bar';
 import ApolloProvider from './graphql';
+import StreamProvider from './stream';
 import ThemeProvider from './theme';
 import Router from './router';
 
@@ -14,11 +15,13 @@ export default function AppRouter() {
       <ApolloProvider>
         <SafeAreaProvider>
           <ThemeProvider>
-            <StatusBar translucent animated style="dark" />
-            <RootToaster />
-            <PortalHost>
-              <Router />
-            </PortalHost>
+            <StreamProvider>
+              <StatusBar translucent animated style="dark" />
+              <RootToaster />
+              <PortalHost>
+                <Router />
+              </PortalHost>
+            </StreamProvider>
           </ThemeProvider>
         </SafeAreaProvider>
       </ApolloProvider>
