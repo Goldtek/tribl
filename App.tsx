bd8750d2a5c638cd8dcd57009236480b4cab17d7
josemak25 <amakirij@gmail.com>
2021-01-09 09:23:22 +0100

    refactor app to remove firebase old realtime database

--- App.tsx
+++ App.tsx
@@ -6,27 +6,18 @@ import {
 } from 'react-native';
 //@ts-ignore
 import RNUxcam from 'react-native-ux-cam';
-import database from '@react-native-firebase/database';
 import { enableScreens } from 'react-native-screens';
 import loadResources from './src/libs/loadResources';
 import './src/internationalization';
 import AppRouter from './src';
 import { Mixpanel } from './src/config';
-import { fireAuth } from './src/firebase/config';
 
 enableScreens();
 
 export default function App() {
   const [isAppReady, setIsAppReady] = useState(false);
-  useEffect(() => {
-    loadApp();
-  }, []);
 
   let uxcamEvent: EventSubscription;
-  useEffect(() => {
-    _uxcamSessionStartListener();
-    return () => uxcamEvent.remove();
-  }, []);
 
   //Setup listener
   function _uxcamSessionStartListener() {
@@ -44,27 +35,19 @@ export default function App() {
     });
   }
 
-  useEffect(() => {
-    // Assuming user is logged in
-    const userId = fireAuth.currentUser?.uid;
-
-    const reference = database().ref(`/presence/${userId}`);
-
-    // Set the /users/:userId value to true
-    reference.set({ lastSeen: Date.now(), status: 'ONLINE' });
-
-    // Remove the node whenever the client disconnects
-    return () => {
-      reference
-        .onDisconnect()
-        .update({ lastSeen: Date.now(), status: 'OFFLINE' });
-    };
-  }, []);
-
   const loadApp = async () => {
     await loadResources();
     setIsAppReady(true);
   };
+
+  useEffect(() => {
+    _uxcamSessionStartListener();
+    return () => uxcamEvent.remove();
+  }, []);
+
+  useEffect(() => {
+    loadApp();
+  }, []);
 
   return isAppReady ? <AppRouter /> : null;
 }
