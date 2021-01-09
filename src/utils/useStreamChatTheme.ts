632f73d35eb39679adcbafdd7d7e71ca94b52662
josemak25 <amakirij@gmail.com>
2021-01-09 08:50:47 +0100

    add stream chat styles

---
+++ src/utils/useStreamChatTheme.ts
@@ -0,0 +1,17 @@
+import { useRef } from 'react';
+import { useThemeContext } from '../theme';
+
+export default function useStreamChatTheme() {
+  const { colors } = useThemeContext();
+
+  const getChatStyle = () => {
+    return {
+      'loadingIndicator.loadingText': `color: ${colors.PRIMARY_TEXT}`,
+      'messageInput.container': `backgroundColor: ${colors.WHITE}; margin: 0; padding: 0; border-radius: 0;`,
+      'messageInput.sendButton': `height: 30px; width: 30px; justify-content: center; align-items: center`,
+      'messageInput.sendButtonIcon': `height: 20px; width: 20px; color: ${colors.WHITE};`
+    };
+  };
+
+  return useRef(getChatStyle()).current;
+}
