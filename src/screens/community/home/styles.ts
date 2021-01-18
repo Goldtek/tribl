6a53a06ffd1c83f240bcc05e93672c02c5ce1f6f
Ore Akintobi <oreakintobi@gmail.com>
2021-01-18 14:29:22 +0100

    changed button views for community home page and passport page

--- src/screens/community/home/styles.ts
+++ src/screens/community/home/styles.ts
@@ -23,6 +23,7 @@ export const RecommendedListHeader = styled.View`
   align-items: center;
   justify-content: space-between;
   padding-left: 15px;
+  padding-right: 15px;
 `;
 
 export const RecommendedCommunityContainer = styled.View`
