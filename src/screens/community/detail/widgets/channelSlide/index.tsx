64ce0fd3a5338f14325b61bd35e01d6071ab21ed
josemak25 <amakirij@gmail.com>
2021-01-24 19:39:41 +0100

    add realtime network synching to communty channels tab

--- src/screens/community/detail/widgets/channelSlide/index.tsx
+++ src/screens/community/detail/widgets/channelSlide/index.tsx
@@ -1,11 +1,19 @@
-import React, { useMemo, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { FlatList } from 'react-native';
-import { Divider } from 'react-native-paper';
+import { ActivityIndicator, Divider } from 'react-native-paper';
 import ChannelCard from './widget';
 import { useThemeContext } from '../../../../../theme';
 import { NavigationInterface } from '../../../../types';
 import { tagScreenName } from '../../../../../utils/uxcamHelper';
-import { CommunityInterface } from '../../../../../graphql/types';
+import {
+  CommunityChannelRequestInterface,
+  CommunityInterface
+} from '../../../../../graphql/types';
+import { GET_COMMUNITY_CHANNELS } from '../../../../../graphql/server/query';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { useIsFocused } from '@react-navigation/native';
+
+import { LoadingIndicatorContainer, LoadingChannels } from './styles';
 
 // DEFINE SCREEN PROP TYPES
 interface ScreenProp extends NavigationInterface {
@@ -16,24 +24,41 @@ export default function ChannelScreen(props: ScreenProp) {
   const { communityDetails } = props.route;
   const { colors } = useThemeContext();
 
+  const isFocused = useIsFocused();
+
   useEffect(() => {
     tagScreenName('TribeChannelScreen');
   }, []);
 
-  const _renderItem = useMemo(
-    () => (props: any) => (
-      <ChannelCard {...props} communityDetails={communityDetails} />
-    ),
-    []
+  const [getCommunityChannels, { data, refetch }] = useLazyQuery<
+    CommunityChannelRequestInterface
+  >(GET_COMMUNITY_CHANNELS, {
+    variables: { communityId: communityDetails.id }
+  });
+
+  useEffect(() => {
+    data ? refetch() : getCommunityChannels();
+  }, [isFocused, data]);
+
+  const _renderItem = (props: any) => (
+    <ChannelCard {...props} communityDetails={communityDetails} />
   );
 
   return (
     <FlatList
+      bounces={false}
       renderItem={_renderItem}
-      data={communityDetails?.channels}
+      data={data?.communityChannels}
+      contentContainerStyle={{ flex: 1 }}
       ItemSeparatorComponent={() => (
         <Divider style={{ borderWidth: 0.6, borderColor: colors.DISABLED }} />
       )}
+      ListEmptyComponent={
+        <LoadingIndicatorContainer>
+          <ActivityIndicator size={30} />
+          <LoadingChannels>loading channels...</LoadingChannels>
+        </LoadingIndicatorContainer>
+      }
       keyExtractor={(item) => item.id}
     />
   );
--- src/screens/community/detail/widgets/channelSlide/styles.ts
+++ src/screens/community/detail/widgets/channelSlide/styles.ts
@@ -16,3 +16,16 @@ export const RecommendedListHeader = styled.View`
   justify-content: space-between;
   padding-left: 15px;
 `;
+
+export const LoadingIndicatorContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LoadingChannels = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
+  color: ${({ theme }) => theme.colors.PRIMARY_TEXT};
+  margin-top: 20px;
+  text-transform: capitalize;
+`;
