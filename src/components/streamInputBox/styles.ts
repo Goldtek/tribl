1da2331afab4d7c756b042114b68a06a6e1491f8
Olei Eto <teneeto@gmail.com>
2021-01-27 12:39:27 +0100

    replace emoji with gif icon and baord

--- src/components/streamInputBox/styles.ts
+++ src/components/streamInputBox/styles.ts
@@ -37,13 +37,16 @@ export const SendButtonContainer = styled.View`
   background-color: ${({ theme }) => theme.colors.primary};
 `;
 
-export const IconContainer = styled(TouchableRipple)`
+export const IconContainer = styled(TouchableRipple)<{ borderColor?: string }>`
   width: 30px;
   height: 30px;
   align-items: center;
   justify-content: center;
+  border: ${({ borderColor }) =>
+    borderColor ? `1px solid ${borderColor}` : '0'};
   border-radius: ${30 / 2}px;
   margin: 5px;
+  margin-right: ${({ borderColor }) => (borderColor ? '15px' : '5px')};
 `;
 
 export const ButtonContainer = styled.TouchableOpacity`
@@ -51,6 +54,23 @@ export const ButtonContainer = styled.TouchableOpacity`
   ${({ theme }) => theme.messageInput.sendButton.css};
 `;
 
-export const InputTextSpacer = styled.View`
-  margin: 15px;
+export const GifContainer = styled.View`
+  padding-horizontal: 13px;
+  /* flex-direction: row; */
+  /* flex-wrap: wrap; */
+  align-items: center;
+  width: 100%;
+  padding-bottom: 50px;
+`;
+
+export const InputWrapper = styled.View``;
+export const GifImageWrapper = styled.TouchableOpacity`
+  margin-horizontal: 2.5px;
+  margin-vertical: 2.5px;
+  width: 50%;
+`;
+
+export const HeaderWrapper = styled.View`
+  margin-horizontal: 15px;
+  margin-bottom: 30px;
 `;
