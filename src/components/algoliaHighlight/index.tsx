4def1db38f9a816b415e2ef391190e7f4c148862
josemak25 <amakirij@gmail.com>
2021-02-05 10:09:32 +0100

    add fix to pagination

--- src/components/algoliaHighlight/index.tsx
+++ src/components/algoliaHighlight/index.tsx
@@ -13,21 +13,21 @@ import { NameContainer, Container } from './style';
 
 // DEFINE SCREEN PROP TYPES
 interface HighlightProp {
+  highlight(T: any): any[];
   attribute: string;
   hit: any;
-  highlight(T: any): any[];
 }
 
 const Highlight = (props: HighlightProp) => {
   const navigation = useNavigation();
+  const { colors, fonts } = useThemeContext();
   const { attribute, hit, highlight } = props;
+
   const highlights = highlight({
     highlightProperty: '_highlightResult',
     attribute,
     hit
   });
-
-  const { colors, fonts } = useThemeContext();
 
   const handleNavigation = () => {
     navigation.navigate('CommunityDetailScreen', {
@@ -45,120 +45,114 @@ const Highlight = (props: HighlightProp) => {
     });
   };
 
+  const filteredList = hit.id !== fireAuth.currentUser?.uid ? hit : null;
+  const state = hit?.currentLocation?.state;
+  const city = hit?.currentLocation?.city;
+  const country = hit?.currentLocation?.country;
+
   return (
     <Container>
-      {highlights.map((_, index: number) => {
-        const filteredList = hit.id !== fireAuth.currentUser?.uid ? hit : null;
-        const state = hit?.currentLocation?.state;
-        const city = hit?.currentLocation?.city;
-        const country = hit?.currentLocation?.country;
-
-        return (
+      {hit.name ? (
+        <TouchableRipple
+          key={hit.id}
+          style={{
+            flex: 1,
+            width: '100%',
+            height: RFValue(80),
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: 10
+          }}
+          rippleColor={hexToRGB(colors.PRIMARY, 0.3)}
+          onPress={handleNavigation}
+        >
           <Fragment>
-            {hit.name ? (
-              <TouchableRipple
-                key={hit.id}
+            <FastImage
+              resizeMode={FastImage.resizeMode.stretch}
+              source={{
+                uri: hit.avatar,
+                priority: FastImage.priority.high
+              }}
+              style={{
+                width: RFValue(60),
+                height: RFValue(60),
+                borderRadius: RFValue(4)
+              }}
+            />
+            <NameContainer>
+              <Title
                 style={{
-                  flex: 1,
-                  width: '100%',
-                  height: RFValue(80),
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  paddingHorizontal: 10
+                  color: colors.PRIMARY_TEXT,
+                  fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                  fontSize: RFValue(fonts.LARGE_SIZE),
+                  textTransform: 'capitalize'
                 }}
-                rippleColor={hexToRGB(colors.PRIMARY, 0.3)}
-                onPress={handleNavigation}
               >
-                <Fragment>
-                  <FastImage
-                    resizeMode={FastImage.resizeMode.stretch}
-                    source={{
-                      uri: hit.avatar,
-                      priority: FastImage.priority.high
-                    }}
-                    style={{
-                      width: RFValue(60),
-                      height: RFValue(60),
-                      borderRadius: RFValue(4)
-                    }}
-                  />
-                  <NameContainer>
-                    <Title
-                      style={{
-                        color: colors.PRIMARY_TEXT,
-                        fontFamily: fonts.WORK_SANS_SEMI_BOLD,
-                        fontSize: RFValue(fonts.LARGE_SIZE),
-                        textTransform: 'capitalize'
-                      }}
-                    >
-                      {hit.name}
-                    </Title>
-                    <Text
-                      style={{
-                        color: colors.SECONDARY_TEXT,
-                        fontFamily: fonts.WORK_SANS_REGULAR,
-                        fontSize: RFValue(fonts.MEDIUM_SIZE)
-                      }}
-                    >
-                      {hit.membersCount}
-                    </Text>
-                  </NameContainer>
-                </Fragment>
-              </TouchableRipple>
-            ) : (
-              <TouchableRipple
-                key={hit.id}
+                {hit.name}
+              </Title>
+              <Text
                 style={{
-                  flex: 1,
-                  width: '100%',
-                  height: RFValue(80),
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  paddingHorizontal: 10
+                  color: colors.SECONDARY_TEXT,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.MEDIUM_SIZE)
                 }}
-                rippleColor={hexToRGB(colors.PRIMARY, 0.3)}
-                onPress={handlePassportNavigation}
               >
-                <Fragment>
-                  <FastImage
-                    resizeMode={FastImage.resizeMode.contain}
-                    source={{
-                      uri: filteredList?.avatar,
-                      priority: FastImage.priority.high
-                    }}
-                    style={{
-                      width: RFValue(60),
-                      height: RFValue(60),
-                      borderRadius: RFValue(4)
-                    }}
-                  />
-                  <NameContainer>
-                    <Title
-                      style={{
-                        color: colors.PRIMARY_TEXT,
-                        fontFamily: fonts.WORK_SANS_SEMI_BOLD,
-                        fontSize: RFValue(fonts.LARGE_SIZE),
-                        textTransform: 'capitalize'
-                      }}
-                    >
-                      {`${hit?.firstName} ${hit?.lastName}`}
-                    </Title>
-                    <Text
-                      style={{
-                        color: colors.SECONDARY_TEXT,
-                        fontFamily: fonts.WORK_SANS_REGULAR,
-                        fontSize: RFValue(fonts.MEDIUM_SIZE)
-                      }}
-                    >
-                      {city ? `${city} ${state}` : `${state} ${country}`}
-                    </Text>
-                  </NameContainer>
-                </Fragment>
-              </TouchableRipple>
-            )}
+                {hit.membersCount}
+              </Text>
+            </NameContainer>
           </Fragment>
-        );
-      })}
+        </TouchableRipple>
+      ) : (
+        <TouchableRipple
+          key={hit.id}
+          style={{
+            flex: 1,
+            width: '100%',
+            height: RFValue(80),
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: 10
+          }}
+          rippleColor={hexToRGB(colors.PRIMARY, 0.3)}
+          onPress={handlePassportNavigation}
+        >
+          <Fragment>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              source={{
+                uri: filteredList?.avatar,
+                priority: FastImage.priority.high
+              }}
+              style={{
+                width: RFValue(60),
+                height: RFValue(60),
+                borderRadius: RFValue(4)
+              }}
+            />
+            <NameContainer>
+              <Title
+                style={{
+                  color: colors.PRIMARY_TEXT,
+                  fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+                  fontSize: RFValue(fonts.LARGE_SIZE),
+                  textTransform: 'capitalize'
+                }}
+              >
+                {`${hit?.firstName} ${hit?.lastName}`}
+              </Title>
+              <Text
+                style={{
+                  color: colors.SECONDARY_TEXT,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.MEDIUM_SIZE)
+                }}
+              >
+                {city ? `${city} ${state}` : `${state} ${country}`}
+              </Text>
+            </NameContainer>
+          </Fragment>
+        </TouchableRipple>
+      )}
     </Container>
   );
 };
