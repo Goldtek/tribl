92e813afde237199d3e4114887138077fb1aaddc
josemak25 <amakirij@gmail.com>
2021-02-10 12:04:31 +0100

    add fix on chat tab and direct message view loader

---
+++ src/screens/inbox/home/directMessageTab/index.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import CustomChannelPreview from './widget';
+import { NavigationInterface } from '../../../types';
+import { tagScreenName } from '../../../../utils/uxcamHelper';
+import { ChannelList, Chat, DefaultCommandType } from 'stream-chat-expo';
+import { useQuery } from '@apollo/react-hooks';
+import {
+  LocalAttachmentType,
+  LocalChannelType,
+  LocalEventType,
+  LocalMessageType,
+  LocalReactionType,
+  LocalUserType,
+  chatClient
+} from '../../../../stream/types';
+import { ChannelSort } from 'stream-chat';
+import { useStreamContext } from '../../../../stream';
+import { MyPassportInterface } from '../../../../graphql/types';
+import { GET_USER_PASSPORT } from '../../../../graphql/server/query';
+
+import { Container } from './styles';
+
+// DEFINE SCREEN PROP TYPES
+interface ScreenProp extends NavigationInterface {}
+
+function DirectMessageTab(props: ScreenProp) {
+  const { setChannel, setActivityScreen } = useStreamContext();
+
+  const { data: userData } = useQuery<MyPassportInterface>(GET_USER_PASSPORT);
+
+  const filters = {
+    members: { $in: [userData?.myPassport.id] },
+    type: 'team',
+    isDm: true
+  };
+
+  const sort: ChannelSort<LocalChannelType> = { last_message_at: -1 };
+
+  const options = { presence: true, state: true, watch: true };
+
+  useEffect(() => {
+    tagScreenName('DirectMessageTab');
+  }, []);
+
+  return (
+    <Chat
+      // @ts-ignore
+      client={chatClient}
+    >
+      <StatusBar translucent animated style="dark" />
+      <Container>
+        <ChannelList<
+          LocalAttachmentType,
+          LocalChannelType,
+          DefaultCommandType,
+          LocalEventType,
+          LocalMessageType,
+          LocalReactionType,
+          LocalUserType
+        >
+          // @ts-ignore
+          filters={filters}
+          onSelect={(channel) => {
+            setChannel(channel as any);
+            setActivityScreen('channelScreen');
+          }}
+          sort={sort}
+          options={options}
+          Preview={CustomChannelPreview}
+          additionalFlatListProps={{ showsVerticalScrollIndicator: false }}
+        />
+      </Container>
+    </Chat>
+  );
+}
+
+export default React.memo(DirectMessageTab);
---
+++ src/screens/inbox/home/directMessageTab/widget/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const Date = styled.Text`
+  color: ${({ theme }) => theme.colors.SECONDARY_TEXT};
+  font-size: 11px;
+  text-align: right;
+  ${({ theme }) => theme.channelPreview.date.css}
+`;
+
+export const Details = styled.View`
+  flex: 1;
+  padding-left: 10px;
+  ${({ theme }) => theme.channelPreview.details.css}
+`;
+
+export const DetailsTop = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  ${({ theme }) => theme.channelPreview.detailsTop.css}
+`;
+
+export const DetailsBottom = styled(DetailsTop)`
+  align-items: center;
+  margin-top: 2px;
+`;
+
+export const NotificationContainer = styled(DetailsTop)`
+  align-items: center;
+`;
+
+export const StyledMessage = styled.Text<{ unread?: number }>`
+  color: ${({ theme, unread }) =>
+    unread
+      ? theme.channelPreview.message.unreadColor
+      : theme.channelPreview.message.color};
+  font-size: ${({ theme }) => RFValue(theme.fonts.SMALL_SIZE + 1)}px;
+  font-weight: ${({ theme, unread }) =>
+    unread
+      ? theme.channelPreview.message.unreadFontWeight
+      : theme.channelPreview.message.fontWeight};
+  ${({ theme }) => theme.channelPreview.message.css}
+`;
+
+export const Title = styled.Text`
+  font-size: ${({ theme }) => RFValue(theme.fonts.MEDIUM_SIZE - 1)}px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.PRIMARY_TEXT};
+  ${({ theme }) => theme.channelPreview.title.css}
+`;
+
+export const ListActionText = styled.Text`
+  color: ${({ theme }) => theme.colors.WHITE};
+  font-size: ${({ theme }) => RFValue(theme.fonts.MEDIUM_SIZE - 1)}px;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
+
+export const ListActionTextWrapper = styled.View<{ color?: string }>`
+  width: 70px;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-color: ${({ theme, color }) =>
+    color ? color : theme.colors.PRIMARY};
+`;
+
+export const ActionContainer = styled.View`
+  flex-direction: row;
+`;
