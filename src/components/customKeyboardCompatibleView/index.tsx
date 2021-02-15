b5ae636e4f4d75fdc6d3fa64a1fb080c0400742f
Olei Eto <teneeto@gmail.com>
2021-02-15 23:36:21 +0100

    fix keyboard compatible view for insets/non-insets

--- src/components/customKeyboardCompatibleView/index.tsx
+++ src/components/customKeyboardCompatibleView/index.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { KeyboardCompatibleView } from 'stream-chat-expo';
+import { useStreamContext } from '../../stream';
 import { DEVICE_OS } from '../../utils/device';
 
+let insetsIos = 35;
+let noneInsets = 0;
 export default function CustomKeyboardCompatibleView({ children }: any) {
   const insets = useSafeAreaInsets();
+  const { activityScreen } = useStreamContext();
+
+  useEffect(() => {
+    if (
+      activityScreen === 'directMessage' ||
+      activityScreen === 'directMessageThreadScreen'
+    ) {
+      insetsIos = insets.bottom / 2.5;
+      noneInsets = 8;
+    } else {
+      insetsIos = insets.bottom * 3.5;
+      noneInsets = 95;
+    }
+  }, [activityScreen]);
+  const iosVerticalOffset = insets.bottom > 0 ? insetsIos : noneInsets;
 
   return (
     <KeyboardCompatibleView
-      keyboardVerticalOffset={DEVICE_OS === 'ios' ? insets.bottom : undefined}
+      keyboardVerticalOffset={
+        DEVICE_OS === 'ios' ? iosVerticalOffset : undefined
+      }
       behavior={DEVICE_OS === 'ios' ? 'padding' : 'position'}
     >
       {children}
