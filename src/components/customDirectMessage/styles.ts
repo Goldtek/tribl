682c46929db49818e526e1a4a897dc5d88be6d2f
Olei Eto <teneeto@gmail.com>
2021-02-16 02:57:47 +0100

    add Avatar container for DM

--- src/components/customDirectMessage/styles.ts
+++ src/components/customDirectMessage/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { Alignment } from 'stream-chat-expo';
 
 export const Container = styled.View`
   flex-direction: column;
@@ -9,4 +10,12 @@ export const Edited = styled.Text`
   color: ${({ theme }) => theme.colors.SECONDARY_TEXT};
   font-size: ${({ theme }) => theme.fonts.MEDIUM_SIZE}px;
   margin-top: 3px;
+`;
+
+export const AvatarContainer = styled.TouchableOpacity<{
+  alignment: Alignment;
+}>`
+  margin-left: ${({ alignment }) => (alignment === 'right' ? 8 : 0)}px;
+  margin-right: ${({ alignment }) => (alignment === 'left' ? 8 : 0)}px;
+  ${({ theme }) => theme.message.avatarWrapper.container.css};
 `;
