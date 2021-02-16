f50c63d83b65cf1e1433180b507ee1971c9b4608
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-02-16 13:58:52 +0100

    create and style notification screen and tribe invite slide

---
+++ src/screens/account/notifications/widgets/general/widget/member/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const NameContainer = styled.View`
+  margin-left: ${RFValue(10)}px;
+`;
