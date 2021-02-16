b820ddb19307d9dd08ff7dc3e79992edde9c9f17
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-02-16 14:22:39 +0100

    create and style new tribe screens

---
+++ src/screens/community/home/widget/newTribe/highlightSlide/styles.ts
@@ -0,0 +1,80 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const Container = styled.View`
+  flex: 1;
+  flex-direction: column;
+`;
+
+export const CardContainer = styled.View`
+  flex-direction: row;
+  padding: 20px 10px;
+`;
+
+export const TextContainer = styled.View`
+  flex: 1;
+  padding: 0px 15px;
+`;
+
+export const TagContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const Tags = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+
+export const TagText = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.WORK_SANS_SEMI_BOLD};
+  font-size: ${({ theme }) => RFValue(theme.fonts.MEDIUM_SIZE)}px;
+  color: ${({ theme }) => theme.colors.PRIMARY_TEXT};
+  margin-top: 10px;
+  margin-right: 10px;
+  padding: ${RFValue(5)}px;
+  border-width: ${RFValue(1.2)}px;
+  border-color: ${({ theme }) => theme.colors.SECONDARY_TEXT};
+  text-transform: capitalize;
+  border-radius: 4px;
+`;
+
+export const DescrptionCover = styled.View`
+  height: ${RFValue(60)}px;
+  border: 1px solid ${({ theme }) => theme.colors.INACTIVE};
+  border-radius: 4px;
+`;
+
+export const AddTag = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.WORK_SANS_BOLD};
+  font-size: ${({ theme }) => RFValue(theme.fonts.MEDIUM_SIZE)}px;
+  color: ${({ theme }) => theme.colors.PRIMARY_TEXT};
+  width: 50px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: ${RFValue(6)}px;
+  border-width: ${RFValue(1.2)}px;
+  border-color: ${({ theme }) => theme.colors.SECONDARY_TEXT};
+  text-transform: uppercase;
+  border-radius: 4px;
+`;
+
+export const TagButton = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.WORK_SANS_BOLD};
+  font-size: ${({ theme }) => RFValue(theme.fonts.MEDIUM_SIZE)}px;
+  color: ${({ theme }) => theme.colors.PRIMARY_TEXT};
+  margin-top: 10px;
+  margin-right: 10px;
+  padding: ${RFValue(10)}px;
+  border-width: ${RFValue(1.2)}px;
+  border-color: ${({ theme }) => theme.colors.INACTIVE};
+  text-transform: uppercase;
+  border-radius: 4px;
+`;
+
+export const TagButtonCover = styled.View`
+  position: relative;
+  flex-direction: row;
+  align-items: center;
+`;
---
+++ src/screens/community/home/widget/newTribe/index.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { NavigationInterface } from '../../../../types';
+import { useTranslation } from 'react-i18next';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import { DEVICE_FULL_WIDTH } from '../../../../../utils/device';
+import { Title } from 'react-native-paper';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useThemeContext } from '../../../../../theme';
+import highlightSlide from './highlightSlide';
+import channelSlide from './highlightSlide';
+import memberSlide from './highlightSlide';
+import { StatusBar } from 'expo-status-bar';
+import { GLOBAL_HEADER_STYLE } from '../../../../../constants';
+
+// IMPORT FOR ALL CUSTOM STYLES
+import { Container } from './styles';
+
+// DEFINE SCREEN PROP TYPES
+interface ScreenProp extends NavigationInterface {}
+
+export default function SearchScreen(props: ScreenProp) {
+  const { colors, fonts } = useThemeContext();
+  const { t } = useTranslation();
+  const details = props.route.params;
+  const [isMember, setIsMember] = useState(false);
+  const [tabIndex, setTabIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {
+      key: 'highlightSlide',
+      title: `${t(`community.tabPanel.highlight`)}`,
+      communityDetails: details
+    },
+    {
+      key: 'channelSlide',
+      title: `${t(`community.tabPanel.channel`)}`
+    },
+    {
+      key: 'memberSlide',
+      title: `${t(`community.tabPanel.member`)}`
+    }
+  ]);
+
+  const renderScene = SceneMap({ highlightSlide, channelSlide, memberSlide });
+
+  const renderLabel = ({
+    route,
+    focused
+  }: {
+    route: { title: string };
+    focused: boolean;
+  }) => (
+    <Title
+      style={{
+        fontFamily: focused
+          ? fonts.WORK_SANS_SEMI_BOLD
+          : fonts.WORK_SANS_REGULAR,
+        fontSize: RFValue(fonts.LARGE_SIZE + 1),
+        color: focused
+          ? colors.PRIMARY
+          : !isMember
+          ? colors.INACTIVE
+          : colors.PRIMARY_TEXT,
+        textTransform: 'capitalize',
+        marginTop: 0,
+        marginBottom: 0,
+        width: '105%'
+      }}
+    >
+      {route.title}
+    </Title>
+  );
+
+  const renderTabBar = (props: any) => {
+    return (
+      <TabBar
+        {...props}
+        indicatorStyle={{
+          backgroundColor: colors.PRIMARY,
+          height: RFValue(4)
+        }}
+        renderLabel={renderLabel}
+        style={{
+          ...GLOBAL_HEADER_STYLE,
+          backgroundColor: colors.WHITE,
+          marginBottom: 10,
+          paddingBottom: 10
+        }}
+      />
+    );
+  };
+
+  const handleIndexChange = (index: number) => {
+    isMember ? setTabIndex(index) : setTabIndex(0);
+  };
+
+  return (
+    <Container>
+      <StatusBar translucent animated style="dark" />
+      <TabView
+        navigationState={{ index: tabIndex, routes }}
+        renderScene={renderScene}
+        renderTabBar={renderTabBar}
+        onIndexChange={handleIndexChange}
+        initialLayout={{ width: DEVICE_FULL_WIDTH }}
+        swipeEnabled={isMember ? true : false}
+      />
+    </Container>
+  );
+}
