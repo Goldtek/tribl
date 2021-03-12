d29e510fe944aab41610f9430a1adbd1272550db
josemak25 <amakirij@gmail.com>
2021-03-12 08:27:30 +0100

    add fix to user connection card component

---
+++ src/screens/passport/userConnectionListScreen/widget/myCommunitySkeleton/index.tsx
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+
+// DEFINE SCREEN PROP TYPES
+interface MyConnectionSkeletonProps {
+  skeletonSize?: number;
+}
+
+export default function MyConnectionSkeleton({
+  skeletonSize = 3
+}: MyConnectionSkeletonProps) {
+  return (
+    <Fragment>
+      {[...Array(skeletonSize)].map((_, index) => (
+        <SkeletonPlaceholder key={index.toString()}>
+          <SkeletonPlaceholder.Item
+            marginTop={20}
+            marginLeft={15}
+            flexDirection="row"
+            alignItems="center"
+          >
+            <SkeletonPlaceholder.Item
+              width={55}
+              height={55}
+              borderRadius={10}
+            />
+            <SkeletonPlaceholder.Item justifyContent="center" marginLeft={20}>
+              <SkeletonPlaceholder.Item
+                width={100}
+                height={10}
+                borderRadius={4}
+                marginTop={5}
+              />
+              <SkeletonPlaceholder.Item
+                width={150}
+                height={15}
+                borderRadius={4}
+                marginTop={10}
+              />
+            </SkeletonPlaceholder.Item>
+          </SkeletonPlaceholder.Item>
+        </SkeletonPlaceholder>
+      ))}
+    </Fragment>
+  );
+}
