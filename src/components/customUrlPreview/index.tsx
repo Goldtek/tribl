10827f0fe3d58ec555dfbeacbfeea3c190f721ef
Eto Olei <teneeto@Etos-MacBook-Air.local>
2021-03-16 12:32:46 +0100

    add prop types and truncate link letters

--- src/components/customUrlPreview/index.tsx
+++ src/components/customUrlPreview/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import truncate from 'lodash/truncate';
 
 import { Container, DetailsContainer, TitleUrl, Title, Description, Thumbnail } from './styles';
 
-export const CustomUrlPreview = (props: any )=> {
+type Props = {
+  title_link: string; 
+  title: string | undefined; 
+  image_url: any; 
+  thumb_url: any; 
+  text: string | undefined; 
+}
+
+export const CustomUrlPreview = (props: Props )=> {
   const getDomain = (url: string )=> {
     let domain = url && url.replace('https://', '').replace('http://', '');
 
@@ -21,17 +30,28 @@ export const CustomUrlPreview = (props: any )=> {
     <Container>
       <DetailsContainer>
         <TitleUrl>{getDomain(props.title_link)}</TitleUrl>
-        <Title>{props.title}</Title>
-        <Description>{props.text}</Description>
+        <Title>
+          {truncate(props.title, {
+            length: 50,
+            omission: '...',
+          })}
+        </Title>
+        <View>
+          <Thumbnail
+            source={{
+              // @ts-ignore
+              url: props.image_url || props.thumb_url,
+            }}
+            resizeMode="cover"
+          />
+        </View>
+        <Description> 
+          { truncate(props.text, {
+            length: 100,
+            omission: '...',
+          })}
+        </Description>
       </DetailsContainer>
-      <View>
-        <Thumbnail
-          source={{
-            url: props.image_url || props.thumb_url,
-          }}
-          resizeMode="cover"
-        />
-      </View>
     </Container>
   );
 };
