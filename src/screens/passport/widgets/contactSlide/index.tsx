4b40233bcb86c0e0ecb4e96a4c913847743b95db
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-03-18 23:38:09 +0100

    remove slice

--- src/screens/passport/widgets/contactSlide/index.tsx
+++ src/screens/passport/widgets/contactSlide/index.tsx
@@ -110,7 +110,6 @@ function ContactSlide(props: ScreenProp) {
   const participantOf = state?.participantOf;
   const myConnections = state?.myConnections;
   const currentLocation = state?.currentLocation;
-  const myConnections = state?.myConnections?.slice(0, 10);
   const myChannels = state?.recentChannels;
 
   const _renderMyConnectionItem = ({ item }: { item: PassportInterface }) => (
