67decc959ad6c2b968b8f3840c87d0646c1b01b8
Eto Olei <teneeto@Etos-MacBook-Air.local>
2021-03-18 06:48:08 +0100

    cleanup and refactor reaction picker

--- src/components/customReactionPicker/index.tsx
+++ src/components/customReactionPicker/index.tsx
@@ -1,37 +1,26 @@
-import {useTheme} from '@react-navigation/native';
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 import {
   Modal,
-  View,
-  Text,
-  Animated,
-  TouchableOpacity,
-  SectionList,
-  StyleSheet,
 } from 'react-native';
-// import {SCText} from './SCText';
-// import * as Haptics from 'expo-haptics';
-// import {groupedSupportedReactions} from '../utils/supportedReactions';
 
-// import EmojiSelector, { Categories } from "react-native-emoji-selector";
 import EmojiBoard from 'react-native-emoji-board';
 
+import { useThemeContext } from '../../theme';
+import { Overlay, PickerContainer, BoardContainer, InnerWrapper } from './styles';
 
-export const ReactionPicker = (props) => {
+export const ReactionPicker = (props: any) => {
   const {dismissReactionPicker, handleReaction, reactionPickerVisible} = props;
-  const {colors} = useTheme();
+  
+  const { colors } = useThemeContext();
+  
   const _dismissReactionPicker = () => {
-
       dismissReactionPicker();
-
   };
 
-  const _handleReaction = (type: any) => {
+  const _handleReaction = (type: { name: string }) => {
     handleReaction(type.name);
 		_dismissReactionPicker();
   };
-
-
 
   return (
     <Modal
@@ -39,84 +28,20 @@ export const ReactionPicker = (props) => {
       onRequestClose={_dismissReactionPicker}
       transparent
       visible={reactionPickerVisible}>
-      <TouchableOpacity
-        style={styles.overlay}
+      <Overlay
         activeOpacity={1}
         onPress={() => {
           _dismissReactionPicker();
         }}
       >
-
-        <View
-          style={[
-            {
-              backgroundColor: colors.background,
-            },
-            styles.pickerContainer,
-          ]}>
-          <View style={styles.listContainer}>
-            <View style={{height: 300, minWidth: '100%', }}>
-                {/* <Text style={{height: 300, minWidth: '100%',}}> */}
-								<EmojiBoard showBoard={true} onClick={_handleReaction} />
-								{/* </Text> */}
-            </View>
-          </View>
-        </View>
-				</TouchableOpacity>
+          <PickerContainer>
+            <BoardContainer>
+              <InnerWrapper>
+                <EmojiBoard containerStyle={{backgroundColor: colors.WHITE}} hideBackSpace showBoard onClick={_handleReaction} />
+              </InnerWrapper>
+            </BoardContainer>
+          </PickerContainer>
+				</Overlay>
     </Modal>
   );
 };
-
-const styles = StyleSheet.create({
-  overlay: {
-    flex: 1,
-		width: '100%',
-    // alignSelf: 'flex-end',
-    // alignItems: 'flex-start',
-		alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.7)',
-  },
-  animatedContainer: {
-    position: 'absolute',
-    backgroundColor: 'transparent',
-		justifyContent:'center',
-		alignItems: 'center'
-
-  },
-  pickerContainer: {
-    flexDirection: 'column',
-    borderRadius: 15,
-    padding: 10,
-		// marginBottom: 300, 
-		// overflow: 'scroll'
-  },
-  listContainer: {
-    width: '100%',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    marginBottom: 20,
-  },
-  groupTitle: {
-    padding: 10,
-    paddingLeft: 13,
-    fontWeight: '200',
-  },
-  reactionsRow: {
-    width: '100%',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    marginTop: 3,
-  },
-  reactionsItemContainer: {
-    alignItems: 'center',
-    marginTop: -5,
-  },
-  reactionsItem: {
-    fontSize: 35,
-    margin: 5,
-    marginVertical: 5,
-  },
-});
