867dcee496f0bcc399474caf8d17103db59d7153
Eto Olei <teneeto@gmail.com>
2021-03-18 08:14:53 +0100

    add styled component for custom Message Footer

--- src/components/customMessageFooter/styles.ts
+++ src/components/customMessageFooter/styles.ts
@@ -1,6 +1,37 @@
 import styled from 'styled-components/native';
 
-export const MoreEmoji = styled.Text`
+const ReactionListContainer = styled.View`
+  flex-direction: row;
+	align-self: flex-start;
+	align-items: center;
+	margin-top: 5px;
+	margin-bottom: 10px;
+	margin-left: 10px;
+	flex-wrap: wrap;
+`
+const ReactionItemContainer = styled.TouchableOpacity`
+	border-width: 1px;
+	padding: 4px;
+	padding-left: 8px;
+	padding-right: 8px;
+	border-radius: 17px;
+	margin-right: 6px;
+	margin-top: 5px;
+`
+const StyledReactionItem = styled.Text`
+	font-size: 16px;
+	color: #0064c2;
+`
+
+const ReactionPickerContainer = styled.TouchableOpacity`
+	padding: 6px;
+	padding-left: 8px;
+	padding-right: 8px;
+	border-radius: 10px;
+	background-color: #F0F0F0;
+`
+
+const MoreEmoji = styled.Text`
   position: absolute;
   top: 0;
   right: 0;
@@ -8,3 +39,5 @@ export const MoreEmoji = styled.Text`
   font-family: ${({ theme }) => theme.fonts.WORK_SANS_MEDIUM};
   z-index: 1;
 `
+
+export { ReactionListContainer, ReactionItemContainer, StyledReactionItem, ReactionPickerContainer, MoreEmoji }
