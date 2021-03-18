476a2f73da9af63ea52cad80ac9c4e82ecd21d43
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-03-18 16:36:38 +0100

    restyle search cover

--- src/screens/passport/widgets/citizenshipScreen/styles.ts
+++ src/screens/passport/widgets/citizenshipScreen/styles.ts
@@ -3,3 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.WHITE};
 `;
+
+export const SearchCover = styled.View`
+  position: relative;
+`;
