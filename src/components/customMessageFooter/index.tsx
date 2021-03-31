cc98aa3409a22ebb22b30983cb762439e2ae6fa9
Eto Olei <teneeto@gmail.com>
2021-04-01 00:58:25 +0100

    add list of reacted users modal

--- src/components/customMessageFooter/index.tsx
+++ src/components/customMessageFooter/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useRef } from 'react';
 import EmojiIcon from '../../../assets/icons/emoji';
 import {
   DefaultAttachmentType,
@@ -9,6 +9,16 @@ import {
 import { ReactionPicker } from '../customReactionPicker';
 
 import { ReactionText, Container, MoreReaction, Reaction } from './styles';
+import { Portal } from 'react-native-portalize';
+import { StatusBar } from 'expo-status-bar';
+import { Modalize } from 'react-native-modalize';
+import { Text, View, Image } from 'react-native';
+import { DEVICE_FULL_HEIGHT } from '../../utils/device';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useThemeContext } from '../../theme';
+import { MessageAvatar, Avatar } from 'stream-chat-expo';
+import { Divider, Button } from 'react-native-paper';
+import hexToRGB from '../../utils/hexToRGB';
 
 // DEFINE SCREEN PROP TYPES
 type MessageProps = MessageSimpleProps<
@@ -73,6 +83,7 @@ export const renderReactions = (props: MessageProps) => {
         reactionCounts={reaction_counts}
         emojiDataByType={emojiDataByType}
         ownReactionTypes={ownReactionTypes}
+        latestReactions={latest_reactions}
       />
     ) : null
   );
@@ -83,19 +94,138 @@ const ReactionItem = ({
   handleReaction,
   reactionCounts,
   emojiDataByType,
-  ownReactionTypes
+  ownReactionTypes,
+  latestReactions
 }: any) => {
   const isOwnReaction = ownReactionTypes.indexOf(type) > -1;
 
+  const modalizeRef = useRef<Modalize>(null);
+
+  const openModal = () => modalizeRef.current?.open();
+
+  // const closeModal = () => modalizeRef.current?.close();
+
+  const { colors, fonts } = useThemeContext();
+
+  console.tron('tron', latestReactions);
+
+  const _renderItem = ({ item }: any) => {
+    return (
+      <View
+        style={{
+          marginVertical: 5,
+          flexDirection: 'row',
+          alignItems: 'center'
+        }}
+      >
+        <Text style={{ fontSize: 25 }}>{emojiDataByType[item.type].icon}</Text>
+
+        <View
+          style={{
+            marginLeft: 15,
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}
+        >
+          <Image
+            style={{ height: 30, width: 30, borderRadius: 20 }}
+            source={{
+              uri: item.user.image
+            }}
+          />
+          <Text
+            style={{
+              marginHorizontal: 5,
+              fontFamily: fonts.WORK_SANS_SEMI_BOLD
+            }}
+          >
+            {item ? item.user.user.firstName : null}{' '}
+            {item ? item.user.user.lastName : null}
+          </Text>
+        </View>
+
+        {/* {isOwnReaction && <Button>Undo</Button>} */}
+      </View>
+    );
+  };
+
   return (
-    <Reaction
-      isOwnReaction={isOwnReaction}
-      onPress={() => handleReaction(type)}
-      key={type}
-    >
-      <ReactionText>
-        {emojiDataByType[type].icon} {reactionCounts[type]}
-      </ReactionText>
-    </Reaction>
+    <>
+      <Reaction
+        isOwnReaction={isOwnReaction}
+        onPress={() => handleReaction(type)}
+        onLongPress={() => openModal()}
+        key={type}
+      >
+        <ReactionText>
+          {emojiDataByType[type].icon} {reactionCounts[type]}
+        </ReactionText>
+      </Reaction>
+
+      <Portal>
+        <StatusBar translucent animated style="light" />
+
+        <Modalize
+          ref={modalizeRef}
+          // onClose={closeModal}
+          modalHeight={DEVICE_FULL_HEIGHT / 2.5}
+          modalStyle={{
+            paddingTop: RFValue(30),
+            paddingBottom: RFValue(20),
+            paddingHorizontal: RFValue(10),
+            backgroundColor: colors.WHITE
+          }}
+          handlePosition="inside"
+          HeaderComponent={
+            <Fragment>
+              <Text
+                style={{
+                  fontFamily: fonts.WORK_SANS_BOLD,
+                  fontSize: RFValue(fonts.LARGE_SIZE),
+                  color: colors.PRIMARY_TEXT,
+                  textTransform: 'capitalize',
+                  lineHeight: 20,
+                  // marginLeft: RFValue(15),
+                  marginBottom: RFValue(10),
+                  textAlign: 'center'
+                }}
+              >
+                Reactions
+              </Text>
+              <Divider />
+            </Fragment>
+          }
+          flatListProps={{
+            data: latestReactions,
+            renderItem: _renderItem,
+            showsVerticalScrollIndicator: true,
+            keyExtractor: ({ id }: any) => id,
+            // contentContainerStyle: { flexGrow: 1, paddingBottom: 120 },
+            contentContainerStyle: {
+              marginTop: 20,
+              marginBottom: 100,
+              paddingHorizontal: 15,
+              paddingBottom: 40,
+              backgroundColor: colors.WHITE
+            },
+            ItemSeparatorComponent: () => (
+              <Divider
+                style={{
+                  height: 1.5,
+                  backgroundColor: hexToRGB(colors.INACTIVE, 0.5)
+                  // marginHorizontal: RFValue(20)
+                }}
+              />
+            ),
+            ListFooterComponentStyle: { justifyContent: 'center' }
+          }}
+          // ListFooterComponent={_renderFooter}
+          // renderItem={_renderMyCommunityItem}
+          // onMomentumScrollEnd={handleEndReach}
+          // ListFooterComponentStyle={{ justifyContent: 'center' }}
+          // showsHorizontalScrollIndicator={false}
+        />
+      </Portal>
+    </>
   );
 };
