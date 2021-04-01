2a1d3bafe7312726b1720bd6e669486e07c8b58c
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-04-01 19:13:43 +0100

    remove unused state

--- src/screens/community/home/widget/addAdmin/widget/index.tsx
+++ src/screens/community/home/widget/addAdmin/widget/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import CheckBox from '@react-native-community/checkbox';
 import { Text, TouchableRipple, Title } from 'react-native-paper';
 import FastImage from 'react-native-fast-image';
