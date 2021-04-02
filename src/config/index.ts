77c4a8148df69b59261121bd8b3407c1fc027799
josemak25 <amakirij@gmail.com>
2021-04-02 12:26:29 +0100

    add axios config for graphql

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,10 +2,13 @@ import { NativeModules } from 'react-native';
 import Reactotron from 'reactotron-react-native';
 //@ts-ignore
 import RNUxcam from 'react-native-ux-cam';
+import axios from 'axios';
 import ENVIRONMENT_VARIABLES from 'react-native-config';
 import RNMixpanel from 'react-native-mixpanel';
 import MixpanelAnalytics from '../libs/mixpanel';
 import algolia from 'algoliasearch';
+import Storage from '../libs/storage';
+import { VerifyOTPIT } from '../graphql/types';
 
 declare global {
   interface Console {
@@ -39,6 +42,32 @@ RNMixpanel.sharedInstanceWithToken(ENVIRONMENT_VARIABLES.TRIBL_MIX_PANEL).then(
     RNMixpanel.optOutTracking();
     RNMixpanel.optInTracking();
   }
+);
+
+// Set config defaults when creating the instance
+export const axiosInstance = axios.create({
+  baseURL: ENVIRONMENT_VARIABLES.TRIBL_SERVER_BASE_URI
+});
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const storageData = await Storage.getUserCredentials();
+
+    // Do something before request is sent
+    let authToken: string | undefined = undefined;
+
+    if (storageData) {
+      const credentials = JSON.parse(storageData) as VerifyOTPIT;
+      authToken = credentials.id_token;
+    }
+
+    return {
+      ...config,
+      headers: { ...config.headers, authorization: authToken }
+    };
+  },
+  (error) => Promise.reject(error)
 );
 
 export const Mixpanel = new MixpanelAnalytics();
