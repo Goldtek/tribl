9e2b05cf1d79924db9ef00dd3a723d08273ace54
josemak25 <amakirij@gmail.com>
2021-04-06 23:26:45 +0100

    add group message information view

---
+++ src/screens/inbox/groupInformation/index.tsx
@@ -0,0 +1,320 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import { NavigationInterface } from '../../types';
+import { Text, TouchableRipple, Paragraph, Divider } from 'react-native-paper';
+import { Switch, ScrollView, Alert } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import FastImage from 'react-native-fast-image';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { AntDesign, Ionicons, Feather, Entypo } from '@expo/vector-icons';
+import { crashlytics } from '../../../firebase/config';
+import { useThemeContext } from '../../../theme';
+import { tagScreenName } from '../../../utils/uxcamHelper';
+import { useStreamContext } from '../../../stream';
+import { chatClient } from '../../../stream/types';
+import { Mixpanel } from '../../../config';
+
+// IMPORT FOR ALL CUSTOM STYLES
+import {
+  LeftCover,
+  Container,
+  RightCover,
+  OptionWrapper,
+  HeaderContainer,
+  HeaderImageContainer,
+  HeaderTitleContainer,
+  ChannelInformationContainer
+} from './styles';
+import { USER_DEFAULT_AVATAR } from '../../../constants';
+
+// DEFINE SCREEN PROP TYPES
+interface GroupInformationProp extends NavigationInterface {}
+
+export default function GroupInformation(props: GroupInformationProp) {
+  const { navigation } = props;
+
+  const { t } = useTranslation();
+  const { channel } = useStreamContext();
+  const { colors, fonts } = useThemeContext();
+
+  useEffect(() => {
+    tagScreenName('GroupInformationScreen');
+    Mixpanel.track('Group Information', {
+      info: `User views group information`,
+      'Activity Screen': 'Group Information Screen'
+    });
+  }, []);
+
+  const getMuteStatus = channel.muteStatus().muted;
+  const [muted, setMuted] = useState(getMuteStatus);
+
+  const channelMembers = Object.values(channel.state.members);
+
+  const groupCreationDate = new Date(
+    channel.data?.created_at as string
+  ).toDateString();
+
+  const toggleMute = async () => {
+    try {
+      if (muted) {
+        await channel.unmute();
+        setMuted(false);
+      } else {
+        await channel.mute();
+        setMuted(true);
+      }
+    } catch {
+      setMuted(getMuteStatus);
+    }
+  };
+
+  const handleLeaveGroup = async () => {
+    Alert.alert('Leave group', `Are you sure you want to leave this group`, [
+      {
+        text: 'Cancel',
+        onPress: () => {},
+        style: 'cancel'
+      },
+      {
+        text: 'Leave',
+        onPress: async () => {
+          try {
+            await channel.removeMembers([`${chatClient.user?.id}`]);
+            navigation.navigate('InboxScreen');
+          } catch (error) {
+            crashlytics.recordError(new Error(error));
+          }
+        }
+      }
+    ]);
+  };
+
+  const handleReportGroup = async () => {
+    // await leaveChannel({ variables: { payload: { channelId: channel.id } } });
+    // navigation.goBack();
+  };
+
+  return (
+    <Container>
+      <HeaderContainer>
+        <TouchableRipple
+          onPress={navigation.goBack}
+          style={{
+            height: 40,
+            width: 40,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 40 / 2,
+            marginRight: 10
+          }}
+        >
+          <Ionicons name="md-arrow-back" size={24} color={colors.PRIMARY} />
+        </TouchableRipple>
+
+        <HeaderTitleContainer>
+          <Paragraph
+            style={{
+              fontSize: fonts.MEDIUM_SIZE + 2,
+              fontFamily: fonts.WORK_SANS_BOLD,
+              marginHorizontal: 5
+            }}
+          >
+            {t(`community.chat.groupInformation`)}
+          </Paragraph>
+        </HeaderTitleContainer>
+      </HeaderContainer>
+
+      <ScrollView
+        bounces={false}
+        contentContainerStyle={{ paddingVertical: 70 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <HeaderImageContainer>
+          <FastImage
+            source={{
+              uri: channelMembers[0]?.user?.image || USER_DEFAULT_AVATAR,
+              priority: FastImage.priority.high
+            }}
+            style={{ width: '33.5%', height: '100%' }}
+            resizeMode={FastImage.resizeMode.stretch}
+          />
+          <FastImage
+            source={{
+              uri:
+                channelMembers[channelMembers?.length - 2]?.user?.image ||
+                USER_DEFAULT_AVATAR,
+              priority: FastImage.priority.high
+            }}
+            style={{ width: '33.5%', height: '100%' }}
+            resizeMode={FastImage.resizeMode.stretch}
+          />
+          <FastImage
+            source={{
+              uri:
+                channelMembers[channelMembers?.length - 1]?.user?.image ||
+                USER_DEFAULT_AVATAR,
+              priority: FastImage.priority.high
+            }}
+            style={{ width: '33.5%', height: '100%' }}
+            resizeMode={FastImage.resizeMode.stretch}
+          />
+        </HeaderImageContainer>
+
+        <ChannelInformationContainer>
+          <Text
+            style={{
+              fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+              fontSize: RFValue(fonts.LARGE_SIZE + 1),
+              color: colors.PRIMARY,
+              textTransform: 'capitalize'
+            }}
+          >
+            {`#${channel.data?.name}`}
+          </Text>
+          <Paragraph
+            style={{
+              fontSize: fonts.MEDIUM_SIZE,
+              fontFamily: fonts.WORK_SANS_MEDIUM
+            }}
+          >
+            {t(`community.chat.groupCreatedBy`)} {channel.data?.created_by.name}
+            {`, ${groupCreationDate}`}
+          </Paragraph>
+        </ChannelInformationContainer>
+
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+        <OptionWrapper onPress={toggleMute}>
+          <Fragment>
+            <LeftCover>
+              <Ionicons
+                name="ios-notifications-outline"
+                size={25}
+                color={colors.PRIMARY}
+                style={{ paddingRight: RFValue(10) }}
+              />
+              <Text
+                style={{
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: fonts.LARGE_SIZE,
+                  color: colors.PRIMARY_TEXT
+                }}
+              >
+                {t(`community.chat.muteNotifications`)}
+              </Text>
+            </LeftCover>
+            <RightCover>
+              <Switch
+                trackColor={{ false: colors.DISABLED, true: colors.PRIMARY }}
+                thumbColor={colors.WHITE}
+                ios_backgroundColor={colors.DISABLED}
+                onValueChange={toggleMute}
+                value={muted}
+                style={{ transform: [{ scaleX: 0.9 }, { scaleY: 0.9 }] }}
+              />
+            </RightCover>
+          </Fragment>
+        </OptionWrapper>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+        <OptionWrapper
+          onPress={() => navigation.navigate('GroupMembersScreen')}
+        >
+          <Fragment>
+            <LeftCover>
+              <Feather
+                name="users"
+                size={20}
+                color={colors.PRIMARY}
+                style={{ paddingRight: RFValue(10) }}
+              />
+              <Text
+                style={{
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: fonts.LARGE_SIZE,
+                  color: colors.PRIMARY_TEXT,
+                  textTransform: 'capitalize'
+                }}
+              >
+                {t(`community.chat.members`)}
+              </Text>
+            </LeftCover>
+            <RightCover>
+              <Text
+                style={{
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: fonts.LARGE_SIZE,
+                  color: colors.PRIMARY_TEXT,
+                  paddingRight: RFValue(10)
+                }}
+              >
+                {(channel?.data?.member_count as unknown) as string}
+              </Text>
+              <AntDesign name="right" size={20} color={colors.PRIMARY_TEXT} />
+            </RightCover>
+          </Fragment>
+        </OptionWrapper>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+        <OptionWrapper
+          onPress={() => navigation.navigate('AddMembersToGroupScreen')}
+        >
+          <LeftCover>
+            <AntDesign
+              name="addusergroup"
+              size={20}
+              color={colors.PRIMARY}
+              style={{ paddingRight: RFValue(10) }}
+            />
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: fonts.LARGE_SIZE,
+                color: colors.PRIMARY_TEXT
+              }}
+            >
+              {t(`community.chat.inviteToGroup`)}
+            </Text>
+          </LeftCover>
+        </OptionWrapper>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+        <OptionWrapper onPress={handleLeaveGroup}>
+          <LeftCover>
+            <Entypo
+              name="log-out"
+              size={20}
+              color={colors.RED}
+              style={{ paddingRight: RFValue(10) }}
+            />
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: fonts.LARGE_SIZE,
+                color: colors.PRIMARY_TEXT
+              }}
+            >
+              {t(`community.chat.leaveGroup`)}
+            </Text>
+          </LeftCover>
+        </OptionWrapper>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+        <OptionWrapper onPress={handleReportGroup}>
+          <LeftCover>
+            <Entypo
+              name="thumbs-down"
+              size={20}
+              color={colors.RED}
+              style={{ paddingRight: RFValue(10) }}
+            />
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: fonts.LARGE_SIZE,
+                color: colors.PRIMARY_TEXT
+              }}
+            >
+              {t(`community.chat.reportGroup`)}
+            </Text>
+          </LeftCover>
+        </OptionWrapper>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+      </ScrollView>
+    </Container>
+  );
+}
---
+++ src/screens/inbox/groupInformation/styles.ts
@@ -0,0 +1,57 @@
+import Constants from 'expo-constants';
+import styled from 'styled-components/native';
+import { TouchableRipple } from 'react-native-paper';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.WHITE};
+`;
+
+export const OptionWrapper = styled(TouchableRipple)`
+  height: 60px;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const LeftCover = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding-left: ${RFValue(20)}px;
+`;
+
+export const RightCover = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding-right: ${RFValue(20)}px;
+`;
+
+export const HeaderContainer = styled.View`
+  width: 100%;
+  height: 70px;
+  margin-top: ${Constants.statusBarHeight}px;
+  flex-direction: row;
+  align-items: center;
+  z-index: 999;
+  position: absolute;
+  background-color: ${({ theme }) => theme.colors.WHITE};
+  border: 1px solid ${({ theme }) => theme.colors.DISABLED};
+  border-top-color: transparent;
+`;
+
+export const HeaderTitleContainer = styled.View`
+  flex: 1;
+  padding-horizontal: 10px;
+`;
+
+export const ChannelInformationContainer = styled.View`
+  margin-vertical: 10px;
+  padding-horizontal: 10px;
+`;
+
+export const HeaderImageContainer = styled.View`
+  flex-direction: row;
+  height: ${RFValue(220)}px;
+`;
