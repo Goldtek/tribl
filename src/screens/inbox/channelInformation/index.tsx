2c9cf9891abf93909ce74a171287e63f12b014b0
josemak25 <amakirij@gmail.com>
2021-04-06 23:23:33 +0100

    add channel information

--- src/screens/inbox/channelInformation/index.tsx
+++ src/screens/inbox/channelInformation/index.tsx
@@ -1,26 +1,41 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { NavigationInterface } from '../../types';
-import { Text, TouchableRipple, Divider } from 'react-native-paper';
-import { Switch } from 'react-native';
+import { Text, TouchableRipple, Paragraph, Divider } from 'react-native-paper';
+import { Switch, Alert, ScrollView } from 'react-native';
 import { useTranslation } from 'react-i18next';
-// @ts-ignore
-import SingleImage from '../../../libs/react-native-zoom-lightbox';
 import { RFValue } from 'react-native-responsive-fontsize';
+import FastImage from 'react-native-fast-image';
 import { AntDesign, Ionicons, Feather, Entypo } from '@expo/vector-icons';
+import { LEAVE_COMMUNITY_CHANNEL } from '../../../graphql/server/mutations';
 import { useThemeContext } from '../../../theme';
+import { useMutation } from '@apollo/react-hooks';
 import { tagScreenName } from '../../../utils/uxcamHelper';
+import { useStreamContext } from '../../../stream';
+import { crashlytics } from '../../../firebase/config';
 import { Mixpanel } from '../../../config';
 
 // IMPORT FOR ALL CUSTOM STYLES
-import { Container, LeftCover, RightCover } from './styles';
+import {
+  LeftCover,
+  Container,
+  RightCover,
+  OptionWrapper,
+  HeaderContainer,
+  HeaderTitleContainer,
+  ChannelInformationContainer
+} from './styles';
 
 // DEFINE SCREEN PROP TYPES
 interface MyChannelInformationProp extends NavigationInterface {}
 
 export default function ChannelInformation(props: MyChannelInformationProp) {
   const { navigation } = props;
-  const { colors, fonts } = useThemeContext();
+
   const { t } = useTranslation();
+  const { channel } = useStreamContext();
+  const { colors, fonts } = useThemeContext();
+
+  const [leaveChannel] = useMutation(LEAVE_COMMUNITY_CHANNEL);
 
   useEffect(() => {
     tagScreenName('ChannelInformationScreen');
@@ -30,94 +45,199 @@ export default function ChannelInformation(props: MyChannelInformationProp) {
     });
   }, []);
 
-  const [isEnabled, setIsEnabled] = useState(false);
+  const getMuteStatus = channel.muteStatus().muted;
+  const [muted, setMuted] = useState(getMuteStatus);
 
-  const toggleSwitch = async () => {
-    setIsEnabled((previousState) => !previousState);
+  const channelCreationDate = new Date(
+    channel.data?.created_at as string
+  ).toDateString();
+
+  const toggleMute = async () => {
+    try {
+      if (muted) {
+        await channel.unmute();
+        setMuted(false);
+      } else {
+        await channel.mute();
+        setMuted(true);
+      }
+    } catch {
+      setMuted(getMuteStatus);
+    }
+  };
+
+  const handleLeaveChannel = async () => {
+    Alert.alert('Leave group', `Are you sure you want to leave this group`, [
+      {
+        text: 'Cancel',
+        onPress: () => {},
+        style: 'cancel'
+      },
+      {
+        text: 'Leave',
+        onPress: async () => {
+          try {
+            await leaveChannel({
+              variables: { payload: { channelId: channel.id } }
+            });
+            navigation.navigate('InboxScreen');
+          } catch (error) {
+            crashlytics.recordError(new Error(error));
+          }
+        }
+      }
+    ]);
+  };
+
+  const handleReportChannel = async () => {
+    // await leaveChannel({ variables: { payload: { channelId: channel.id } } });
+    // navigation.goBack();
   };
 
   return (
     <Container>
-      <SingleImage
-        uri={'xfgchj'}
-        style={{
-          width: RFValue(110),
-          height: RFValue(110),
-          borderRadius: 80,
-          borderWidth: 4,
-          marginTop: RFValue(35),
-          marginBottom: RFValue(5),
-          marginLeft: 'auto',
-          marginRight: 'auto'
-        }}
-      />
-      <Text
-        style={{
-          fontFamily: fonts.WORK_SANS_SEMI_BOLD,
-          fontSize: RFValue(fonts.LARGE_SIZE + 2),
-          color: colors.PRIMARY_TEXT,
-          textTransform: 'capitalize',
-          textAlign: 'center',
-          marginBottom: RFValue(15)
-        }}
+      <HeaderContainer>
+        <TouchableRipple
+          onPress={navigation.goBack}
+          style={{
+            height: 40,
+            width: 40,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 40 / 2,
+            marginRight: 10
+          }}
+        >
+          <Ionicons name="md-arrow-back" size={24} color={colors.PRIMARY} />
+        </TouchableRipple>
+
+        <HeaderTitleContainer>
+          <Paragraph
+            style={{
+              fontSize: fonts.MEDIUM_SIZE + 2,
+              fontFamily: fonts.WORK_SANS_BOLD,
+              marginHorizontal: 5
+            }}
+          >
+            {t(`community.chat.channelInformation`)}
+          </Paragraph>
+        </HeaderTitleContainer>
+      </HeaderContainer>
+
+      <ScrollView
+        bounces={false}
+        contentContainerStyle={{ paddingVertical: 70 }}
+        showsVerticalScrollIndicator={false}
       >
-        General
-      </Text>
-      <Divider style={{ backgroundColor: colors.INPUT }} />
-      <TouchableRipple
-        onPress={() => {}}
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          height: RFValue(60)
-        }}
-      >
-        <Fragment>
+        <FastImage
+          source={{
+            uri: channel.data?.image || channel.data?.community.avatar,
+            priority: FastImage.priority.high
+          }}
+          style={{ width: '100%', height: RFValue(220) }}
+          resizeMode={FastImage.resizeMode.stretch}
+        />
+
+        <ChannelInformationContainer>
+          <Text
+            style={{
+              fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+              fontSize: RFValue(fonts.LARGE_SIZE + 1),
+              color: colors.PRIMARY,
+              textTransform: 'capitalize'
+            }}
+          >
+            {`#${channel.data?.name}`}
+          </Text>
+          <Paragraph
+            style={{
+              fontSize: fonts.MEDIUM_SIZE + 2,
+              fontFamily: fonts.WORK_SANS_MEDIUM
+            }}
+          >
+            {t(`community.chat.createdBy`)} {channelCreationDate}
+          </Paragraph>
+        </ChannelInformationContainer>
+
+        <Divider style={{ backgroundColor: colors.INPUT, height: 10 }} />
+        <OptionWrapper onPress={toggleMute}>
+          <Fragment>
+            <LeftCover>
+              <Ionicons
+                name="ios-notifications-outline"
+                size={25}
+                color={colors.PRIMARY}
+                style={{ paddingRight: RFValue(10) }}
+              />
+              <Text
+                style={{
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: fonts.LARGE_SIZE,
+                  color: colors.PRIMARY_TEXT
+                }}
+              >
+                {t(`community.chat.muteNotifications`)}
+              </Text>
+            </LeftCover>
+            <RightCover>
+              <Switch
+                trackColor={{ false: colors.DISABLED, true: colors.PRIMARY }}
+                thumbColor={colors.WHITE}
+                ios_backgroundColor={colors.DISABLED}
+                onValueChange={toggleMute}
+                value={muted}
+                style={{ transform: [{ scaleX: 0.9 }, { scaleY: 0.9 }] }}
+              />
+            </RightCover>
+          </Fragment>
+        </OptionWrapper>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+
+        <OptionWrapper
+          onPress={() => navigation.navigate('ChannelMembersScreen')}
+        >
+          <Fragment>
+            <LeftCover>
+              <Feather
+                name="users"
+                size={20}
+                color={colors.PRIMARY}
+                style={{ paddingRight: RFValue(10) }}
+              />
+              <Text
+                style={{
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: fonts.LARGE_SIZE,
+                  color: colors.PRIMARY_TEXT,
+                  textTransform: 'capitalize'
+                }}
+              >
+                {t(`community.chat.members`)}
+              </Text>
+            </LeftCover>
+            <RightCover>
+              <Text
+                style={{
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: fonts.LARGE_SIZE,
+                  color: colors.PRIMARY_TEXT,
+                  paddingRight: RFValue(10)
+                }}
+              >
+                {`${channel.data?.member_count}`}
+              </Text>
+              <AntDesign name="right" size={20} color={colors.PRIMARY_TEXT} />
+            </RightCover>
+          </Fragment>
+        </OptionWrapper>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+
+        <OptionWrapper
+          onPress={() => navigation.navigate('InvitationToChannelScreen')}
+        >
           <LeftCover>
-            <Ionicons
-              name="ios-notifications-outline"
-              size={25}
-              color={colors.PRIMARY}
-              style={{ paddingRight: RFValue(10) }}
-            />
-            <Text
-              style={{
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: fonts.LARGE_SIZE,
-                color: colors.PRIMARY_TEXT,
-                textTransform: 'capitalize'
-              }}
-            >
-              {t(`community.chat.mute`)}
-            </Text>
-          </LeftCover>
-          <RightCover>
-            <Switch
-              trackColor={{ false: colors.DISABLED, true: colors.PRIMARY }}
-              thumbColor={colors.WHITE}
-              ios_backgroundColor={colors.DISABLED}
-              onValueChange={toggleSwitch}
-              value={isEnabled}
-              style={{ transform: [{ scaleX: 0.9 }, { scaleY: 0.9 }] }}
-            />
-          </RightCover>
-        </Fragment>
-      </TouchableRipple>
-      <Divider style={{ backgroundColor: colors.INPUT }} />
-      <TouchableRipple
-        onPress={() => {}}
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          height: RFValue(60)
-        }}
-      >
-        <Fragment>
-          <LeftCover>
-            <Feather
-              name="users"
+            <AntDesign
+              name="addusergroup"
               size={20}
               color={colors.PRIMARY}
               style={{ paddingRight: RFValue(10) }}
@@ -126,59 +246,57 @@ export default function ChannelInformation(props: MyChannelInformationProp) {
               style={{
                 fontFamily: fonts.WORK_SANS_REGULAR,
                 fontSize: fonts.LARGE_SIZE,
-                color: colors.PRIMARY_TEXT,
-                textTransform: 'capitalize'
+                color: colors.PRIMARY_TEXT
               }}
             >
-              {t(`community.chat.members`)}
+              {t(`community.chat.inviteToChannel`)}
             </Text>
           </LeftCover>
-          <RightCover>
+        </OptionWrapper>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+
+        <OptionWrapper onPress={handleLeaveChannel}>
+          <LeftCover>
+            <Entypo
+              name="log-out"
+              size={20}
+              color={colors.RED}
+              style={{ paddingRight: RFValue(10) }}
+            />
             <Text
               style={{
                 fontFamily: fonts.WORK_SANS_REGULAR,
                 fontSize: fonts.LARGE_SIZE,
-                color: colors.PRIMARY_TEXT,
-                textTransform: 'capitalize',
-                paddingRight: RFValue(10)
+                color: colors.PRIMARY_TEXT
               }}
             >
-              2
+              {t(`community.chat.leaveChannel`)}
             </Text>
-            <AntDesign name="right" size={20} color={colors.PRIMARY_TEXT} />
-          </RightCover>
-        </Fragment>
-      </TouchableRipple>
-      <Divider style={{ backgroundColor: colors.INPUT }} />
-      <TouchableRipple
-        onPress={() => {}}
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          height: RFValue(60)
-        }}
-      >
-        <LeftCover>
-          <Entypo
-            name="log-out"
-            size={20}
-            color={colors.RED}
-            style={{ paddingRight: RFValue(10) }}
-          />
-          <Text
-            style={{
-              fontFamily: fonts.WORK_SANS_REGULAR,
-              fontSize: fonts.LARGE_SIZE,
-              color: colors.PRIMARY_TEXT,
-              textTransform: 'capitalize'
-            }}
-          >
-            {t(`community.chat.leaveChannel`)}
-          </Text>
-        </LeftCover>
-      </TouchableRipple>
-      <Divider style={{ backgroundColor: colors.INPUT }} />
+          </LeftCover>
+        </OptionWrapper>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+
+        <OptionWrapper onPress={handleReportChannel}>
+          <LeftCover>
+            <Entypo
+              name="thumbs-down"
+              size={20}
+              color={colors.RED}
+              style={{ paddingRight: RFValue(10) }}
+            />
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: fonts.LARGE_SIZE,
+                color: colors.PRIMARY_TEXT
+              }}
+            >
+              {t(`community.chat.reportChannel`)}
+            </Text>
+          </LeftCover>
+        </OptionWrapper>
+        <Divider style={{ backgroundColor: colors.INPUT }} />
+      </ScrollView>
     </Container>
   );
 }
--- src/screens/inbox/channelInformation/styles.ts
+++ src/screens/inbox/channelInformation/styles.ts
@@ -1,10 +1,19 @@
+import Constants from 'expo-constants';
 import styled from 'styled-components/native';
+import { TouchableRipple } from 'react-native-paper';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.WHITE};
-  padding-bottom: 15px;
+`;
+
+export const OptionWrapper = styled(TouchableRipple)`
+  height: 60px;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 export const LeftCover = styled.View`
@@ -17,4 +26,32 @@ export const RightCover = styled.View`
   flex-direction: row;
   align-items: center;
   padding-right: ${RFValue(20)}px;
+`;
+
+export const HeaderContainer = styled.View`
+  width: 100%;
+  height: 70px;
+  margin-top: ${Constants.statusBarHeight}px;
+  flex-direction: row;
+  align-items: center;
+  z-index: 999;
+  position: absolute;
+  background-color: ${({ theme }) => theme.colors.WHITE};
+  border: 1px solid ${({ theme }) => theme.colors.DISABLED};
+  border-top-color: transparent;
+`;
+
+export const HeaderTitleContainer = styled.View`
+  flex: 1;
+  padding-horizontal: 10px;
+`;
+
+export const ChannelInformationContainer = styled.View`
+  margin-vertical: 10px;
+  padding-horizontal: 10px;
+`;
+
+export const HeaderImageContainer = styled.View`
+  height: 220px;
+  padding-horizontal: 10px;
 `;
