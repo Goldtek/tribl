9eea98117267a205ab10cbe0efbea526145611eb
josemak25 <amakirij@gmail.com>
2021-04-14 05:53:21 +0100

    add changes to ota deployment channels

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,12 +16,10 @@ export const LOCAL_GIPHY_CACHE: string = '@LOCAL_GIPHY_CACHE';
 export const SHOW_MODAL: string = '@SHOW_MODAL';
 
 // EXPO RELEASE CHANNELS UPDATE CONSTANTS
-const DEVELOPMENT = 'development_channel_new_api';
-const PRODUCTION: string = 'production_channel_new_api';
-const STAGING_INTERNAL: string = 'staging_internal_channel_new_api';
-const STAGING_EXTERNAL: string = 'staging_external_channel_new_api';
-const STAGING_EXTERNAL_FOR_OTA_TEST: string = `staging_external_channel_for_ota_test`;
-const STAGING_INTERNAL_FOR_OTA_TEST: string = `staging_internal_channel_for_ota_test`;
+const DEVELOPMENT = 'development_channel';
+const STAGING: string = 'staging_channel';
+const PRODUCTION: string = 'production_channel';
+const CURRENTLY_USED_CHANNEL: string = `staging_channel_with_ota`;
 
 // ALL PAGE LOGICAL CONSTANTS
 export const PAGINATION_DEFAULT: number = 20;
