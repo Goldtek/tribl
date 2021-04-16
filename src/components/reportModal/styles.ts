e1019701e17d71c752e261caf365143acd3064dc
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-04-16 13:00:03 +0100

    create and style report modal

---
+++ src/components/reportModal/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const HeaderContainer = styled.View`
+  align-items: center;
+  padding-bottom: ${RFValue(10)}px;
+`;
+
+export const ButtonCover = styled.View`
+  margin: ${RFValue(100)}px ${RFValue(15)}px ${RFValue(15)}px;
+`;
