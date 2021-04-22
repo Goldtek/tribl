e9c92e465bc8192b6652e2aa5d5ad4ee4f10c490
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-04-22 14:27:42 +0100

    implement invite member to tribe with email

--- src/screens/community/invitationToTribe/styles.tsx
+++ src/screens/community/invitationToTribe/styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
@@ -12,11 +13,27 @@ export const ButtonCover = styled.View`
   margin: 0 15px;
 `;
 
-export const AutoTagCover = styled.View`
-  flex: 1;
-  left: 0;
+export const Tags = styled.View`
+  background-color: ${({ theme }) => theme.colors.GREY_LIGHT};
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  border: 1px solid ${({ theme }) => theme.colors.INACTIVE};
+  border-radius: 4px;
+  padding: ${RFValue(10)}px;
+  margin: 0 ${RFValue(15)}px;
+`;
+
+export const TagButtonCover = styled.View`
   position: relative;
-  right: 0;
-  top: 0;
-  z-index: 1;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const InviteCover = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: ${RFValue(40)}px;
+  padding: 0 ${RFValue(15)}px;
 `;
