445ff6083255fff591828cb6362707d90425a8ba
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-04-22 14:23:29 +0100

    create and style blockaccount screens

---
+++ src/screens/account/blockedAccounts/widget/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const TextContainer = styled.View`
+  flex: 1;
+  margin-left: ${RFValue(20)}px;
+`;
