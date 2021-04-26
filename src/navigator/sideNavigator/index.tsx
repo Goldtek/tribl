6c249f5bff677f6dcf0a157afda0408f95a2ef3b
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-04-26 19:10:51 +0100

    add new channel screens to navigators

--- src/navigator/sideNavigator/index.tsx
+++ src/navigator/sideNavigator/index.tsx
@@ -114,6 +114,13 @@ export default function DrawerStackNavigator() {
     setCommunityMenu(false);
   };
 
+  const ChannelRequestNavigation = (id: string) => {
+    navigation.navigate('ChannelRequestScreen', {
+      communityId: id
+    });
+    setCommunityMenu(false);
+  };
+
   const [connectionLoading, setConnectionLoading] = useState(false);
 
   const [id, setID] = useState('');
@@ -746,6 +753,35 @@ export default function DrawerStackNavigator() {
                           requestNavigation(route.params?.details?.id)
                         }
                         title={t(`community.recommended.communityRequests`)}
+                        style={{
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          paddingTop: 10,
+                          paddingBottom: 10,
+                          paddingLeft: 10,
+                          paddingRight: 10
+                        }}
+                        titleStyle={{
+                          fontFamily: fonts.WORK_SANS_REGULAR,
+                          color: colors.PRIMARY_TEXT,
+                          textAlign: 'center',
+                          textTransform: 'capitalize'
+                        }}
+                      />
+                    </Fragment>
+                  ) : null
+                }
+                {
+                  //@ts-ignore
+                  route?.params?.details?.isModerator ? (
+                    <Fragment>
+                      <Divider />
+                      <Menu.Item
+                        onPress={() =>
+                          //@ts-ignore
+                          ChannelRequestNavigation(route.params?.details?.id)
+                        }
+                        title={t(`community.recommended.newChannelRequest`)}
                         style={{
                           alignItems: 'center',
                           justifyContent: 'center',
