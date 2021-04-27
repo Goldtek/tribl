dd81383a55410179a30485efafdc8f89a05d54fc
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-04-27 01:08:34 +0100

    rename folders

--- src/screens/account/notifications/widgets/general/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components/native';
-
-export const Container = styled.View`
-  flex: 1;
-  background-color: ${({ theme }) => theme.colors.WHITE};
-  padding: 20px 0px;
-`;
