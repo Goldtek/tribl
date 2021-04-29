18a6ba2e53da3cdd523b4c7ed69c5dce4c7a1a10
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-04-29 18:45:51 +0100

    create general feed component

---
+++ src/screens/account/notifications/widgets/tribes/widget/general.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+//@ts-ignore
+import { FlatFeed, Activity } from 'expo-activity-feed';
+import ActivityCard from '../../activityCard';
+import { chatClient } from '../../../../../../stream/types';
+
+export default function General() {
+  const CustomActivity = (props: any) => {
+    if (props?.activity?.activityType === 'COMMUNITY') {
+      return (
+        <Activity
+          {...props}
+          Header={null}
+          Content={
+            <ActivityCard
+              activityType={props?.activity?.activityType}
+              userAvatar={props?.activity?.passport?.avatar}
+              tribeAvatar={props?.activity?.community?.avatar}
+              message={props.activity.message}
+              timeStamp={props.activity.time}
+            />
+          }
+        />
+      );
+    } else {
+      return null;
+    }
+  };
+  return (
+    <FlatFeed
+      feedGroup="timeline"
+      userId={chatClient.user?.id}
+      Activity={CustomActivity}
+    />
+  );
+}
