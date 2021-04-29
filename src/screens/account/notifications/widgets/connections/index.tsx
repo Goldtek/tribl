661e810c5379c9a87ce046b60baa37543ffa3134
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-04-29 18:44:13 +0100

    show connection feed

--- src/screens/account/notifications/widgets/connections/index.tsx
+++ src/screens/account/notifications/widgets/connections/index.tsx
@@ -1,37 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Title } from 'react-native-paper';
 import { RFValue } from 'react-native-responsive-fontsize';
+//@ts-ignore
+import { FlatFeed, Activity } from 'expo-activity-feed';
 import { useThemeContext } from '../../../../../theme';
 import { NavigationInterface } from '../../../../types';
 import { tagScreenName } from '../../../../../utils/uxcamHelper';
+import { chatClient } from '../../../../../stream/types';
+import ActivityCard from '../activityCard';
 
 // IMPORT FOR ALL CUSTOM STYLES
 import { Container } from './styles';
 
 // DEFINE SCREEN PROP TYPES
-interface GeneralSlideProp extends NavigationInterface {}
+interface ConnectionSlideProp extends NavigationInterface {}
 
-export default function GeneralSlide(props: GeneralSlideProp) {
-  const { colors, fonts } = useThemeContext();
-
+export default function ConnectionSlide(props: ConnectionSlideProp) {
   useEffect(() => {
     tagScreenName('NotificationScreen');
   }, []);
 
+  const CustomActivity = (props: any) => {
+    if (props?.activity?.activityType === 'CONNECTION') {
+      return (
+        <Activity
+          {...props}
+          Header={null}
+          Content={
+            <ActivityCard
+              message={props.activity.message}
+              timeStamp={props.activity.time}
+              activityType={props?.activity?.activityType}
+              userAvatar={props?.activity?.passport?.avatar}
+              tribeAvatar={props?.activity?.community?.avatar}
+            />
+          }
+        />
+      );
+    } else {
+      return null;
+    }
+  };
+
   return (
     <Container>
-      <Title
-        style={{
-          color: colors.PRIMARY_TEXT,
-          fontFamily: fonts.WORK_SANS_SEMI_BOLD,
-          fontSize: RFValue(fonts.LARGE_SIZE),
-          marginTop: RFValue(20),
-          marginLeft: RFValue(10),
-          textTransform: 'capitalize'
-        }}
-      >
-        General Notification
-      </Title>
+      <FlatFeed
+        feedGroup="timeline"
+        userId={chatClient.user?.id}
+        Activity={CustomActivity}
+      />
     </Container>
   );
 }
