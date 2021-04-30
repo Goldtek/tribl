b60e835a68a9c17c3ba60e6fe097eb3791fffd82
Eto Olei <teneeto@gmail.com>
2021-04-30 10:37:55 +0100

    add group rename modal

--- src/screens/inbox/groupInformation/widgets/index.tsx
+++ src/screens/inbox/groupInformation/widgets/index.tsx
@@ -1,51 +1,139 @@
 import React, { useEffect, useRef, useState } from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  Platform
-} from 'react-native';
+import { Modal, SafeAreaView } from 'react-native';
 
 import { Portal } from 'react-native-portalize';
 import { Modalize } from 'react-native-modalize';
-import { Button } from 'react-native-paper';
-import * as ImagePicker from 'expo-image-picker';
-import Constants from 'expo-constants';
+import { ActivityIndicator, TouchableRipple } from 'react-native-paper';
+
+import { DEVICE_FULL_HEIGHT } from '../../../../utils/device';
 
 // IMPORT FOR ALL CUSTOM STYLES
 
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useThemeContext } from '../../../../theme';
+import {
+  Container,
+  HeaderTitle,
+  SubjectInput,
+  HeaderAction,
+  LoaderMessage,
+  InputContainer,
+  ContentWrapper,
+  HeaderContainer,
+  HeaderActionText,
+  ModalContentWrapper,
+  Overlay
+} from './styles';
+import { Ionicons } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { crashlytics } from '../../../../firebase/config';
+import { chatClient } from '../../../../stream/types';
 
 export default function EditGroupName(props: any) {
+  const { channel, user } = props;
   const insets = useSafeAreaInsets();
-  const { colors, fonts } = useThemeContext();
+  const { colors } = useThemeContext();
+  const [subject, setSubject] = useState(channel.data?.name);
+  const [loader, setLoader] = useState(false);
+  const closeModal = () => props.modalizeRef.current?.close();
 
-  const modalizeRef = useRef<Modalize>(null);
+  const updateGroupName = async () => {
+    try {
+      setLoader(true);
 
-  const openModal = () => modalizeRef.current?.open();
+      await channel.updatePartial({ set: { name: subject } });
+      await channel.sendMessage({
+        text: `${
+          chatClient.user?.name?.split(' ')[0]
+        } changed group name to ${subject}`,
+        group_system: true,
+        receiver: {
+          id: `${user?.id}`,
+          name: `${user?.name}`,
+          image: `${user?.image}`
+        }
+      });
 
-  const closeModal = () => modalizeRef.current?.close();
+      setLoader(false);
+      closeModal();
+    } catch (error) {
+      setLoader(false);
+      crashlytics.recordError(new Error(error));
+      crashlytics.log(`ERROR MESSAGE, ${error.toString()}`);
+    }
+  };
 
   return (
     <SafeAreaView>
       <Portal>
         <Modalize
-          ref={modalizeRef}
+          ref={props.modalizeRef}
+          modalHeight={DEVICE_FULL_HEIGHT}
           modalStyle={{
             borderTopLeftRadius: 30,
             borderTopRightRadius: 30,
             paddingBottom: 20,
-            paddingHorizontal: 10,
-            backgroundColor: colors.PRIMARY
+            paddingTop: insets.top,
+            backgroundColor: colors.WHITE
           }}
           childrenStyle={{ paddingBottom: insets.bottom }}
           handlePosition="inside"
-          adjustToContentHeight
-        ></Modalize>
+          panGestureEnabled={false}
+        >
+          <Container>
+            <HeaderContainer>
+              <TouchableRipple
+                onPress={closeModal}
+                style={{
+                  height: RFValue(40),
+                  width: RFValue(40),
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: RFValue(40 / 2)
+                }}
+              >
+                <Ionicons
+                  name="md-arrow-back"
+                  size={RFValue(24)}
+                  color={colors.PRIMARY}
+                />
+              </TouchableRipple>
+              <HeaderTitle>Edit Group Subject</HeaderTitle>
+              <ContentWrapper style={{ flex: 1, paddingHorizontal: 0 }}>
+                <HeaderAction
+                  onPress={updateGroupName}
+                  disabled={!Boolean(subject)}
+                >
+                  <HeaderActionText selectedParticipants={Boolean(subject)}>
+                    Save
+                  </HeaderActionText>
+                </HeaderAction>
+              </ContentWrapper>
+            </HeaderContainer>
+            <InputContainer>
+              <SubjectInput
+                autoFocus
+                placeholder="Type new group subject here..."
+                value={subject}
+                onChangeText={(text) => setSubject(text)}
+              />
+            </InputContainer>
+          </Container>
+        </Modalize>
       </Portal>
+      <Modal
+        animationType="fade"
+        onRequestClose={() => setLoader(false)}
+        visible={loader}
+        transparent
+      >
+        <Overlay>
+          <ModalContentWrapper>
+            <ActivityIndicator size="large" color={colors.BLACK} />
+            <LoaderMessage>Renaming group...</LoaderMessage>
+          </ModalContentWrapper>
+        </Overlay>
+      </Modal>
     </SafeAreaView>
   );
 }
