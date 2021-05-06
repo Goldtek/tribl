1bb7b0173e0244e5137b13bd09b13f6847b2bbea
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-05-06 21:33:19 +0100

    create and style preview screen

---
+++ src/screens/signup/preview/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const ScrollView = styled.ScrollView`
+  padding-top: 6px;
+  background-color: ${({ theme }) => theme.colors.GREY};
+`;
+
+export const RecommendedList = styled.View`
+  background-color: ${({ theme }) => theme.colors.WHITE};
+  padding-top: ${RFValue(20)}px;
+`;
+
+export const RecommendedListHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-bottom: ${RFValue(10)}px;
+`;
+
+export const RecommendedCommunityContainer = styled.View`
+  margin-top: 10px;
+`;
