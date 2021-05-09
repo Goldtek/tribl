527cdb74128274eb8f5fc4eb99c5f53605608c38
Eto Olei <teneeto@gmail.com>
2021-05-09 15:53:06 +0100

    add DocumentTypeSelectionScreen types

--- src/screens/types.d.ts
+++ src/screens/types.d.ts
@@ -118,6 +118,7 @@ export type RootStackParamList = {
   TribeDetailScreen?: Object;
   MemberPassportDetailScreen?: Object;
   WaitlistScreen?: Object;
+  DocumentTypeSelectionScreen?: Object;
 };
 
 export type RootStackParamScreensList =
@@ -215,7 +216,8 @@ export type RootStackParamScreensList =
   | 'PreviewScreen'
   | 'TribeDetailScreen'
   | 'MemberPassportDetailScreen'
-  | 'WaitlistScreen';
+  | 'WaitlistScreen'
+  | 'DocumentTypeSelectionScreen';
 
 interface StackNavigationInterface extends StackActionHelpers {
   toggleDrawer(): void;
@@ -223,7 +225,7 @@ interface StackNavigationInterface extends StackActionHelpers {
 
 interface StackScreenInterface {
   navigation: StackNavigationProp<RootStackParamList, string> &
-    StackNavigationInterface;
+  StackNavigationInterface;
   route: RouteProp<RootStackParamList, string>;
 }
 
