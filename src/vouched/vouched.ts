6fdb74a3199cc3f1363e4db52c0f2a2cce5151c7
Eto Olei <teneeto@gmail.com>
2021-05-10 12:51:30 +0100

    add vouched setup

---
+++ src/vouched/vouched.ts
@@ -0,0 +1,16 @@
+import { VouchedSession } from '@vouched.id/vouched-react-native';
+import ENVIRONMENT_VARIABLES from 'react-native-config';
+
+const vouchedPublicKey = ENVIRONMENT_VARIABLES.VOUCHED_PUBLIC_KEY
+let session: any;
+
+export const initSession = (apiKey = vouchedPublicKey) => {
+    session = new VouchedSession(apiKey);
+}
+
+export const getSession = () => {
+    if (!session) {
+        throw "Init Vouched session first."
+    }
+    return session;
+}
