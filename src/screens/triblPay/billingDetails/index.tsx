1537f83d73ab54942b65e26e1c5e33f947cae4b4
Eto Olei <teneeto@gmail.com>
2021-05-12 11:05:13 +0100

    implement country modal

--- src/screens/triblPay/billingDetails/index.tsx
+++ src/screens/triblPay/billingDetails/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 
 import { Title, Text, ProgressBar, TextInput } from 'react-native-paper';
 
@@ -20,6 +20,9 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 import { useMutation } from '@apollo/react-hooks';
 import { ONBOARD_USER } from '../../../graphql/server/mutations';
 import { crashlytics } from '../../../firebase/config';
+import Countries from './widgets/bankCountry';
+import { Modalize } from 'react-native-modalize';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 // DEFINE SCREEN PROP TYPES
 interface ScreenProp extends NavigationInterface {}
@@ -29,12 +32,16 @@ export default function BillingDetailsScreen(props: ScreenProp) {
   const { job, details } = props.route.params;
   const { colors, fonts } = useThemeContext();
   const { t } = useTranslation();
+  const modalizeRef = useRef<Modalize>(null);
+
+  const openModal = () => modalizeRef.current?.open();
+
   const [billingDetails, setBillingDetails] = useState({
     addressLine: '',
     addressCity: '',
     addressState: '',
     addressPostalCode: '',
-    addressCountryCode: details.name
+    addressCountryCode: details.iso2
   });
   const {
     addressLine,
@@ -91,236 +98,246 @@ export default function BillingDetailsScreen(props: ScreenProp) {
   };
 
   return (
-    <KeyboardAwareScrollView
-      bounces={false}
-      showsVerticalScrollIndicator={false}
-      scrollEnabled={true}
-      keyboardShouldPersistTaps={'always'}
-      enableOnAndroid={true}
-      contentContainerStyle={{
-        backgroundColor: colors.WHITE,
-        flex: 1
-      }}
-    >
-      <Fragment>
-        <HeaderCover>
-          <ProgressBar
-            progress={5 / 5}
-            color={colors.PRIMARY}
-            style={{
-              height: RFValue(5),
-              backgroundColor: '#F2F2F7',
-              borderRadius: 4,
-              marginBottom: 10
-            }}
-          />
-          <Text
-            style={{
-              fontFamily: fonts.WORK_SANS_REGULAR,
-              fontSize: RFValue(fonts.LARGE_SIZE - 1),
-              color: colors.PRIMARY,
-              textTransform: 'capitalize',
-              marginBottom: RFValue(10),
-              marginTop: RFValue(15),
-              lineHeight: RFValue(19)
-            }}
-          >
-            {' '}
-            {t(`community.passport.step`)} 5
-          </Text>
-          <Title
-            style={{
-              fontFamily: fonts.WORK_SANS_BOLD,
-              fontSize: RFValue(fonts.LARGE_SIZE + 5),
-              color: colors.PRIMARY_TEXT,
-              lineHeight: RFValue(30)
-            }}
-          >
-            Enter Billing Details
-          </Title>
-          <Text
-            style={{
-              fontFamily: fonts.WORK_SANS_REGULAR,
-              fontSize: RFValue(fonts.MEDIUM_SIZE + 1),
-              color: colors.SECONDARY_TEXT,
-              textTransform: 'capitalize',
-              marginBottom: RFValue(10),
-              lineHeight: RFValue(19)
-            }}
-          >
-            Please Enter billing details.
-          </Text>
-        </HeaderCover>
-
-        <ContactContainer>
-          <InputContainer>
-            <LabelContainer>
-              <Title
-                style={{
-                  fontFamily: fonts.WORK_SANS_BOLD,
-                  fontSize: RFValue(fonts.MEDIUM_SIZE),
-                  color: colors.PRIMARY_TEXT,
-                  textTransform: 'uppercase'
-                }}
-              >
-                address
-              </Title>
-            </LabelContainer>
-            <TextInput
-              multiline={true}
-              dense={true}
-              value={addressLine}
-              onChangeText={(addressLine: string) =>
-                setBillingDetails({ ...billingDetails, addressLine })
-              }
+    <Fragment>
+      <KeyboardAwareScrollView
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        scrollEnabled={true}
+        keyboardShouldPersistTaps={'always'}
+        enableOnAndroid={true}
+        contentContainerStyle={{
+          backgroundColor: colors.WHITE,
+          flex: 1
+        }}
+      >
+        <Fragment>
+          <HeaderCover>
+            <ProgressBar
+              progress={5 / 5}
+              color={colors.PRIMARY}
               style={{
-                height: 30,
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
-                color: colors.PRIMARY_TEXT,
-                backgroundColor: colors.WHITE,
-                borderColor: colors.PRIMARY,
-                textTransform: 'capitalize'
-              }}
-            />
-          </InputContainer>
-
-          <InputContainer>
-            <LabelContainer>
-              <Title
-                style={{
-                  fontFamily: fonts.WORK_SANS_BOLD,
-                  fontSize: RFValue(fonts.MEDIUM_SIZE),
-                  color: colors.PRIMARY_TEXT,
-                  textTransform: 'uppercase'
-                }}
-              >
-                City
-              </Title>
-            </LabelContainer>
-            <TextInput
-              value={addressCity}
-              onChangeText={(addressCity: string) =>
-                setBillingDetails({ ...billingDetails, addressCity })
-              }
-              style={{
-                height: 30,
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
-                color: colors.PRIMARY_TEXT,
-                backgroundColor: colors.WHITE,
-                borderColor: colors.PRIMARY,
-                textTransform: 'capitalize'
-              }}
-            />
-          </InputContainer>
-
-          <InputContainer>
-            <LabelContainer>
-              <Title
-                style={{
-                  fontFamily: fonts.WORK_SANS_BOLD,
-                  fontSize: RFValue(fonts.MEDIUM_SIZE),
-                  color: colors.PRIMARY_TEXT,
-                  textTransform: 'uppercase'
-                }}
-              >
-                State
-              </Title>
-            </LabelContainer>
-            <TextInput
-              value={addressState}
-              onChangeText={(addressState: string) =>
-                setBillingDetails({ ...billingDetails, addressState })
-              }
-              style={{
-                height: 30,
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
-                color: colors.PRIMARY_TEXT,
-                backgroundColor: colors.WHITE,
-                borderColor: colors.PRIMARY,
-                textTransform: 'capitalize'
-              }}
-            />
-          </InputContainer>
-
-          <InputContainer>
-            <LabelContainer>
-              <Title
-                style={{
-                  fontFamily: fonts.WORK_SANS_BOLD,
-                  fontSize: RFValue(fonts.MEDIUM_SIZE),
-                  color: colors.PRIMARY_TEXT,
-                  textTransform: 'uppercase'
-                }}
-              >
-                Postal Code
-              </Title>
-            </LabelContainer>
-            <TextInput
-              value={addressPostalCode}
-              onChangeText={(addressPostalCode: string) =>
-                setBillingDetails({ ...billingDetails, addressPostalCode })
-              }
-              style={{
-                height: 30,
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
-                color: colors.PRIMARY_TEXT,
-                backgroundColor: colors.WHITE,
-                borderColor: colors.PRIMARY,
-                textTransform: 'capitalize'
-              }}
-            />
-          </InputContainer>
-          <InputContainer>
-            <LabelContainer>
-              <Title
-                style={{
-                  fontFamily: fonts.WORK_SANS_BOLD,
-                  fontSize: RFValue(fonts.MEDIUM_SIZE),
-                  color: colors.PRIMARY_TEXT,
-                  textTransform: 'uppercase'
-                }}
-              >
-                Country code
-              </Title>
-            </LabelContainer>
-            <TextInput
-              value={addressCountryCode}
-              onChangeText={(addressCountryCode: string) =>
-                setBillingDetails({ ...billingDetails, addressCountryCode })
-              }
-              style={{
-                height: 30,
-                fontFamily: fonts.WORK_SANS_REGULAR,
-                fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
-                color: colors.PRIMARY_TEXT,
-                backgroundColor: colors.WHITE,
-                borderColor: colors.PRIMARY,
-                textTransform: 'capitalize',
+                height: RFValue(5),
+                backgroundColor: '#F2F2F7',
+                borderRadius: 4,
                 marginBottom: 10
               }}
             />
-          </InputContainer>
-        </ContactContainer>
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: RFValue(fonts.LARGE_SIZE - 1),
+                color: colors.PRIMARY,
+                textTransform: 'capitalize',
+                marginBottom: RFValue(10),
+                marginTop: RFValue(15),
+                lineHeight: RFValue(19)
+              }}
+            >
+              {' '}
+              {t(`community.passport.step`)} 5
+            </Text>
+            <Title
+              style={{
+                fontFamily: fonts.WORK_SANS_BOLD,
+                fontSize: RFValue(fonts.LARGE_SIZE + 5),
+                color: colors.PRIMARY_TEXT,
+                lineHeight: RFValue(30)
+              }}
+            >
+              Enter Billing Details
+            </Title>
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: RFValue(fonts.MEDIUM_SIZE + 1),
+                color: colors.SECONDARY_TEXT,
+                textTransform: 'capitalize',
+                marginBottom: RFValue(10),
+                lineHeight: RFValue(19)
+              }}
+            >
+              Please Enter billing details.
+            </Text>
+          </HeaderCover>
 
-        <GradientButton
-          disabled={loading}
-          loading={loading}
-          onPress={submitKyc}
-          style={{ height: 50 }}
-          gradientContainerstyle={{
-            height: 50,
-            marginBottom: RFValue(30),
-            marginHorizontal: RFValue(15)
-          }}
-          contentStyle={{ height: 50 }}
-        >
-          {loading ? 'loading...' : 'Submit'}
-        </GradientButton>
-      </Fragment>
-    </KeyboardAwareScrollView>
+          <ContactContainer>
+            <InputContainer>
+              <LabelContainer>
+                <Title
+                  style={{
+                    fontFamily: fonts.WORK_SANS_BOLD,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE),
+                    color: colors.PRIMARY_TEXT,
+                    textTransform: 'uppercase'
+                  }}
+                >
+                  address
+                </Title>
+              </LabelContainer>
+              <TextInput
+                multiline={true}
+                dense={true}
+                value={addressLine}
+                onChangeText={(addressLine: string) =>
+                  setBillingDetails({ ...billingDetails, addressLine })
+                }
+                style={{
+                  height: 30,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                  color: colors.PRIMARY_TEXT,
+                  backgroundColor: colors.WHITE,
+                  borderColor: colors.PRIMARY,
+                  textTransform: 'capitalize'
+                }}
+              />
+            </InputContainer>
+
+            <InputContainer>
+              <LabelContainer>
+                <Title
+                  style={{
+                    fontFamily: fonts.WORK_SANS_BOLD,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE),
+                    color: colors.PRIMARY_TEXT,
+                    textTransform: 'uppercase'
+                  }}
+                >
+                  City
+                </Title>
+              </LabelContainer>
+              <TextInput
+                value={addressCity}
+                onChangeText={(addressCity: string) =>
+                  setBillingDetails({ ...billingDetails, addressCity })
+                }
+                style={{
+                  height: 30,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                  color: colors.PRIMARY_TEXT,
+                  backgroundColor: colors.WHITE,
+                  borderColor: colors.PRIMARY,
+                  textTransform: 'capitalize'
+                }}
+              />
+            </InputContainer>
+
+            <InputContainer>
+              <LabelContainer>
+                <Title
+                  style={{
+                    fontFamily: fonts.WORK_SANS_BOLD,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE),
+                    color: colors.PRIMARY_TEXT,
+                    textTransform: 'uppercase'
+                  }}
+                >
+                  State
+                </Title>
+              </LabelContainer>
+              <TextInput
+                value={addressState}
+                onChangeText={(addressState: string) =>
+                  setBillingDetails({ ...billingDetails, addressState })
+                }
+                style={{
+                  height: 30,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                  color: colors.PRIMARY_TEXT,
+                  backgroundColor: colors.WHITE,
+                  borderColor: colors.PRIMARY,
+                  textTransform: 'capitalize'
+                }}
+              />
+            </InputContainer>
+
+            <InputContainer>
+              <LabelContainer>
+                <Title
+                  style={{
+                    fontFamily: fonts.WORK_SANS_BOLD,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE),
+                    color: colors.PRIMARY_TEXT,
+                    textTransform: 'uppercase'
+                  }}
+                >
+                  Postal Code
+                </Title>
+              </LabelContainer>
+              <TextInput
+                value={addressPostalCode}
+                onChangeText={(addressPostalCode: string) =>
+                  setBillingDetails({ ...billingDetails, addressPostalCode })
+                }
+                style={{
+                  height: 30,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                  color: colors.PRIMARY_TEXT,
+                  backgroundColor: colors.WHITE,
+                  borderColor: colors.PRIMARY,
+                  textTransform: 'capitalize'
+                }}
+              />
+            </InputContainer>
+
+            <TouchableOpacity onPress={openModal}>
+              <InputContainer>
+                <LabelContainer>
+                  <Title
+                    style={{
+                      fontFamily: fonts.WORK_SANS_BOLD,
+                      fontSize: RFValue(fonts.MEDIUM_SIZE),
+                      color: colors.PRIMARY_TEXT,
+                      textTransform: 'uppercase'
+                    }}
+                  >
+                    Country code
+                  </Title>
+                </LabelContainer>
+
+                <TextInput
+                  value={addressCountryCode}
+                  disabled={true}
+                  style={{
+                    height: 30,
+                    fontFamily: fonts.WORK_SANS_REGULAR,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                    color: colors.PRIMARY_TEXT,
+                    backgroundColor: colors.WHITE,
+                    borderColor: colors.PRIMARY,
+                    textTransform: 'capitalize',
+                    marginBottom: 10
+                  }}
+                />
+              </InputContainer>
+            </TouchableOpacity>
+          </ContactContainer>
+
+          <GradientButton
+            disabled={loading}
+            loading={loading}
+            onPress={submitKyc}
+            style={{ height: 50 }}
+            gradientContainerstyle={{
+              height: 50,
+              marginBottom: RFValue(30),
+              marginHorizontal: RFValue(15)
+            }}
+            contentStyle={{ height: 50 }}
+          >
+            {loading ? 'loading...' : 'Submit'}
+          </GradientButton>
+        </Fragment>
+      </KeyboardAwareScrollView>
+
+      <Countries
+        modalizeRef={modalizeRef}
+        billingDetails={billingDetails}
+        setBillingDetails={setBillingDetails}
+      />
+    </Fragment>
   );
 }
