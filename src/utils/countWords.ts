e8f14eb1c2ae29c271fbb667deb45e48ebebd6f4
josemak25 <amakirij@gmail.com>
2021-05-18 12:22:02 +0100

    add fix to KYC keys

--- src/utils/countWords.ts
+++ src/utils/countWords.ts
@@ -1,3 +1,3 @@
-export const countWords = (word: string) => {
-    return word.trim().split('').length < 1;
+export const countWords = (word: string | null) => {
+  return !word ? true : word.trim().split('').length < 1;
 };
