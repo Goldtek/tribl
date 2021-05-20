1a99bd2f8b7ef0b762fa45995758d2e929b02bca
Eto Olei <teneeto@gmail.com>
2021-05-20 22:36:16 +0100

    add bank account billing information screen

---
+++ src/screens/triblPay/bankAccount/bankBillingDetails/index.tsx
@@ -0,0 +1,417 @@
+import React, { Fragment, useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useMutation } from '@apollo/react-hooks';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Title, Text, ProgressBar, TextInput } from 'react-native-paper';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+import Countries from './widgets/bankCountry';
+import LocalStates from './widgets/localStates';
+import { Modalize } from 'react-native-modalize';
+import { useThemeContext } from '../../../../theme';
+import { NavigationInterface } from '../../../types';
+import { countWords } from '../../../../utils/countWords';
+import { crashlytics } from '../../../../firebase/config';
+import GradientButton from '../../../../components/gradientButton';
+import { ONBOARD_USER } from '../../../../graphql/server/mutations';
+import { tagScreenName, logEvent } from '../../../../utils/uxcamHelper';
+
+import {
+  HeaderCover,
+  LabelContainer,
+  InputContainer,
+  ContactContainer
+} from './styles';
+
+// DEFINE SCREEN PROP TYPES
+interface ScreenProp extends NavigationInterface {}
+
+export default function BankBillingDetailsScreen(props: ScreenProp) {
+  const { navigation } = props;
+  const { colors, fonts } = useThemeContext();
+  const { t } = useTranslation();
+  const modalizeRef = useRef<Modalize>(null);
+  const modalizeStateRef = useRef<Modalize>(null);
+
+  const openModal = () => modalizeRef.current?.open();
+  const openStateModal = () => modalizeStateRef.current?.open();
+
+  const [billingDetails, setBillingDetails] = useState({
+    addressLine: '',
+    addressLine2: '',
+    addressCity: '',
+    addressState: '',
+    addressCountry: '',
+    addressStateCode: '',
+    addressPostalCode: '',
+    addressCountryCode: ''
+  });
+
+  const [isLocal, setIsLocal] = useState(false);
+
+  const {
+    addressLine,
+    addressLine2,
+    addressCity,
+    addressState,
+    addressCountry,
+    addressStateCode,
+    addressPostalCode,
+    addressCountryCode
+  } = billingDetails;
+
+  useEffect(() => {
+    if (isLocal === false && addressCountryCode === 'US') {
+      setBillingDetails({
+        ...billingDetails,
+        addressState: 'Select'
+      });
+      setIsLocal(true);
+    }
+
+    tagScreenName('BillingDetailsScreen');
+    logEvent('Verify user identity', { from: 'passport' });
+  }, []);
+
+  const [verifyKyc, { loading }] = useMutation(ONBOARD_USER, {
+    variables: {
+      payload: {
+        address: {
+          addressLine,
+          addressLine2,
+          addressCity,
+          addressState,
+          addressCountry,
+          addressStateCode,
+          addressPostalCode,
+          addressCountryCode
+        }
+      }
+    }
+  });
+
+  const submitKyc = async () => {
+    if (countWords(addressLine)) return alert('Address field is compulsory');
+    if (countWords(addressPostalCode))
+      return alert('Postal code field is compulsory');
+    if (countWords(addressCountryCode))
+      return alert('Country code field is compulsory');
+    if (countWords(addressState) || addressState.trim() === 'Select')
+      return alert('State field is compulsory');
+    if (countWords(addressCity)) return alert('City field is compulsory');
+
+    try {
+      const { data } = await verifyKyc();
+      if (data.onBoardUser.status === 'APPROVED')
+        return navigation.navigate('ActivateWalletScreen', { billingDetails });
+      if (data.onBoardUser.status === 'MANUAL_REVIEW')
+        return navigation.navigate('PendingWalletStatusScreen', { data });
+
+      navigation.navigate('FailedWalletStatusScreen', {
+        data,
+        document
+      });
+    } catch (error) {
+      crashlytics.recordError(new Error(error));
+      crashlytics.log(`ERROR MESSAGE, ${error.toString()}`);
+    }
+  };
+
+  const handleStateValue = () => {
+    if (isLocal && addressState === '')
+      setBillingDetails({ ...billingDetails, addressState: 'Select' });
+    return addressState;
+  };
+
+  return (
+    <Fragment>
+      <KeyboardAwareScrollView
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        scrollEnabled={true}
+        keyboardShouldPersistTaps={'always'}
+        enableOnAndroid={true}
+        contentContainerStyle={{
+          backgroundColor: colors.WHITE
+        }}
+      >
+        <Fragment>
+          <HeaderCover>
+            <ProgressBar
+              progress={2 / 2}
+              color={colors.PRIMARY}
+              style={{
+                height: RFValue(5),
+                backgroundColor: '#F2F2F7',
+                borderRadius: 4,
+                marginBottom: 10
+              }}
+            />
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: RFValue(fonts.LARGE_SIZE - 1),
+                color: colors.PRIMARY,
+                textTransform: 'capitalize',
+                marginBottom: RFValue(10),
+                marginTop: RFValue(15),
+                lineHeight: RFValue(19)
+              }}
+            >
+              {' '}
+              {t(`community.passport.step`)} 2
+            </Text>
+            <Title
+              style={{
+                fontFamily: fonts.WORK_SANS_BOLD,
+                fontSize: RFValue(fonts.LARGE_SIZE + 5),
+                color: colors.PRIMARY_TEXT,
+                lineHeight: RFValue(30)
+              }}
+            >
+              Enter Billing Details
+            </Title>
+            <Text
+              style={{
+                fontFamily: fonts.WORK_SANS_REGULAR,
+                fontSize: RFValue(fonts.MEDIUM_SIZE + 1),
+                color: colors.SECONDARY_TEXT,
+                textTransform: 'capitalize',
+                marginBottom: RFValue(10),
+                lineHeight: RFValue(19)
+              }}
+            >
+              Please Enter billing details.
+            </Text>
+          </HeaderCover>
+
+          <ContactContainer>
+            <InputContainer>
+              <LabelContainer>
+                <Title
+                  style={{
+                    fontFamily: fonts.WORK_SANS_BOLD,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE),
+                    color: colors.PRIMARY_TEXT,
+                    textTransform: 'uppercase'
+                  }}
+                >
+                  address
+                </Title>
+              </LabelContainer>
+              <TextInput
+                multiline={true}
+                dense={true}
+                value={addressLine}
+                onChangeText={(addressLine: string) =>
+                  setBillingDetails({ ...billingDetails, addressLine })
+                }
+                style={{
+                  height: 30,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                  color: colors.PRIMARY_TEXT,
+                  backgroundColor: colors.WHITE,
+                  borderColor: colors.PRIMARY,
+                  textTransform: 'capitalize'
+                }}
+              />
+            </InputContainer>
+
+            <InputContainer>
+              <LabelContainer>
+                <Title
+                  style={{
+                    fontFamily: fonts.WORK_SANS_BOLD,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE),
+                    color: colors.PRIMARY_TEXT,
+                    textTransform: 'uppercase'
+                  }}
+                >
+                  address 2
+                </Title>
+              </LabelContainer>
+              <TextInput
+                multiline={true}
+                dense={true}
+                value={addressLine2}
+                onChangeText={(addressLine2: string) =>
+                  setBillingDetails({ ...billingDetails, addressLine2 })
+                }
+                style={{
+                  height: 30,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                  color: colors.PRIMARY_TEXT,
+                  backgroundColor: colors.WHITE,
+                  borderColor: colors.PRIMARY,
+                  textTransform: 'capitalize'
+                }}
+              />
+            </InputContainer>
+
+            <InputContainer>
+              <LabelContainer>
+                <Title
+                  style={{
+                    fontFamily: fonts.WORK_SANS_BOLD,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE),
+                    color: colors.PRIMARY_TEXT,
+                    textTransform: 'uppercase'
+                  }}
+                >
+                  Postal Code
+                </Title>
+              </LabelContainer>
+              <TextInput
+                value={addressPostalCode}
+                onChangeText={(addressPostalCode: string) =>
+                  setBillingDetails({ ...billingDetails, addressPostalCode })
+                }
+                style={{
+                  height: 30,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                  color: colors.PRIMARY_TEXT,
+                  backgroundColor: colors.WHITE,
+                  borderColor: colors.PRIMARY,
+                  textTransform: 'capitalize'
+                }}
+              />
+            </InputContainer>
+
+            <TouchableOpacity onPress={openModal}>
+              <InputContainer>
+                <LabelContainer>
+                  <Title
+                    style={{
+                      fontFamily: fonts.WORK_SANS_BOLD,
+                      fontSize: RFValue(fonts.MEDIUM_SIZE),
+                      color: colors.PRIMARY_TEXT,
+                      textTransform: 'uppercase'
+                    }}
+                  >
+                    Country
+                  </Title>
+                </LabelContainer>
+
+                <TextInput
+                  value={addressCountry}
+                  disabled={true}
+                  style={{
+                    height: 30,
+                    fontFamily: fonts.WORK_SANS_REGULAR,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                    color: colors.PRIMARY_TEXT,
+                    backgroundColor: colors.WHITE,
+                    borderColor: colors.DISABLED,
+                    textTransform: 'capitalize',
+                    borderBottomWidth: isLocal ? 1 : 0
+                  }}
+                />
+              </InputContainer>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={isLocal ? openStateModal : () => {}}>
+              <InputContainer>
+                <LabelContainer>
+                  <Title
+                    style={{
+                      fontFamily: fonts.WORK_SANS_BOLD,
+                      fontSize: RFValue(fonts.MEDIUM_SIZE),
+                      color: colors.PRIMARY_TEXT,
+                      textTransform: 'uppercase'
+                    }}
+                  >
+                    State
+                  </Title>
+                </LabelContainer>
+                <TextInput
+                  value={handleStateValue()}
+                  disabled={isLocal}
+                  onChangeText={(addressState: string) =>
+                    setBillingDetails({
+                      ...billingDetails,
+                      addressState,
+                      addressStateCode: addressState
+                    })
+                  }
+                  style={{
+                    height: 30,
+                    fontFamily: fonts.WORK_SANS_REGULAR,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                    color: colors.PRIMARY_TEXT,
+                    backgroundColor: colors.WHITE,
+                    borderColor: colors.DISABLED,
+                    textTransform: 'capitalize',
+                    borderBottomWidth: isLocal ? 1 : 0
+                  }}
+                />
+              </InputContainer>
+            </TouchableOpacity>
+
+            <InputContainer>
+              <LabelContainer>
+                <Title
+                  style={{
+                    fontFamily: fonts.WORK_SANS_BOLD,
+                    fontSize: RFValue(fonts.MEDIUM_SIZE),
+                    color: colors.PRIMARY_TEXT,
+                    textTransform: 'uppercase'
+                  }}
+                >
+                  City
+                </Title>
+              </LabelContainer>
+              <TextInput
+                value={addressCity}
+                onChangeText={(addressCity: string) =>
+                  setBillingDetails({ ...billingDetails, addressCity })
+                }
+                style={{
+                  height: 30,
+                  fontFamily: fonts.WORK_SANS_REGULAR,
+                  fontSize: RFValue(fonts.MEDIUM_SIZE + 2),
+                  color: colors.PRIMARY_TEXT,
+                  backgroundColor: colors.WHITE,
+                  borderColor: colors.PRIMARY,
+                  textTransform: 'capitalize',
+                  marginBottom: 10
+                }}
+              />
+            </InputContainer>
+          </ContactContainer>
+
+          <GradientButton
+            disabled={loading}
+            loading={loading}
+            onPress={submitKyc}
+            style={{ height: 50 }}
+            gradientContainerstyle={{
+              height: 50,
+              marginBottom: RFValue(30),
+              marginHorizontal: RFValue(15)
+            }}
+            contentStyle={{ height: 50 }}
+          >
+            {loading ? 'loading...' : 'Submit'}
+          </GradientButton>
+        </Fragment>
+      </KeyboardAwareScrollView>
+
+      <Countries
+        modalizeRef={modalizeRef}
+        billingDetails={billingDetails}
+        setBillingDetails={setBillingDetails}
+        setIsLocal={setIsLocal}
+      />
+
+      <LocalStates
+        modalizeStateRef={modalizeStateRef}
+        billingDetails={billingDetails}
+        setBillingDetails={setBillingDetails}
+      />
+    </Fragment>
+  );
+}
---
+++ src/screens/triblPay/bankAccount/bankBillingDetails/widgets/bankCountry/index.tsx
@@ -0,0 +1,139 @@
+import React, { useState, useEffect } from 'react';
+import { Title } from 'react-native-paper';
+import { AntDesign } from '@expo/vector-icons';
+import { useTranslation } from 'react-i18next';
+import { Portal } from 'react-native-portalize';
+import { Modalize } from 'react-native-modalize';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import CountryCard from './widgets/country';
+import Input from '../../../../../../components/input';
+import { useThemeContext } from '../../../../../../theme';
+import { DEVICE_FULL_HEIGHT } from '../../../../../../utils/device';
+import { DataProvider, RecyclerListView } from 'recyclerlistview';
+import { getCountryLayout } from '../../../../../../utils/LayoutUtil';
+import { tagScreenName, logEvent } from '../../../../../../utils/uxcamHelper';
+import countriesDB, {
+  CountryInterface
+} from '../../../../../../libs/countries';
+
+import { Container, SearchCover, HeaderCover } from './styles';
+
+// DEFINE SCREEN PROP TYPES
+
+export default function Countries(props: any) {
+  const insets = useSafeAreaInsets();
+  const { colors, fonts } = useThemeContext();
+  const { t } = useTranslation();
+
+  const [state, setState] = useState({
+    dataProvider: new DataProvider((r1, r2) => r1 !== r2).cloneWithRows(
+      countriesDB.getAllCountries()
+    ),
+    layoutProvider: getCountryLayout()
+  });
+
+  useEffect(() => {
+    tagScreenName('BankCountryScreen');
+    logEvent('select bank country', { from: 'passport' });
+  }, []);
+
+  const rowRenderer = (_type: React.ReactText, data: CountryInterface) => {
+    if (data?.isEmpty) {
+      return (
+        <Title
+          style={{
+            fontFamily: fonts.WORK_SANS_SEMI_BOLD,
+            fontSize: RFValue(Math.ceil(fonts.LARGE_SIZE)),
+            color: colors.PRIMARY_TEXT,
+            lineHeight: RFValue(30)
+          }}
+        >
+          {t(`signup.selectCountry.emptyList`)}
+        </Title>
+      );
+    }
+
+    return <CountryCard {...data} {...props} />;
+  };
+
+  const handleSearch = (value: string) => {
+    const result = countriesDB.searchCountry(value);
+
+    if (!result.length) {
+      return setState({
+        ...state,
+        dataProvider: state.dataProvider.cloneWithRows([{ isEmpty: true }])
+      });
+    }
+
+    setState({
+      ...state,
+      dataProvider: state.dataProvider.cloneWithRows(result)
+    });
+  };
+
+  return (
+    <Portal>
+      <Modalize
+        ref={props.modalizeRef}
+        modalHeight={DEVICE_FULL_HEIGHT}
+        modalStyle={{
+          flex: 1,
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          paddingBottom: 20,
+          paddingTop: insets.top,
+          backgroundColor: colors.WHITE
+        }}
+        childrenStyle={{ paddingBottom: insets.bottom }}
+        handlePosition="inside"
+        panGestureEnabled={false}
+      >
+        <Container>
+          <HeaderCover>
+            <Title
+              style={{
+                fontFamily: fonts.WORK_SANS_BOLD,
+                fontSize: RFValue(fonts.LARGE_SIZE + 5),
+                color: colors.PRIMARY_TEXT,
+                textTransform: 'capitalize',
+                lineHeight: RFValue(30),
+                marginBottom: RFValue(5)
+              }}
+            >
+              Select Country
+            </Title>
+            <Input
+              placeholder={t(`signup.selectCountry.placeholder`)}
+              defaultValue=""
+              onChangeText={handleSearch}
+              textInputStyle={{ fontFamily: fonts.WORK_SANS_SEMI_BOLD }}
+              contanierStyle={{
+                height: RFValue(45),
+                width: '100%',
+                alignSelf: 'center'
+              }}
+            >
+              <SearchCover>
+                <AntDesign
+                  name="search1"
+                  color={colors.PRIMARY_TEXT}
+                  size={RFValue(20)}
+                />
+              </SearchCover>
+            </Input>
+          </HeaderCover>
+
+          <RecyclerListView
+            style={{ height: 600, width: 600, marginTop: RFValue(15) }}
+            dataProvider={state.dataProvider}
+            layoutProvider={state.layoutProvider}
+            rowRenderer={rowRenderer}
+          />
+        </Container>
+      </Modalize>
+    </Portal>
+  );
+}
