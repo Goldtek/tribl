e721c8b98466457d0735271707fb05394da6cc70
Eto Olei <teneeto@gmail.com>
2021-05-24 17:27:06 +0100

    update truncate util

--- src/utils/truncate.ts
+++ src/utils/truncate.ts
@@ -1,4 +1,4 @@
-export const truncateString = (str: string, length: number, omission = '...') => {
+export const truncateString = (str = '', length: number, omission = '...') => {
     if (str.length > length) {
         let subStr = str.substring(str.length - length, str.length);
         return omission + subStr;
