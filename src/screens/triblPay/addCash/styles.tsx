59eac190a493082cb2583632a2cd9461e21e590f
Eto Olei <teneeto@gmail.com>
2021-05-24 16:48:59 +0100

    add loading modal styles for add cash

--- src/screens/triblPay/addCash/styles.tsx
+++ src/screens/triblPay/addCash/styles.tsx
@@ -29,3 +29,15 @@ export const Overlay = styled.TouchableOpacity`
   padding-horizontal: 10px;
   background-color: rgba(0, 0, 0, 0.7);
 `;
+
+export const ModalContentWrapper = styled.View`
+  padding: 20px;
+  border-radius: 5px;
+  background-color: ${({ theme }) => theme.colors.WHITE};
+`;
+
+export const LoaderMessage = styled.Text`
+  color: ${({ theme }) => theme.colors.PRIMARY_TEXT};
+  margin-top: 20px;
+  font-size: ${({ theme }) => theme.fonts.MEDIUM_SIZE}px;
+`;
